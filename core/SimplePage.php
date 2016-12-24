63fff5e0a1ba218db6e4e85569ea4a4f2e918dcb
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-24 09:32:30 -0800

    Create SimplePage.php

---
+++ core/SimplePage.php
@@ -0,0 +1,34 @@
+<?php
+
+function SimplePage($PageTitle='',$BodyCallback = '', $HeadCallback = ''){
+
+	?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta content="IE=edge" http-equiv="X-UA-Compatible">
+	<meta content="initial-scale=1.0, width=device-width" name="viewport">
+	
+	<title><?php echo $PageTitle; ?></title>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	
+	<?php
+		eval($HeadCallback);
+	?>
+</head>
+<body>
+	<?php
+		eval($BodyCallback);
+	?>
+</body>
+</html>
+	<?php
+	
+  exit;
+  
+}
