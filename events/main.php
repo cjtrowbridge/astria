555a0712d095e150c7b1cd17096381c3dc5ae82b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-24 09:11:21 -0800

    Update main.php

--- events/main.php
+++ events/main.php
@@ -1,2 +1,17 @@
 <?php
 
+global $EVENTS, $DEBUG;
+
+$EVENTS=array();
+$DEBUG=array(
+  0=>array(
+    'debug point'=> 'Startup',
+    'memory usage'=> (memory_get_usage()/1000000),
+    'runtime'=>0,
+    'time'=> round(microtime(true),4)
+  )
+);
+
+require('Hook.php');
+require('Event.php');
+require('DebugShowSummary.php');
