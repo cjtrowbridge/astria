f2f04e29efa15851247d9260759954598185a9a5
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-24 09:12:35 -0800

    Create Blowfish.php

---
+++ core/Blowfish.php
@@ -0,0 +1,19 @@
+<?php 
+
+function BlowfishEncrypt($pure_string, $encryption_key=ENCRYPTIONKEY) {
+
+  $iv_size = mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_ECB);
+	$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+	$encrypted_string = base64_encode(mcrypt_encrypt(MCRYPT_BLOWFISH, $encryption_key, utf8_encode($pure_string), MCRYPT_MODE_ECB, $iv));
+	return $encrypted_string;
+
+}
+
+function BlowfishDecrypt($encrypted_string, $encryption_key=ENCRYPTIONKEY) {
+	
+  $iv_size = mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_ECB);
+	$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+	$decrypted_string = mcrypt_decrypt(MCRYPT_BLOWFISH, $encryption_key, base64_decode($encrypted_string), MCRYPT_MODE_ECB, $iv);
+	return $decrypted_string;
+  
+}
