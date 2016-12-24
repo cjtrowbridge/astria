2b7d23b221289fc4420a308d323bed2990ee6d15
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-24 09:46:29 -0800

    Update SimplePage.php

--- core/SimplePage.php
+++ core/SimplePage.php
@@ -17,6 +17,8 @@ function SimplePage($PageTitle='',$BodyCallback = '', $HeadCallback = ''){
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	
+	<link rel="stylesheet" href="/css/style.css">
+	
 	<?php
 		eval($HeadCallback);
 	?>
