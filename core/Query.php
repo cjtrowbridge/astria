705ba92e48cb2e594e7d5bfe7e06fa36eb6f342c
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-24 09:37:33 -0800

    Create Query.php

---
+++ core/Query.php
@@ -0,0 +1,36 @@
+<?php 
+
+global $NUMBER_OF_QUERIES_RUN, $QUERIES_RUN;
+$NUMBER_OF_QUERIES_RUN=0;
+$QUERIES_RUN='';
+
+function Query(
+	$SQL,
+	$Database = 'astria core administrative database',
+	$ForceFresh = false
+){
+
+	global $NUMBER_OF_QUERIES_RUN, $QUERIES_RUN;
+	$NUMBER_OF_QUERIES_RUN+=1;
+	$QUERIES_RUN.=$SQL."\n\n";
+	
+	//Check that database exists and is available, and connect to it.
+	MakeSureDBConnected($Database);
+	
+	global $DATABASES;
+	switch($DATABASES[$Database]['type']){
+		case 'mysql':
+			$result=mysql_query($SQL,$DATABASES[$Database]['resource']) or die(mysql_error());
+			if(!(is_bool($result))){
+				$Output=array();
+				while($Row=mysql_fetch_assoc($result)){
+					$Output[]=$Row;
+				}
+				return $Output;
+			}
+			break;
+		default:
+			die('Unsupported database type: "'.$DATABASES[$Database]['type'].'"');
+	}
+	
+}
