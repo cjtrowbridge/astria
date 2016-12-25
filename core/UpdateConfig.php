5fdb76cbb81586b9841fad04212cbf7694a76d87
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-25 11:55:41 -0800

    Update UpdateConfig.php

--- core/UpdateConfig.php
+++ core/UpdateConfig.php
@@ -1,7 +1,106 @@
 <?php
 
 function UpdateConfig(
-
+  $defaultSessionLength,
+  $encryptionKey,
+  $appName,
+  $appURL,
+  $favicon,
+  $showErrors,
+  $verbose,
+  $smtpUsername,
+  $smtpPassword,
+  $smtpPort,
+  $smtpHost,
+  $smtpAdminEmail,
+  $smtpDefaultFrom,
+  $PHPMailerDebuggingFlag,
+  $dbType,
+  $dbHost,
+  $dbUsername,
+  $dbPassword,
+  $dbName,
+  $GoogleOAuth2ClientID,
+  $GoogleOAuth2ClientSecret,
+  $timezone
 ){
+  //As far as validation goes, anyone using this function is obviously assumed to be a trusted user. Validation here seems like a moot point.
+  
+  //Build New Config File
+  $newConfig="
 
+  global $ASTRIA;
+
+  $ASTRIA = array(
+
+    'app' => array(
+
+      'defaultSessionLength'       => ".$defaultSessionLength.",
+      'encryptionKey'              => ".$encryptionKey.",
+      'appName'                    => ".$appName.",
+      'appURL'                     => ".$appURL.",
+      'favicon'                    => ".$favicon."
+
+    ),
+
+    'debugging' => array(
+
+      'showErrors'                 => ".$showErrors.",
+      'verbose'                    => ".$verbose."
+
+    ),
+
+    'smtp' => array(
+
+      'username'                   => ".$smtpUsername.",
+      'password'                   => ".$smtpPassword.",
+      'port'                       => ".$smtpPort.",
+      'host'                       => ".$smtpHost.",
+      'adminEmail'                 => ".$smtpAdminEmail.",
+      'defaultEmailFrom'           => ".$smtpDefaultFrom.",
+      'PHPMailerDebuggingFlag'     => ".$PHPMailerDebuggingFlag."
+      /*
+        0 = off (for production use)
+        1 = client messages
+        2 = client and server messages      
+      */
+    
+    ),
+
+    'databases'=array(
+      'astria core administrative database' => array(
+
+        'type'                     => ".$dbType.",
+        'hostname'                 => ".$dbHost.",
+        'username'                 => ".$dbUsername.",
+        'password'                 => ".$dbPassword.",
+        'database'                 => ".$dbName.",
+        'resource'                 => false
+
+      )
+    ),
+
+    'oauth'=array(
+
+      'Google' => array(
+
+        'GoogleOAuth2ClientID'     => ".$GoogleOAuth2ClientID.",
+        'GoogleOAuth2ClientSecret' => ".$GoogleOAuth2ClientSecret."
+
+      )
+
+    ),
+    'locale'=>array(
+      'timezone' => ".$timezone."
+    )
+
+  );
+  
+  ";
+  
+  $result=file_put_contents('config.php', $newConfig);
+  if($result==false){
+   die("Could not write config file. Please give write permission or copy the following into a new config.php file;\n\n".$newConfig); 
+  }
+  
 }
