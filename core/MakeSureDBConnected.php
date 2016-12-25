3f47ae8e86622c531d421610107c68d304a2b617
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-25 13:23:48 -0800

    Update MakeSureDBConnected.php

--- core/MakeSureDBConnected.php
+++ core/MakeSureDBConnected.php
@@ -1,24 +1,24 @@
 <?php
 
 function MakeSureDBConnected($Database='astria core administrative database'){
-  
-	global $DATABASES;
-	
-  if(!(isset($DATABASES[$Database]))){die('Database configuration not found for '.$Database.'. Please add to config.php.');}
-  
-	if($DATABASES[$Database]['resource']==false){
-		switch($DATABASES[$Database]['type']){
-			case 'mysql':
-				$DATABASES[$Database]['resource'] = mysqli_connect(
-					$DATABASES[$Database]['hostname'],
-					$DATABASES[$Database]['username'],
-					$DATABASES[$Database]['password'],
-          $DATABASES[$Database]['database']
-				) or die(mysql_error());
-				break;
-			default:
-				die('Unsupported database type: "'.$DATABASES[$Database]['type'].'" for database "'.$Database.'"');
-		}
-	}
-	
+
+global $ASTRIA;
+
+if(!(isset($DATABASES[$Database]))){die("Database configuration not found for '".$Database."'. Please add to config.php.");}
+
+if($DATABASES[$Database]['resource']==false){
+switch($DATABASES[$Database]['type']){
+case 'mysql':
+$DATABASES[$Database]['resource'] = mysqli_connect(
+$DATABASES[$Database]['hostname'],
+$DATABASES[$Database]['username'],
+$DATABASES[$Database]['password'],
+$DATABASES[$Database]['database']
+) or die(mysql_error());
+break;
+default:
+die('Unsupported database type: "'.$DATABASES[$Database]['type'].'" for database "'.$Database.'"');
+}
+}
+
 }
