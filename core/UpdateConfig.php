af9bdfe674d8ceeecaf882f693ac8e2bbc65440c
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-25 12:10:38 -0800

    Update UpdateConfig.php

--- core/UpdateConfig.php
+++ core/UpdateConfig.php
@@ -26,7 +26,30 @@ function UpdateConfig(
 ){
   //TODO add a null default to each argument, and then fill any nulls in with current values, then sort the arguments in order of how frequently they may ned to be updated
   
-  //As far as validation goes, anyone using this function is obviously assumed to be a trusted user. Validation here seems like a moot point.
+  //As far as validation goes, anyone using this function is obviously assumed to be a trusted user. Validation here seems like a moot point, but apostrophes still need to be escaped or the array wont work. Obviously if there is an apostrophe in a hostname, we have other problems, but we can deal with validating later.
+  $defaultSessionLength      = str_replace("'","\'",$defaultSessionLength);
+  $encrypionKey              = str_replace("'","\'",$encrypionKey);
+  $appName                   = str_replace("'","\'",$appName);
+  $appURL                    = str_replace("'","\'",$appURL);
+  $favicon                   = str_replace("'","\'",$favicon);
+  $showErrors                = str_replace("'","\'",$showErrors);
+  $verbose                   = str_replace("'","\'",$verbose);
+  $smtpUsername              = str_replace("'","\'",$smtpUsername);
+  $smtpPassword              = str_replace("'","\'",$smtpPassword);
+  $smtpPort                  = str_replace("'","\'",$smtpPort);
+  $smtpHost                  = str_replace("'","\'",$smtpHost);
+  $smtpAdminEmail            = str_replace("'","\'",$smtpAdminEmail);
+  $smtpDefaultFrom           = str_replace("'","\'",$smtpDefaultFrom);
+  $PHPMailerDebuggingFlag    = str_replace("'","\'",$PHPMailerDebuggingFlag);
+  $dbType                    = str_replace("'","\'",$dbType);
+  $dbHost                    = str_replace("'","\'",$dbHost);
+  $dbUsername                = str_replace("'","\'",$dbUsername);
+  $dbPassword                = str_replace("'","\'",$dbPassword);
+  $dbName                    = str_replace("'","\'",$dbName);
+  $GoogleOAuth2ClientID      = str_replace("'","\'",$GoogleOAuth2ClientID);
+  $GoogleOAuth2ClientSecret  = str_replace("'","\'",$GoogleOAuth2ClientSecret);
+  $timezone                  = str_replace("'","\'",$timezone);
+
   
   //Build New Config File
   $newConfig="
