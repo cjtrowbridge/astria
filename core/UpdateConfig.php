665429b1397a5357f3e05f695208ad4586eefa08
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-25 12:04:49 -0800

    Update UpdateConfig.php

--- core/UpdateConfig.php
+++ core/UpdateConfig.php
@@ -31,36 +31,37 @@ function UpdateConfig(
   //Build New Config File
   $newConfig="
 
-  global $ASTRIA;
+  global \$ASTRIA;
 
-  $ASTRIA = array(
+  \$ASTRIA = array(
 
     'app' => array(
 
-      'defaultSessionLength'       => ".$defaultSessionLength.",
-      'encryptionKey'              => ".$encryptionKey.",
-      'appName'                    => ".$appName.",
-      'appURL'                     => ".$appURL.",
-      'favicon'                    => ".$favicon."
+      'defaultSessionLength'       => '".$defaultSessionLength."',
+      'encryptionKey'              => '".$encryptionKey."',
+      'appName'                    => '".$appName."',
+      'appURL'                     => '".$appURL."',
+      'favicon'                    => '".$favicon."'
 
     ),
 
     'debugging' => array(
 
-      'showErrors'                 => ".$showErrors.",
-      'verbose'                    => ".$verbose."
+      'showErrors'                 => '".$showErrors."',
+      'verbose'                    => '".$verbose."'
 
     ),
 
     'smtp' => array(
 
-      'username'                   => ".$smtpUsername.",
-      'password'                   => ".$smtpPassword.",
-      'port'                       => ".$smtpPort.",
-      'host'                       => ".$smtpHost.",
-      'adminEmail'                 => ".$smtpAdminEmail.",
-      'defaultEmailFrom'           => ".$smtpDefaultFrom.",
-      'PHPMailerDebuggingFlag'     => ".$PHPMailerDebuggingFlag."
+      'username'                   => '".$smtpUsername."',
+      'password'                   => '".$smtpPassword."',
+      'port'                       => '".$smtpPort."',
+      'host'                       => '".$smtpHost."',
+      'adminEmail'                 => '".$smtpAdminEmail."',
+      'defaultEmailFrom'           => '".$smtpDefaultFrom."',
+      'PHPMailerDebuggingFlag'     => '".$PHPMailerDebuggingFlag."'
+      
       /*
         0 = off (for production use)
         1 = client messages
@@ -72,11 +73,11 @@ function UpdateConfig(
     'databases'=array(
       'astria core administrative database' => array(
 
-        'type'                     => ".$dbType.",
-        'hostname'                 => ".$dbHost.",
-        'username'                 => ".$dbUsername.",
-        'password'                 => ".$dbPassword.",
-        'database'                 => ".$dbName.",
+        'type'                     => '".$dbType."',
+        'hostname'                 => '".$dbHost."',
+        'username'                 => '".$dbUsername."',
+        'password'                 => '".$dbPassword."',
+        'database'                 => '".$dbName."',
         'resource'                 => false
 
       )
@@ -86,14 +87,14 @@ function UpdateConfig(
 
       'Google' => array(
 
-        'GoogleOAuth2ClientID'     => ".$GoogleOAuth2ClientID.",
-        'GoogleOAuth2ClientSecret' => ".$GoogleOAuth2ClientSecret."
+        'GoogleOAuth2ClientID'     => '".$GoogleOAuth2ClientID."',
+        'GoogleOAuth2ClientSecret' => '".$GoogleOAuth2ClientSecret."'
 
       )
 
     ),
     'locale'=>array(
-      'timezone' => ".$timezone."
+      'timezone' => '".$timezone."'
     )
 
   );
@@ -104,5 +105,7 @@ function UpdateConfig(
   if($result==false){
    die("Could not write config file. Please give write permission or copy the following into a new config.php file;\n\n".$newConfig); 
   }
+  
+  exit;
   
 }
