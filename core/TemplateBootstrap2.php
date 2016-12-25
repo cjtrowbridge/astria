1eed65f328134f115f9daec09ad8f92bcf56705c
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-25 13:43:16 -0800

    Update TemplateBootstrap2.php

--- core/TemplateBootstrap2.php
+++ core/TemplateBootstrap2.php
@@ -61,10 +61,6 @@ function TemplateBootstrap2($title=''){
 
   <div class="container">
 
-    <div class="starter-template">
-      <h1>Bootstrap starter template</h1>
-      <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-    </div>
     <?php 
       Event('Template Body');
     ?> 
