abe91b0af83ce5d7a0b88e784de82876233532b5
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-25 10:32:19 -0800

    Update setup.php

--- setup.php
+++ setup.php
@@ -1,5 +1,12 @@
 <?php
 
 function setup(){
+  require('core/SimplePage.php');
+  SimplePage('Astria Setup','setupBodyCallback();');
+}
 
+function setupBodyCallback(){
+?><h1>Welcome To Astria Setup</h1>
+
+<?php
 }
