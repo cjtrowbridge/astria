5fa14147772c21b6b03d7d9fa4524354ec27ab9e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-25 20:52:48 -0800

    Create DiskCache.php

---
+++ core/DiskCache.php
@@ -0,0 +1,35 @@
+<?php
+
+function writeDiskCache($hash,$value){
+  
+  $value=var_export($value,$true);
+  
+  $value=BlowfishEncrypt($value);
+  
+  return file_put_contents('cache/'.$hash.'.php',$value);
+
+}
+
+function readDiskCache($hash,$ttl){
+  if(!(isValidMd5($hash))){
+    return false;
+  }
+  
+  $path='cache/'.$hash.'.php';
+  
+  if(!(file_exists($path)){
+    return false;
+  }
+  
+  if((filemtime($path)+$ttl)>time()){
+    unlink($path);
+    return false;
+  }
+  
+  $value=file_get_contents($path);
+  $value=BlowfishDecrypt($value);
+  
+  eval('$return = ' . $value);
+
+  return $return;
+}
