f4cc72d26952c0fd953b6ba6dffba0f5f474e252
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-28 17:22:19 -0800

    Update architect.php

--- core/architect.php
+++ core/architect.php
@@ -1,2 +1,25 @@
 <?php
 
+include_once('Path.php');
+
+if(
+  path()=='architect'
+  //&& TODO User should be able to see this
+){
+  Hook('Template Body','ArchitectBodyCallback();');
+}
+
+function ArchitectBodyCallback(){
+  global $EVENTS;
+  ?>
+<h1>Architect</h1>
+<div class="col-xs-12 xol-md-6">
+  <h2>Current Hooks</h2>
+  <?php pd($EVENTS); ?>
+</div>
+<div class="col-xs-12 xol-md-6">
+  <h2>Debug Summary</h2>
+  <?php DebugShowSummary(); ?>
+</div>
+<?php
+}
