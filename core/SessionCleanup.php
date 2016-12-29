10b9eb1e3bb03fbdeb5cba8ea04dd8f7d011e614
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-28 21:53:50 -0800

    Update SessionCleanup.php

--- core/SessionCleanup.php
+++ core/SessionCleanup.php
@@ -5,11 +5,12 @@ function SessionCleanup(){
   $path = 'cache/';
   if ($handle = opendir($path)) {
     while (false !== ($file = readdir($handle))) {
-      if ((time()-filectime($path.$file)) < 1){  
+      if ((time()-filectime($path.$file)) > $ASTRIA['app']['defaultSessionLength']){  
         if(strpos($file, 'session_')==1){
-          unlink($path.$file);
+          //unlink($path.$file);
+          echo 'deleted file: '.$file."<br>\n";
         }else{echo 'skipped nonsession file'.$file.": ".(strpos($file, 'session_'))."<br>\n";}
-      }else{echo 'skipped fresh file '.$file." age: ".(time()-filectime($path.$file))."<br>\n";}
+      }else{echo 'skipped fresh file '.$file." age: ".(time()-filectime($path.$file))."/ ".$ASTRIA['app']['defaultSessionLength']."<br>\n";}
     }
   }else{die('Cant open cache dir.');}
 }
