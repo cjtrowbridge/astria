5aea1848c8c68efdd113360f6cdfd0ba4d1b3ab7
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-28 21:55:09 -0800

    Update SessionCleanup.php

--- core/SessionCleanup.php
+++ core/SessionCleanup.php
@@ -5,8 +5,8 @@ function SessionCleanup(){
   $path = 'cache/';
   if ($handle = opendir($path)) {
     while (false !== ($file = readdir($handle))) {
-      if ((time()-filectime($path.$file)) > 1){  
-        if(strpos($file, 'session_')==1){
+      if ((time()-filectime($path.$file)) > $ASTRIA['app']['defaultSessionLength']){  
+        if(strpos($file, 'session_')==0){
           //unlink($path.$file);
           echo 'deleted file: '.$file."<br>\n";
         }else{echo 'skipped nonsession file'.$file.": ".(strpos($file, 'session_'))."<br>\n";}
