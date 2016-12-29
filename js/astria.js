ab4eb6676e617f92c8451fa315f1bbf0c43a6ce9
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-28 23:36:12 -0800

    Update astria.js

--- js/astria.js
+++ js/astria.js
@@ -7,7 +7,7 @@ function AddCard(
 '    <div class="card card-block">'+
 '      <h4 class="card-title">'+title+'</h4>'+
 '      <p class="card-text">'+text+'</p>'+
-'      <button type="button" class="btn btn-outline-danger" onclick="$(this).parent().fadeOut(300);">Cancel</button>'+
+'      <button type="button" class="btn btn-outline-danger" onclick="$(this).parent().fadeOut(100);">Cancel</button>'+
 '      <button type="button" class="btn btn-success">Continue</button>'+
 '    </div>'
   );
