a55650a6b71e7823c07a37c0792939c7de8567d2
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-28 16:48:01 -0800

    Update TemplateBootstrap2.php

--- core/TemplateBootstrap2.php
+++ core/TemplateBootstrap2.php
@@ -45,15 +45,6 @@ function TemplateBootstrap2($title=''){
   <nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
     <a class="navbar-brand" href="#"><?php echo $ASTRIA['app']['appName']; ?></a>
     <ul class="nav navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
-      </li>
       <?php Event('Template Nav'); ?>
     </ul>
   </nav>
