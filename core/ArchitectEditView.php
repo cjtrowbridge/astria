03fab606a7db3728cce3c80e3a03e3b4ccf9a4bc
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-29 15:42:43 -0800

    Update ArchitectEditView.php

--- core/ArchitectEditView.php
+++ core/ArchitectEditView.php
@@ -68,14 +68,14 @@ function ArchitectEditViewBodyCallback(){
           <div class="form-group row">
             <label class="col-xs-2 col-form-label">Groups:</label>
             <div class="col-xs-10">
-              <button type="button" class="btn btn-secondary btn-lg" disabled>New Group</button>
+              <button type="button" class="btn btn-secondary btn-lg float-xs-right" disabled>New Group</button>
               List Groups
             </div>
           </div>
           <div class="form-group row">
             <label class="col-xs-2 col-form-label">Hooks and Callbacks:</label>
             <div class="col-xs-10">
-              <button type="button" class="btn btn-secondary btn-lg" disabled>New Hook</button>
+              <button type="button" class="btn btn-secondary btn-lg float-xs-right" disabled>New Hook</button>
               List Hooks
             </div>
           </div>
