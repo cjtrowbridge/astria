a3f7afcb63e0449e9b8f285c3e83bb611d29a27d
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-28 21:51:55 -0800

    Update SessionCleanup.php

--- core/SessionCleanup.php
+++ core/SessionCleanup.php
@@ -8,8 +8,8 @@ function SessionCleanup(){
       if ((time()-filectime($path.$file)) < 1){  
         if(strpos($file, 'session_')==1){
           unlink($path.$file);
-        }else{echo 'skipped nonsession file'.$file."<br>\n";}
-      }else{echo 'skipped fresh file '.$file."<br>\n";}
+        }else{echo 'skipped nonsession file'.$file.": ".(strpos($file, 'session_')."<br>\n";}
+      }else{echo 'skipped fresh file '.$file." age: ".(time()-filectime($path.$file))."<br>\n";}
     }
   }else{die('Cant open cache dir.');}
 }
