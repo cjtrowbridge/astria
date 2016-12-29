d11fb909509105bf024bdd77daa380347b3cb1f6
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-28 23:58:44 -0800

    Update astria.js

--- js/astria.js
+++ js/astria.js
@@ -8,8 +8,9 @@ function AddCard(
 '      <h4 class="card-title">'+title+' <button type="button" class="btn btn-link float-xs-right muted" onclick="$(this).parent().parent().fadeOut(100);">x</button></h4>'+
 '      <p class="card-text">'+text+'</p>'+
 '    </div>'
-  );
-  $('body card:last-of-type input:first-of-type').focus();
+  ,function(){
+    $('body card:last-of-type input:first-of-type').focus();
+  });
 }
 function Cardify(title,id){
   AddCard(
