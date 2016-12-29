ee482964fce1865470b3f384a36c64ba2d003046
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-28 23:41:38 -0800

    Update astria.js

--- js/astria.js
+++ js/astria.js
@@ -5,7 +5,7 @@ function AddCard(
   body = $('#bodyContainer');
   body.append(
 '    <div class="card card-block">'+
-'      <h4 class="card-title">'+title+' <button type="button" class="btn btn-link float-xs-right muted" onclick="$(this).parent().fadeOut(100);">x</button></h4>'+
+'      <h4 class="card-title">'+title+' <button type="button" class="btn btn-link float-xs-right muted" onclick="$(this).parent().parent().fadeOut(100);">x</button></h4>'+
 '      <p class="card-text">'+text+'</p>'+
 '    </div>'
   );
