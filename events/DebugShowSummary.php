249c1046ab494c12de5621f784dc9ca6c80c036c
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-28 17:27:03 -0800

    Update DebugShowSummary.php

--- events/DebugShowSummary.php
+++ events/DebugShowSummary.php
@@ -3,16 +3,16 @@
 function DebugShowSummary(){
   
   global $DEBUG;
-  echo "<h3>Summary</h3>\n";
+  echo "<h3>Summary:</h3>\n";
   $summary=array(
     array(
       'Total Runtime' => ($DEBUG[(count($DEBUG)-1)]['time']-$DEBUG[0]['time']).'  seconds',
       'Total Memory Consumption' => $DEBUG[(count($DEBUG)-1)]['memory usage'].' megabytes'
     )
   );
-  ArrTabler($summary);
+  echo ArrTabler($summary);
   echo "<h3>Detail:</h3>\n";
-  ArrTabler($DEBUG);
+  echo ArrTabler($DEBUG);
   ?>
   <script>$('.tablesorter').tablesorter({widgets: ["zebra", "filter"]});</script>
   <?php 
