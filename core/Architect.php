db9e5d51ff12840d4d34d77e034bd20eaf9d5c5b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-28 23:37:09 -0800

    Update Architect.php

--- core/Architect.php
+++ core/Architect.php
@@ -41,7 +41,7 @@ function ArchitectBodyCallback(){
       <button onclick="$('#hooks').slideToggle();" type="button" class="btn btn-outline-primary">Hooks</button>
       <button onclick="$('#queriesRun').slideToggle();" type="button" class="btn btn-outline-primary">Queries</button>
       <button onclick="$('#session').slideToggle();" type="button" class="btn btn-outline-primary">Session</button>
-    </form>
+    </form><br>
     <form class="form-inline">
       <button type="button" class="btn btn-info" onclick="NewView();">New View</button>
     </form>
