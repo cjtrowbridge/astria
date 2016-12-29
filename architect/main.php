963a003086a7676cb2c3ac2cdd5cdd0772fb3d42
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-28 16:47:03 -0800

    Create main.php

---
+++ architect/main.php
@@ -0,0 +1,11 @@
+<?php
+
+Hook('Template Nav','ArchitectTemplateNav();');
+
+function ArchitectTemplateNav(){
+?>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Architect</a>
+      </li>
+<?php
+}
