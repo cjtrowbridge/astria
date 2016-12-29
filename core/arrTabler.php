2c11bbd89740646c91f5b1ec67ab7b04a7de5cda
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-28 17:21:56 -0800

    Create arrTabler.php

---
+++ core/arrTabler.php
@@ -0,0 +1,32 @@
+<?php
+
+function ArrTabler($arr, $table_id = null, $table_class = 'tablesorter tablesorter-blue'){
+  $return='';
+  if($table_id==null){
+    $table_id=md5(uniqid(true));
+  }
+  $return.= "\r\n".'	<table id="'.$table_id.'" class="'.$table_class.'">'."\n";
+  $first=true;
+  foreach($arr as $row){
+    if($first){
+      $return.= "		<thead>\n";
+      $return.= "			<tr>\n";
+      foreach($row as $key => $value){
+        $return.= "				<th>".ucwords($key)."</th>\n";
+      }
+      $return.= "			</tr>\n";
+      $return.= "		</thead>\n";
+      $return.= "		<tbody>\n";
+    }
+    $first=false;
+    $return.= "			<tr>\n";
+    foreach($row as $key => $value){
+      $return.=AutoTabler_Table_Field_Handler_Return($key, $value,$row);
+    }
+    $return.= "			</tr>\n";
+  }
+  $return.= "		</tbody>\n";
+  $return.= "	</table>\n";
+  $return.= "	<script>$('#".$table_id."').tablesorter({widgets: ['zebra', 'filter']});</script>\n";
+  return $return;
+}
