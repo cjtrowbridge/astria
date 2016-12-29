7a4066cc5b4969a867ba1b7d1412a1616d7d9125
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-29 01:13:58 -0800

    Update ArchitectEditView.php

--- core/ArchitectEditView.php
+++ core/ArchitectEditView.php
@@ -5,11 +5,27 @@ function ArchitectEditView(){
   TemplateBootstrap2('Architect'); 
 }
 function ArchitectEditViewBodyCallback(){
+  MakeSureDBConnected();
   
   if(is_integer(path(2))){
     //look up by id
-    
+    $View=Query("
+      SELECT * FROM View 
+      LEFT JOIN Hook ON Hook.ViewID = View.ViewID
+      LEFT JOIN Callback.CallbackID = Hook.CallbackID
+      WHERE View.ViewID = ".intval(path(2))."
+    ");
   }else{
     //try looking up by slug
+    $safeslug=mysqli_real_escape_string($ASTRIA['databases']['astria core administrative database']['resource'],path(2));
+    $View=Query("
+      SELECT * FROM View 
+      LEFT JOIN Hook ON Hook.ViewID = View.ViewID
+      LEFT JOIN Callback.CallbackID = Hook.CallbackID
+      WHERE View.Slug LIKE '".$safeslug."'
+    ");
   }
+  pd($View);
+  
+  
 }
