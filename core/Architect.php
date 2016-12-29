40cc075f825eecee2bc90a0bf32dbced78edc7e2
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-29 00:09:51 -0800

    Update Architect.php

--- core/Architect.php
+++ core/Architect.php
@@ -37,12 +37,10 @@ function ArchitectBodyCallback(){
 <div class="row">
   <div class="col-xs-12">
     <form class="form-inline">
-      <button onclick="Cardify('Events','debugSummary');" type="button" class="btn btn-outline-primary">Events</button>
-      <button onclick="Cardify('Hooks','hooks');" type="button" class="btn btn-outline-primary">Hooks</button>
-      <button onclick="Cardify('Queries','queriesRun');" type="button" class="btn btn-outline-primary">Queries</button>
-      <button onclick="Cardify('Session','session');" type="button" class="btn btn-outline-primary">Session</button>
-    </form><br>
-    <form class="form-inline">
+      <button onclick="Cardify('Events','debugSummary');" type="button" class="btn btn-outline-warning">Events</button>
+      <button onclick="Cardify('Hooks','hooks');" type="button" class="btn btn-outline-warning">Hooks</button>
+      <button onclick="Cardify('Queries','queriesRun');" type="button" class="btn btn-outline-warning">Queries</button>
+      <button onclick="Cardify('Session','session');" type="button" class="btn btn-outline-warning">Session</button>
       <button onclick="Cardify('New View','newView');" type="button" class="btn btn-info" >New View</button>
     </form>
   </div>
