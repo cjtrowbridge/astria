d7f4895a987e106bd9f4c301cb2407d9c547b768
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-29 15:59:01 -0800

    Update FieldMask.php

--- core/FieldMask.php
+++ core/FieldMask.php
@@ -5,6 +5,9 @@ function FieldMask($key, $value,$row){
     case 'ViewID':
       return '<a href="/architect/edit-view/'.$value.'">'.$value.'</a>';
       break;
+    case 'ViewCategoryID':
+      return '<a href="/architect/view-category/'.$value.'">'.$value.'</a>';
+      break;
     default:
       return $value;
   }
