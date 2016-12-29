0666f7958bc6de9b814ba1d3ab18bcad9e7e26a4
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-28 23:31:04 -0800

    Create astria.js

---
+++ js/astria.js
@@ -0,0 +1,14 @@
+function AddCard(
+  title,
+  text
+){
+  body = $('#bodyContainer');
+  body.append('
+    <div class="card card-block">
+      <h4 class="card-title">'+title+'</h4>
+      <p class="card-text">'+text+'</p>
+      <button type="button" class="btn btn-outline-danger">Cancel</button>
+      <button type="button" class="btn btn-success">Continue</button>
+    </div>
+  ');
+}
