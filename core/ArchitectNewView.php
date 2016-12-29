1ae972ce0842ab59329c1a043022e57e8f6db3a2
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-29 00:40:15 -0800

    Update ArchitectNewView.php

--- core/ArchitectNewView.php
+++ core/ArchitectNewView.php
@@ -8,9 +8,9 @@ function ArchitectNewView(){
     
     $newViewName         = mysqli_real_escape_string($ASTRIA['databases']['astria core administrative database']['resource'],$_POST['newViewName']);
     $newViewDescription  = mysqli_real_escape_string($ASTRIA['databases']['astria core administrative database']['resource'],$_POST['newViewDescription']);
-    $newViewNameSlug     = mysqli_real_escape_string($ASTRIA['databases']['astria core administrative database']['resource'],$_POST['newViewNameSlug']);
+    $newViewSlug     = mysqli_real_escape_string($ASTRIA['databases']['astria core administrative database']['resource'],$_POST['newViewSlug']);
 
-    $sql="INSERT INTO `View` (`Slug`, `Name`, `Description`) VALUES ('".$newViewNameSlug."', '".$newViewName."', '".$newViewDescription."');";
+    $sql="INSERT INTO `View` (`Slug`, `Name`, `Description`) VALUES ('".$newViewSlug."', '".$newViewName."', '".$newViewDescription."');";
     die($sql);
     Query($sql);
     $ViewID=mysqli_insert_id($ASTRIA['databases']['astria core administrative database']['resource']);
