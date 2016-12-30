03b7495b2b1b0c98ded2ce774bebf0eedcd6b53b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-29 16:04:26 -0800

    Update ArchitectViewCategory.php

--- core/ArchitectViewCategory.php
+++ core/ArchitectViewCategory.php
@@ -1,5 +1,27 @@
 <?php
 
 function ArchitectViewCategory(){
+  //TODO this should eventually work for View Slugs in addition to ViewIDs
+  if(path(2)==intval(path(2))){
+    
+    //Show a specific view
+    $View=Query("SELECT * FROM Views WHERE ViewID = ".intval(path(2))."")
+    ShowAView($View);
+    
+  }else{
+    
+    //List all views 
+    ShowAllViews();
+    
+  }
+}
 
+function ShowAllViews(){
+  echo ArrTabler(Query("
+    SELECT * FROM Views
+  "));
+}
+
+function ShowAView($View){
+   echo ArrTabler($View); 
 }
