4bd3f5a643dee15858198cd6a5d4eebcffc30680
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-12-29 16:43:37 -0800

    Update ArrTabler.php

--- core/ArrTabler.php
+++ core/ArrTabler.php
@@ -5,28 +5,32 @@ function ArrTabler($arr, $table_id = null, $table_class = 'tablesorter tablesort
   if($table_id==null){
     $table_id=md5(uniqid(true));
   }
-  $return.= "\r\n".'	<table id="'.$table_id.'" class="'.$table_class.'">'."\n";
-  $first=true;
-  foreach($arr as $row){
-    if($first){
-      $return.= "		<thead>\n";
+  if(count($arr)>0){
+    $return.= "\r\n".'	<table id="'.$table_id.'" class="'.$table_class.'">'."\n";
+    $first=true;
+    foreach($arr as $row){
+      if($first){
+        $return.= "		<thead>\n";
+        $return.= "			<tr>\n";
+        foreach($row as $key => $value){
+          $return.= "				<th>".ucwords($key)."</th>\n";
+        }
+        $return.= "			</tr>\n";
+        $return.= "		</thead>\n";
+        $return.= "		<tbody>\n";
+      }
+      $first=false;
       $return.= "			<tr>\n";
       foreach($row as $key => $value){
-        $return.= "				<th>".ucwords($key)."</th>\n";
+        $return.="<td>".FieldMask($key, $value,$row)."</td>";
       }
       $return.= "			</tr>\n";
-      $return.= "		</thead>\n";
-      $return.= "		<tbody>\n";
     }
-    $first=false;
-    $return.= "			<tr>\n";
-    foreach($row as $key => $value){
-      $return.="<td>".FieldMask($key, $value,$row)."</td>";
-    }
-    $return.= "			</tr>\n";
+    $return.= "		</tbody>\n";
+    $return.= "	</table>\n";
+    $return.= "	<script>$('#".$table_id."').tablesorter({widgets: ['zebra', 'filter']});</script>\n";
+  }else{
+    $return.="No Results Found.";
   }
-  $return.= "		</tbody>\n";
-  $return.= "	</table>\n";
-  $return.= "	<script>$('#".$table_id."').tablesorter({widgets: ['zebra', 'filter']});</script>\n";
   return $return;
 }
