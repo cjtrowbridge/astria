b152ff514a687a008ee43b25eb875607b37186b7
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-12 23:53:24 -0800

    Update main.php

--- view/main.php
+++ view/main.php
@@ -9,12 +9,13 @@ function noView(){
   TemplateBootstrap2('404');
 }
 function noViewBodyCallback(){
-  global $NUMBER_OF_QUERIES_RUN, $QUERIES_RUN, $DEBUG;
+  global $NUMBER_OF_QUERIES_RUN, $QUERIES_RUN, $DEBUG, $NUMBER_OF_QUERIES_RUN_FROM_DISK_CACHE;
   //echo '<h1>404: Page not found.</h1> User '.$_SESSION['User']['Email'].' is logged in, but nothing happened at this url. Runtime '.round(microtime(true)-STARTTIME,4)." seconds. \n\n\n\n<!-- QUERIES RUN \n".addslashes($QUERIES_RUN)."-->\n\n\n\n".$NUMBER_OF_QUERIES_RUN.' Queries Run.</span> Session Expires '.date('r',$_SESSION['Auth']['Expires']).'. <a href="./?logout">Log Out</a>.';
   echo '<h1>404: Page not found.</h1>';
   echo 'User '.$_SESSION['User']['Email'].' is logged in, but nothing happened at this url.<br>';
   echo 'Runtime '.round(microtime(true)-$DEBUG[0]['timestamp'],4)." seconds.<br>";
   echo 'Ran '.$NUMBER_OF_QUERIES_RUN.' Queries.<br>';
+  echo 'Ran '.$NUMBER_OF_QUERIES_RUN_FROM_DISK_CACHE.' Queries From Disk Cache.<br>';
   echo 'Session Expires '.date('r',$_SESSION['Auth']['Expires']).'.<br>';
   echo '<a href="/?logout">Log Out</a>.';
   //pd($_SESSION); 
