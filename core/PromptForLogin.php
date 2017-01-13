2759d33b1d1fb3f895b45a5d2169d355e2da6b21
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-12 23:52:27 -0800

    Update PromptForLogin.php

--- core/PromptForLogin.php
+++ core/PromptForLogin.php
@@ -14,7 +14,7 @@ function PromptForLoginBodyCallback(){
 				<?php Event('Auth Login Options'); ?>
 			</div>
 			<div class="col-xs-12">
-				<?php global $NUMBER_OF_QUERIES_RUN, $DEBUG; echo 'Runtime '.round(microtime(true)-$DEBUG[0]['timestamp'],4).' seconds. Ran '.$NUMBER_OF_QUERIES_RUN.' Queries.'; ?>
+				<?php global $NUMBER_OF_QUERIES_RUN,$NUMBER_OF_QUERIES_RUN_FROM_DISK_CACHE, $DEBUG; echo 'Runtime '.round(microtime(true)-$DEBUG[0]['timestamp'],4).' seconds. Ran '.$NUMBER_OF_QUERIES_RUN.' Database Queries. Ran '.$NUMBER_OF_QUERIES_RUN_FROM_DISK_CACHE.' Queries From Disk Cache.'; ?>
 			</div>
 		</div>
 	</div>
