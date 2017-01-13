019aecad92ca227a2c395819edbabc234f2779b5
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-13 01:27:42 -0800

    Update and rename view/main.php to core/showNoView.php

--- core/showNoView.php
+++ core/showNoView.php
@@ -1,7 +1,5 @@
 <?php
 
-include('loadViews.php');
-
 Hook('User Is Logged In - No Presentation','noView();');
 
 function noView(){
@@ -14,7 +12,7 @@ function noViewBodyCallback(){
   echo '<h1>404: Page not found.</h1>';
   echo 'User '.$_SESSION['User']['Email'].' is logged in, but nothing happened at this url.<br>';
   echo 'Runtime '.round(microtime(true)-$DEBUG[0]['timestamp'],4)." seconds.<br>";
-  echo 'Ran '.$NUMBER_OF_QUERIES_RUN.' Queries.<br>';
+  echo 'Ran '.$NUMBER_OF_QUERIES_RUN.' Database Queries.<br>';
   echo 'Ran '.$NUMBER_OF_QUERIES_RUN_FROM_DISK_CACHE.' Queries From Disk Cache.<br>';
   echo 'Session Expires '.date('r',$_SESSION['Auth']['Expires']).'.<br>';
   echo '<a href="/?logout">Log Out</a>.';
