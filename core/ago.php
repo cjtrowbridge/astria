9be80a56a5b91cb271b0efef4ba8f633e80341eb
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-13 00:48:27 -0800

    Create ago.php

---
+++ core/ago.php
@@ -0,0 +1,21 @@
+<?php 
+
+function ago($time){
+  $time=MaybeStrToTime($time);
+  if(($time==0)||(empty($time))){
+    return 'Never';
+  }
+  $periods = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
+  $lengths = array("60","60","24","7","4.35","12","10");
+  $now = time();
+  $difference     = $now - $time;
+  $tense         = "ago";
+  for($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
+    $difference /= $lengths[$j];
+  }
+  $difference = round($difference);
+  if($difference != 1) {
+    $periods[$j].= "s";
+  }
+  return "$difference $periods[$j] ago";
+}
