75371fd74ceb226cce898b8381844a05808a1b14
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-12 23:40:00 -0800

    Update PromptForLogin.php

--- core/PromptForLogin.php
+++ core/PromptForLogin.php
@@ -11,12 +11,15 @@ function PromptForLoginBodyCallback(){
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12">
-				<?php 
-					Event('Auth Login Options');
-				?>
+				<?php Event('Auth Login Options'); ?>
+			</div>
+			<div class="col-xs-12">
+				<?php echo 'Runtime '.round(microtime(true)-$DEBUG[0]['timestamp'],4)." seconds. 'Ran '.$NUMBER_OF_QUERIES_RUN.' Queries.'; ?>
 			</div>
 		</div>
 	</div>
 	
 	<?php
+	  
+
 }
