d56e05d25668b6fe587618558266978b2e54e4f7
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-13 00:22:03 -0800

    Create showArchitectDiskCache.php

---
+++ core/showArchitectDiskCache.php
@@ -0,0 +1,26 @@
+<?php 
+
+function showArchitectDiskCache(){
+  if(path(2)=='delete-all'){
+    //TODO
+  }
+  Hook('Template Body',"ArchitectDiskCache();");
+  TemplateBootstrap2('View Cache - Architect'); 
+}
+
+function ArchitectDiskCache(){
+  ?>
+  <h2>Disk Cache</h2>
+  <a href="/architect/disk-cache/delete-all">Delete All</a>
+  <?php
+    global $ASTRIA;
+    $path = 'cache/';
+    if ($handle = opendir($path)) {
+      while (false !== ($file = readdir($handle))) {
+        if(!(strpos($file, '.php')===false)){
+          $hash = rtrim($file,$.php');
+          echo '<a href="/architect/disk-cache/'.$hash.'">'.$hash.'</a> ('.(filesize($path.$file)/1024).'kb)<br>';
+        }
+      }
+    }
+}
