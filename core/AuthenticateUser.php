8820340cd2dfe46d27b76af24a5806dbd08ecf11
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-12 23:35:04 -0800

    Update AuthenticateUser.php

--- core/AuthenticateUser.php
+++ core/AuthenticateUser.php
@@ -48,6 +48,8 @@ function AuthenticateUser($email=null){
   //Cache the entire session to disk with the cookie's SessionHash as the key.
   writeDiskCache($SessionHash,$_SESSION);
   
-  header('Location: '.$_SERVER[REQUEST_URI]);
+  $header='Location: '.$_SERVER[REQUEST_URI];
+  pd($header);die();
+  header($header);
   
 }
