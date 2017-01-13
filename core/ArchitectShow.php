4e25fcaa0448b1fc3f27d7c23ba239836f6d8b52
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-13 01:34:21 -0800

    Update ArchitectShow.php

--- core/ArchitectShow.php
+++ core/ArchitectShow.php
@@ -25,7 +25,7 @@ function ArchitectBodyCallback(){
     <form class="form-inline">
       <button onclick="Cardify('Events','debugSummary');" type="button" class="btn btn-outline-warning">Events</button>
       <button onclick="Cardify('Hooks','hooks');" type="button" class="btn btn-outline-warning">Hooks</button>
-      <button onclick="document.location=/architect/disk-cache" type="button" class="btn btn-outline-warning">Cache</button>
+      <button onclick="document.location='/architect/disk-cache'" type="button" class="btn btn-outline-warning">Cache</button>
       <button onclick="Cardify('Queries','queriesRun');" type="button" class="btn btn-outline-warning">Queries</button>
       <button onclick="Cardify('Session','session');" type="button" class="btn btn-outline-warning">Session</button>
       <button onclick="Cardify('Databases','databases');" type="button" class="btn btn-outline-warning">Databases</button>
