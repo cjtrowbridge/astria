3e70f688154d213b6ec8392a51dd356d68219bfc
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-19 15:46:41 -0800

    Create ArchitectSchema.php

---
+++ core/ArchitectSchema.php
@@ -0,0 +1,49 @@
+<?php
+
+function showArchitectSchema(){
+  Hook('Template Body','ArchitectSchemaBodyCallback();');
+  TemplateBootstrap4('Schema | Architect'); 
+}
+  
+function ArchitectSchemaBodyCallback(){
+?>
+<h2>Schema Architect</h2>
+<div class="row">
+  <div class="col-xs-12">
+    <form class="form-inline">
+      <button onclick="Cardify('New Schema','newSchema');" type="button" class="btn btn-outline-warning">New Schema</button>
+    </form>
+  </div>
+</div><br>
+<div class="row">
+  <div class="hidden" id="newSchema">
+    <form action="/architect/schema/new" method="post">
+      <div class="form-group row">
+        <div class="col-xs-12">
+          What would you call one object in this schema?<br>
+          (Ie, Cats would be "Cat.")<br>
+          <input type="text" class="form-control" placeholder="Description" name="newSchemaObject" id="newSchemaObject">
+        </div>
+      </div>
+      <div class="form-group row">
+        <div class="col-xs-12">
+          <div class="form-check">
+            <label class="form-check-label">
+              <input class="form-check-input" type="checkbox" name="newSchemaVersioning" id="newSchemaVersioning" value="yes" checked="checked">
+              Track version and history for this Schema.
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="form-group row">
+        <div class="col-xs-12">
+          <button type="submit" class="btn btn-success">Create Schema</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+
+
+<?php
+}
