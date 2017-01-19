3c68dd4b3d637b1f4edb27b8fbf73476e63ad849
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-19 15:22:23 -0800

    Create createMembership.php

---
+++ core/createMembership.php
@@ -0,0 +1,15 @@
+<?php
+
+function createMembership($UserID,$GroupID){
+
+  if(
+    (intval($UserID)==0)||
+    (intval($GroupID)==0)
+  ){
+    //TODO make this more graceful
+    die('Invalid Group or User ID');
+  }
+  
+  Query("INSERT INTO `UserMembership` (`UserID`, `GroupID`) VALUES ('".intval($UserID)."', '".intval($GroupID)."');");
+  
+}
