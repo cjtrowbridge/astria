dda3501a158bfd05846cb56c912ccc6bd6107422
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-23 18:38:40 -0800

    Update and rename events/Event.php to core/Event.php

--- core/Event.php
+++ core/Event.php
@@ -1,5 +1,16 @@
 <?php
 
+global $EVENTS, $DEBUG;
+$EVENTS=array();
+$DEBUG=array(
+  0=>array(
+    'description'=> 'Startup',
+    'ram'=> (memory_get_usage()/1000000),
+    'runtime'=>0,
+    'timestamp'=> round(microtime(true),4)
+  )
+);
+
 function Event($EventDescription){
 
   global $EVENTS, $ASTRIA;
