3e6561996563ec5c30a7415f673c0bef743ed65d
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-31 10:29:29 -0800

    Update ArchitectEditHook.php

--- core/ArchitectEditHook.php
+++ core/ArchitectEditHook.php
@@ -2,14 +2,17 @@
 
 function showEditHook(){
   Hook('Template Body','EditHookBodyCallback();');
-  TemplateBootstrap4('Edit Hook| Architect'); 
+  TemplateBootstrap4('Edit Hook | Architect'); 
 
 }
 
 function EditHookBodyCallback(){
   ?>
   <h1>Edit Hook</h1>
-  
-  
   <?php
+  $HookID = intval(path(2));
+  if($HookID==0){echo '<p>Please Specify a HookID. For example /architect/edit-hook/1</p>';return;}
+  
+  $Hook = Query('SELECT * FROM Hook WHERE HookID = '.$HookID);
+  pd($Hook);
 }
