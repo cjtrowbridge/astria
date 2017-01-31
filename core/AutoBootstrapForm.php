06d64152a8e32e562e478d60a53c5125328f22bf
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-31 10:47:51 -0800

    Create AutoBootstrapForm.php

---
+++ core/AutoBootstrapForm.php
@@ -0,0 +1,22 @@
+<?php
+
+function AstriaBootstrapAutoForm($Array,$Action = '/', $Method = 'post'){
+  
+  $Return="\n\n<!--\nAstria Bootstrap AutoForm v1.0\n\nInput:\n".var_export($Array,true)."\n\n-->\n\n";
+  $Return .= "<form action=\"".$Action."\" method=\"".$Method."\">\n";
+  foreach($Array as $Key => $Value){
+    $Return .= "<div class=\"form-group row\">\n";
+    $Return .= "  <label class=\"col-xs-2 col-form-label\">".$Key.":</label>\n";
+    $Return .= "  <div class=\"col-xs-10\">\n";
+    $Return .= "    <input class=\"form-control astriaBootstrapFormInput\" type=\"text\" name=\"".$Key."\" id=\"".$Key."\" value=\"".$Value."\">\n";
+    $Return .= "  </div>\n"
+    $Return .= "</div>\n";
+  }
+  $Return .= "  <input class=\"form-control\" type=\"submit\">\n";
+  $Return .= "  <script>\n";
+  $Return .= "    $('.astriaBootstrapFormInput:first-of-type').focus();\n";
+  $Return .= "  </script>\n";
+  $Return .= "</form>\n\n";
+  
+  return $Return;
+}
