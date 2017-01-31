41e62d4b23b4c2d8fd9a4ea8b226271f892210dd
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-31 10:38:40 -0800

    Update ArchitectEditHook.php

--- core/ArchitectEditHook.php
+++ core/ArchitectEditHook.php
@@ -10,9 +10,25 @@ function EditHookBodyCallback(){
   ?>
   <h1>Edit Hook</h1>
   <?php
+  
+  //Get the HookID from the url and validate it as an integer
   $HookID = intval(path(2));
   if($HookID==0){echo '<p>Please Specify a HookID. For example /architect/edit-hook/1</p>';return;}
   
+  //Get it from the database and make sure it exists
   $Hook = Query('SELECT * FROM Hook WHERE HookID = '.$HookID);
-  pd($Hook);
+  if(!(isset($Hook[0]))){echo '<p>That hook was not found. :[</p>';return;}
+  
+  //Display the form
+  ?>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <?php echo AstriaBootstrapAutoForm($Hook); ?>
+      </div>
+    </div>
+</div>
+
+  <?php
 }
