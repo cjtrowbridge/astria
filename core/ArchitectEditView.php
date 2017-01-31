17733db7ac3d8ae76999a029d07a0f8852b1215e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-01-31 10:25:12 -0800

    Update ArchitectEditView.php

--- core/ArchitectEditView.php
+++ core/ArchitectEditView.php
@@ -73,7 +73,7 @@ function ArchitectEditViewBodyCallback(){
         </div>
         <div class="row">
           <div class="col-xs-12">
-            <h3>Event-Driven Functions (Hooks) Associated With This View <button type="button" class="btn btn-secondary btn-sm float-xs-right" onclick="window.open('/architect/edit-view/<?php echo path(2); ?>/new-hook/', '_blank');">New Hook</button></h3>
+            <h3>These Event-Driven Functions Are Associated With This View: <button type="button" class="btn btn-secondary btn-sm float-xs-right" onclick="window.open('/architect/edit-view/<?php echo path(2); ?>/new-hook/', '_blank');">New Hook</button></h3>
             <?php
               echo ArrTabler(Query("SELECT * FROM Hook WHERE ViewID = ".$View['ViewID']));
             ?>
