1545aa2436cb9755825e9b792e585090a751a2dc
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-02-01 21:33:57 -0800

    Update showNoView.php

--- core/showNoView.php
+++ core/showNoView.php
@@ -8,13 +8,13 @@ function noView(){
 }
 function noViewBodyCallback(){
   global $NUMBER_OF_QUERIES_RUN, $QUERIES_RUN, $DEBUG, $NUMBER_OF_QUERIES_RUN_FROM_DISK_CACHE;
-  //echo '<h1>404: Page not found.</h1> User '.$_SESSION['User']['Email'].' is logged in, but nothing happened at this url. Runtime '.round(microtime(true)-STARTTIME,4)." seconds. \n\n\n\n<!-- QUERIES RUN \n".addslashes($QUERIES_RUN)."-->\n\n\n\n".$NUMBER_OF_QUERIES_RUN.' Queries Run.</span> Session Expires '.date('r',$_SESSION['Auth']['Expires']).'. <a href="./?logout">Log Out</a>.';
+  //echo '<h1>404: Page not found.</h1> User '.$ASTRIA['Session']['User']['Email'].' is logged in, but nothing happened at this url. Runtime '.round(microtime(true)-STARTTIME,4)." seconds. \n\n\n\n<!-- QUERIES RUN \n".addslashes($QUERIES_RUN)."-->\n\n\n\n".$NUMBER_OF_QUERIES_RUN.' Queries Run.</span> Session Expires '.date('r',$ASTRIA['Session']['Auth']['Expires']).'. <a href="./?logout">Log Out</a>.';
   echo '<h1>404: Page not found.</h1>';
-  echo 'User '.$_SESSION['User']['Email'].' is logged in, but nothing happened at this url.<br>';
+  echo 'User '.$ASTRIA['Session']['User']['Email'].' is logged in, but nothing happened at this url.<br>';
   echo 'Runtime '.round(microtime(true)-$DEBUG[0]['timestamp'],4)." seconds.<br>";
   echo 'Ran '.$NUMBER_OF_QUERIES_RUN.' Database Queries.<br>';
   echo 'Ran '.$NUMBER_OF_QUERIES_RUN_FROM_DISK_CACHE.' Queries From Disk Cache.<br>';
-  echo 'Session Expires '.date('r',$_SESSION['Auth']['Expires']).'.<br>';
+  echo 'Session Expires '.date('r',$ASTRIA['Session']['Auth']['Expires']).'.<br>';
   echo '<a href="/?logout">Log Out</a>.';
-  //pd($_SESSION); 
+  //pd($ASTRIA['Session']); 
 }
