53b0a86d3b8aefbdc3af0b2fe77cf4cf93a86a4f
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-02-01 22:24:01 -0800

    Update VerifyAgentAndIP.php

--- core/VerifyAgentAndIP.php
+++ core/VerifyAgentAndIP.php
@@ -2,6 +2,8 @@
 
 function VerifyAgentAndIP(){
 	//Make sure that the user agent and ip have not changed and that the sessions is not expired
+	global $ASTRIA;
+	
 	if(!($ASTRIA['Session']['UserAgentHash']  == md5($_SERVER['HTTP_USER_AGENT']))){
 		LogOut();
 	}
