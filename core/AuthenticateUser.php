1c22446ba605e7e198dc7d829a92154514c654d4
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-02-01 22:38:58 -0800

    Update AuthenticateUser.php

--- core/AuthenticateUser.php
+++ core/AuthenticateUser.php
@@ -4,7 +4,7 @@ function AuthenticateUser($email=null){
   global $ASTRIA;
   
   //Validate and sanitize the email
-  if($email == null){die('Tried to cache invalid user.');}
+  if($email == null){die('Tried to authenticate invalid user.');}
   $cleanEmail=mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],$ASTRIA['Session']['google_oauth2']['user_object']->email);
   
   //Get all the user's profile info for the session
@@ -15,38 +15,41 @@ function AuthenticateUser($email=null){
   $ASTRIA['Session']['User']['Memberships']=GetAllGroups($ASTRIA['Session']['User']['UserID']);
   
   //Create a high-entropy hash to connect the cookie with the session
-  $SessionHash=mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],md5(uniqid(true)));
+  //$SessionHash=mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],md5(uniqid(true)));
   
   //Insert session into database
   $UserIDClean         = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],$User['UserID']);
-  $UserAgentHashClean  = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],md5($_SERVER['HTTP_USER_AGENT']));
-  $UserAgentClean      = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],$_SERVER['HTTP_USER_AGENT']);
-  $UserIPHashClean     = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],md5($_SERVER['REMOTE_ADDR']));
-  $UserIPClean         = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],$_SERVER['REMOTE_ADDR']);
-  $ExpiresTime         = (time()+$ASTRIA['app']['defaultSessionLength']);
-  $Expires             = date('Y-m-d H:i:s',$ExpiresTime);
+  //$UserAgentHashClean  = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],md5($_SERVER['HTTP_USER_AGENT']));
+  //$UserAgentClean      = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],$_SERVER['HTTP_USER_AGENT']);
+  //$UserIPHashClean     = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],md5($_SERVER['REMOTE_ADDR']));
+  //$UserIPClean         = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],$_SERVER['REMOTE_ADDR']);
+  //$ExpiresTime         = (time()+$ASTRIA['app']['defaultSessionLength']);
+  //$Expires             = date('Y-m-d H:i:s',$ExpiresTime);
+  //Query("
+    //INSERT INTO `Session` 
+    //(`SessionHash`, `UserID`, `UserAgentHash`, `UserAgent`, `UserIPHash`, `UserIP`, `Expires`) VALUES 
+    //('".$SessionHash."' , ".$UserIDClean.", '".$UserAgentHashClean."', '".$UserAgentClean."', '".$UserIPHashClean."', '".$UserIPClean."', '".$Expires."');
+  //");
   Query("
-    INSERT INTO `Session` 
-    (`SessionHash`, `UserID`, `UserAgentHash`, `UserAgent`, `UserIPHash`, `UserIP`, `Expires`) VALUES 
-    ('".$SessionHash."' , ".$UserIDClean.", '".$UserAgentHashClean."', '".$UserAgentClean."', '".$UserIPHashClean."', '".$UserIPClean."', '".$Expires."');
+    UPDATE `Session` SET `UserID` = '".$UserIDClean."' WHERE `Session`.`SessionHash` LIKE '".$ASTRIA['Session']['SessionHash']."';
   ");
   
   //Insert hash of user agent and IP into session for security checks
-  $ASTRIA['Session']['SessionHash']    = $SessionHash;
-  $ASTRIA['Session']['UserAgentHash']  = md5($_SERVER['HTTP_USER_AGENT']);
-  $ASTRIA['Session']['RemoteAddrHash'] = md5($_SERVER['REMOTE_ADDR']);
+  //$ASTRIA['Session']['SessionHash']    = $SessionHash;
+  //$ASTRIA['Session']['UserAgentHash']  = md5($_SERVER['HTTP_USER_AGENT']);
+  //$ASTRIA['Session']['RemoteAddrHash'] = md5($_SERVER['REMOTE_ADDR']);
   
   //Insert the session hash into a secure cookie
-  $CookieName=strtolower($ASTRIA['app']['appName']).'_'.md5($ASTRIA['app']['appURL']);
-  if (!setcookie($CookieName, $SessionHash, $ExpiresTime, '/', NULL, true,true)){
-    die('Could not set cookie.');
-  }
+  //$CookieName=strtolower($ASTRIA['app']['appName']).'_'.md5($ASTRIA['app']['appURL']);
+  //if (!setcookie($CookieName, $SessionHash, $ExpiresTime, '/', NULL, true,true)){
+    //die('Could not set cookie.');
+  //}
   
   //Update the expiration date
-  $ASTRIA['Session']['Auth']['Expires'] = $ExpiresTime;
+  //$ASTRIA['Session']['Auth']['Expires'] = $ExpiresTime;
   
-  //Cache the entire session to disk with the cookie's SessionHash as the key.
-  writeDiskCache($SessionHash,$ASTRIA['Session']);
+  //Cache the entire session
+  AstriaSessionSave();
   
   header('Location: /');
   
