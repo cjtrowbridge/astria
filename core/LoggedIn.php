346007ad28559c4005ff91a7e4a123821782f12e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-02-01 22:21:26 -0800

    Update LoggedIn.php

--- core/LoggedIn.php
+++ core/LoggedIn.php
@@ -90,15 +90,3 @@ function LoggedIn(){
   }
   
 }
-function VerifyAgentAndIP(){
-	//Make sure that the user agent and ip have not changed and that the sessions is not expired
-	if(!($ASTRIA['Session']['UserAgentHash']  == md5($_SERVER['HTTP_USER_AGENT']))){
-		LogOut();
-	}
-	if(!($ASTRIA['Session']['RemoteAddrHash'] == md5($_SERVER['REMOTE_ADDR']))){
-		LogOut();
-	}
-	if(!($ASTRIA['Session']['Auth']['Expires']>time())){
-		LogOut();
-	}
-}
