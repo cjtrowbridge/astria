340894f385fefbb2f771f7f29a4409dd33b75695
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-02-02 10:38:57 -0800

    Update showNoView.php

--- core/showNoView.php
+++ core/showNoView.php
@@ -7,7 +7,7 @@ function noView(){
   TemplateBootstrap4('404');
 }
 function noViewBodyCallback(){
-  global $NUMBER_OF_QUERIES_RUN, $QUERIES_RUN, $DEBUG, $NUMBER_OF_QUERIES_RUN_FROM_DISK_CACHE;
+  global $NUMBER_OF_QUERIES_RUN, $QUERIES_RUN, $DEBUG, $NUMBER_OF_QUERIES_RUN_FROM_DISK_CACHE,$ASTRIA;
   //echo '<h1>404: Page not found.</h1> User '.$ASTRIA['Session']['User']['Email'].' is logged in, but nothing happened at this url. Runtime '.round(microtime(true)-STARTTIME,4)." seconds. \n\n\n\n<!-- QUERIES RUN \n".addslashes($QUERIES_RUN)."-->\n\n\n\n".$NUMBER_OF_QUERIES_RUN.' Queries Run.</span> Session Expires '.date('r',$ASTRIA['Session']['Auth']['Expires']).'. <a href="./?logout">Log Out</a>.';
   echo '<h1>404: Page not found.</h1>';
   echo 'User '.$ASTRIA['Session']['User']['Email'].' is logged in, but nothing happened at this url.<br>';
