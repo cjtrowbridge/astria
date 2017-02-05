a55e1b03eb5d05f8e41962df1a0c85f29e6b5ee3
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-02-04 22:12:59 -0800

    Update TemplateBootstrap4.php

--- core/TemplateBootstrap4.php
+++ core/TemplateBootstrap4.php
@@ -74,7 +74,7 @@ function TemplateBootstrap4($title=''){
     <?php if(LoggedIn()){ ?>
     <ul class="nav navbar-nav float-xs-right">
       <li class="nav-item">
-        <a href="https://aboutme.google.com/" target="_blank"><img src="<?php echo $USER['Photo']; ?>"></a>
+        <a href="https://aboutme.google.com/" target="_blank"><img src="<?php echo $ASTRIA['Session']['User']['Photo']; ?>"></a>
       </li>
       <li class="nav-item">
         <a class="nav-link active" href="/logout">Log Out</a>
