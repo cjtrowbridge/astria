297d0276d0f038367504d1024c26605a2a7847a2
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-02-04 21:27:54 -0800

    Update AstriaFunctionEditor.php

--- core/AstriaFunctionEditor.php
+++ core/AstriaFunctionEditor.php
@@ -3,11 +3,11 @@
 function AstriaFunctionEditor($Contents,$FunctionName,$TextareaName){
   ?>
   
-  <p>&lt;?php </p>
-  <?p>function <?php echo $FunctionName; ?>(){</p>
+  <p>function <?php echo $FunctionName; ?>(){</p>
   <textarea class="AstriaFunctionEditor ready" name="<?php echo $TextareaName; ?>" id="<?php echo $TextareaName; ?>"><?php echo $Contents; ?></textarea>
   <p>}</p>
-  <p>?&gt;</p>
+  
   <script>AstriaFunctionEditor();</script>
+
   <?php
 }
