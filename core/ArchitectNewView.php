dbd0d674b7b3227d3316a9ac8dbda4d35b84da78
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-02-08 12:37:17 -0800

    Update ArchitectNewView.php

--- core/ArchitectNewView.php
+++ core/ArchitectNewView.php
@@ -18,4 +18,56 @@ function ArchitectNewView(){
     exit;
     
   }
+  
+  Hook('Template Body','ArchitectNewViewBodyCallback();');
+  TemplateBootstrap4('New View - Architect'); 
 }
+  
+function ArchitectNewViewBodyCallback(){
+?><h1>New View</h1>
+<div class="row">
+  <div class="col-xs-12">
+    <form action="/architect/new-view" method="post">
+      <div class="form-group row">
+        <div class="col-xs-12">
+          <input type="text" class="form-control" placeholder="View Name" name="newViewName" id="newViewName">
+        </div>
+      </div>
+      <div class="form-group row">
+        <div class="col-xs-12">
+          <input type="text" class="form-control" placeholder="Description" name="newViewDescription" id="newViewDescription">
+        </div>
+      </div>
+      <div class="form-group row">
+        <div class="col-xs-12">
+          <input type="text" class="form-control" placeholder="Slug" name="newViewSlug" id="newViewSlug">
+        </div>
+      </div>
+      <div class="form-group row">
+        <div class="col-xs-12">
+          Category:<br>
+          <select class="form-control" placeholder="Category" name="newViewCategory" id="newViewCategory">
+            <?php
+              MakeSureDBConnected();
+              $ViewCategories=Query("SELECT * FROM ViewCategory");
+              foreach($ViewCategories as $ViewCategory){
+            ?>
+            <option value="<?php echo $ViewCategory['ViewCategoryID']; ?>"><?php echo $ViewCategory['Name']; ?></option>
+            <?php 
+              }
+            ?>
+          </select>
+        </div>
+      </div>
+      
+      <div class="form-group row">
+        <div class="col-xs-12">
+          <button type="submit" class="btn btn-success">Create View</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+<?php
+}
+
