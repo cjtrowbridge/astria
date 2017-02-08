e15335bf0246447165f5778d27c063fb557c7f62
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-02-08 11:50:45 -0800

    Update ArchitectShowViewCategoryTree.php

--- core/ArchitectShowViewCategoryTree.php
+++ core/ArchitectShowViewCategoryTree.php
@@ -3,14 +3,37 @@
 function ArchitectShowViewCategoryTree(){
   MakeSureDBConnected();
   $ViewCategories = Query('SELECT * FROM `ViewCategory`');
+  $Output ="  <div class=\"css-treeview\">\n";
+  $Output.="    <ul>\n";
   foreach($ViewCategories as $ViewCategory){
     if($ViewCategory['ParentID'] == ''){
-      ArchitectShowViewCategoryTreeRootElement($ViewCategory,$ViewCategories);
+      $Output.=ArchitectShowViewCategoryTreeRootElement($ViewCategory,$ViewCategories);
     }
   }
-
+  $Output.="    </ul>\n";
+  $Output.="  </div>\n";
+  return $Output;
 }
 
-function ArchitectShowViewCategoryTreeRootElement($Element,$Elements){
-  echo $Element['Name'];
+function ReturnTreeElement($Element,$Elements){
+  $Output ="";
+  $DirectChildren = array();
+  foreach($Elements as $Child){
+   if($Child['ParentID']==$Element['ViewCategoryID']){
+     $DirectChildren[]=$Child;
+   }
+  }
+  
+  if(count($DirectChildren>0){
+    $Output.="      <li><label><input type=\"checkbox\" checked=\"checked\" />".$Element['Name']."</label>\n";
+    $Output.="        <ul>\n";    
+    foreach($DirectChildren as $DirectChild){
+      $Output.= ReturnTreeElement($DirectChild,$Elements);
+    }
+    $Output.="        </ul>\n";
+  }else{
+    $Output.="          <li><a href=\"./\">".$Element['Name']."</a></li>\n";
+  }
+  $Output.="      </li>\n";
+  return $Output;
 };
