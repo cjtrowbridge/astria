76aca2940416fa4e325311e726da148a4ca2daf2
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-02-28 21:07:48 -0800

    Update VerifyAgentAndIP.php

--- core/VerifyAgentAndIP.php
+++ core/VerifyAgentAndIP.php
@@ -5,21 +5,21 @@ function VerifyAgentAndIP(){
 	global $ASTRIA;
 	
 	if(!($ASTRIA['Session']['UserAgentHash']  == md5($_SERVER['HTTP_USER_AGENT']))){
-		if(true||isset($_GET['verbose'])){
+		if(isset($_GET['verbose'])){
 			echo $ASTRIA['Session']['UserAgentHash'].'<br><br> DOES NOT MATCH<br><br>'.$_SERVER['HTTP_USER_AGENT'];
 			exit;
 		}
-		AstriaSessionDestroy();
+		//AstriaSessionDestroy();
 	}
 	if(!($ASTRIA['Session']['RemoteAddrHash'] == md5($_SERVER['REMOTE_ADDR']))){
-		if(true||isset($_GET['verbose'])){
+		if(isset($_GET['verbose'])){
 			echo $ASTRIA['Session']['RemoteAddrHash'].'<br><br> DOES NOT MATCH<br><br>'.md5($_SERVER['REMOTE_ADDR']);
 			exit;
 		}
 		AstriaSessionDestroy();
 	}
 	if(!($ASTRIA['Session']['Expires']>time())){
-		if(true||isset($_GET['verbose'])){
+		if(isset($_GET['verbose'])){
 			echo 'Session expired';
 			exit;
 		}
