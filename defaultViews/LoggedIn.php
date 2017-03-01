56c83378446d10712f7e8b29727620d496f4dbbb
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-02-28 20:34:50 -0800

    Update LoggedIn.php

--- defaultViews/LoggedIn.php
+++ defaultViews/LoggedIn.php
@@ -11,16 +11,6 @@ function defaultViewsHomepage(){
 }
 function defaultViewsHomepageBodyCallback(){
   global $NUMBER_OF_QUERIES_RUN, $QUERIES_RUN, $DEBUG, $NUMBER_OF_QUERIES_RUN_FROM_DISK_CACHE,$ASTRIA;
-  //echo '<h1>404: Page not found.</h1> User '.$ASTRIA['Session']['User']['Email'].' is logged in, but nothing happened at this url. Runtime '.round(microtime(true)-STARTTIME,4)." seconds. \n\n\n\n<!-- QUERIES RUN \n".addslashes($QUERIES_RUN)."-->\n\n\n\n".$NUMBER_OF_QUERIES_RUN.' Queries Run.</span> Session Expires '.date('r',$ASTRIA['Session']['Auth']['Expires']).'. <a href="./?logout">Log Out</a>.';
-  echo '<h1>404: Page not found.</h1>';
-  echo 'User '.$ASTRIA['Session']['User']['Email'].' is logged in, but nothing happened at this url.<br>';
-  echo 'Runtime '.round(microtime(true)-$DEBUG[0]['timestamp'],4)." seconds.<br>";
-  echo 'Ran '.$NUMBER_OF_QUERIES_RUN.' Database Queries.<br>';
-  echo 'Ran '.$NUMBER_OF_QUERIES_RUN_FROM_DISK_CACHE.' Queries From Disk Cache.<br>';
-  echo 'Session Expires '.date('r',$ASTRIA['Session']['Auth']['Expires']).'.<br>';
-  echo '<a href="/?logout">Log Out</a>.';
-  //echo '<h4>Session:</h4>';
-  //pd($ASTRIA['Session']); 
-  //echo '<h4>User:</h4>';
-  //pd($ASTRIA['Session']['User']); 
+  echo '<h1>Welcome To Glorious Astria.io</h1>';
+  
 }
