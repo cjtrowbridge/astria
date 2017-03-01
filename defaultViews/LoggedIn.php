8ef7f7d0bc99b81da3fa40d15fccfd12da9558cf
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-02-28 20:54:42 -0800

    Update LoggedIn.php

--- defaultViews/LoggedIn.php
+++ defaultViews/LoggedIn.php
@@ -1,8 +1,12 @@
 <?php
 
-if(path(0)===false){
-  Hook('Template Body','defaultViewsHomepageBodyCallback();');
-  TemplateBootstrap4('Default Home Page');
+Hook('User Is Logged In - Before Presentation','MaybeDefaultHomepage();');
+
+function MaybeDefaultHomepage(){
+  if(path(0)===false){
+    Hook('Template Body','defaultViewsHomepageBodyCallback();');
+    TemplateBootstrap4('Default Home Page');
+  }
 }
 function defaultViewsHomepageBodyCallback(){
   echo '<h1>Welcome To Glorious Astria.io</h1>';
