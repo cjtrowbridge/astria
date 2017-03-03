c7f35d450c80c2aa4b439893dab2a215915a5413
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-03 09:21:57 -0800

    Update Cache.php

--- core/Cache.php
+++ core/Cache.php
@@ -4,6 +4,7 @@
 define('CACHE_FILE_PREFIX','<?php /* ');
 define('CACHE_FILE_SUFFIX',' */ header("HTTP/1.1 301 Moved Permanently");header("Location: /");');
 define('CACHE_FILE_TTL',60*60*24*7);
+define('CACHE_DATABASE_TTL',60*60*24*7);
 
 function ReadCache($Hash,$TTL){
   //include_once('DiskCache.php');
