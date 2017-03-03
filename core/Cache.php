fa9e295b77d6ad2ee8b70721f0f640a7d948f443
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-03 09:19:42 -0800

    Update Cache.php

--- core/Cache.php
+++ core/Cache.php
@@ -6,17 +6,23 @@ define('CACHE_FILE_SUFFIX',' */ header("HTTP/1.1 301 Moved Permanently");header(
 define('CACHE_FILE_TTL',60*60*24*7);
 
 function ReadCache($Hash,$TTL){
-  include_once('DiskCache.php');
-  return readDiskCache($Hash,$TTL);
+  //include_once('DiskCache.php');
+  //return readDiskCache($Hash,$TTL);
+  include_once('CacheDatabase.php');
+  CacheDatabaseRead($Hash,$TTL);
 }
 
 function WriteCache($Hash,$Value){
-  include_once('DiskCache.php');
-  return writeDiskCache($Hash,$Value);
+  //include_once('DiskCache.php');
+  //return writeDiskCache($Hash,$Value);
+  include_once('CacheDatabase.php');
+  CacheDatabaseWrite($Hash,$TTL);
 }
 
 function DeleteCache($Hash){
-  include_once('DiskCache.php');
-  return deleteDiskCache($Hash);
+  //include_once('DiskCache.php');
+  //return deleteDiskCache($Hash);
+  include_once('CacheDatabase.php');
+  CacheDatabaseDelete($Hash,$TTL);
 }
 
