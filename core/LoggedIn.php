b02bff9a06172aa0d5e02e69c72913ad185bf14e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-03 08:56:30 -0800

    Update LoggedIn.php

--- core/LoggedIn.php
+++ core/LoggedIn.php
@@ -41,7 +41,7 @@ function LoggedIn(){
 	}else{
 	  
     //we need to attempt to authorize the user. one of the called hooks should change the 'Logged In' to true if it was able to authorize the user.
-    Event('Attempt Auth');exit;
+    Event('Attempt Auth');
     $ASTRIA['Session']['Auth']['Already Attempted']=true;
 	  
     //check whether we were sucesful in authorizing the user
