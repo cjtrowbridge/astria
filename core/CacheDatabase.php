a6eb34676737e017b7e3d2062164d15950ed103e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-03 09:02:47 -0800

    Create CacheDatabase.php

---
+++ core/CacheDatabase.php
@@ -0,0 +1,97 @@
+<?php
+
+
+define('DISK_CACHE_FILE_PREFIX','<?php /* ');
+define('DISK_CACHE_FILE_SUFFIX',' */ header("HTTP/1.1 301 Moved Permanently");header("Location: /");');
+define('DISKCACHETTL',60*60*24*7);
+
+function deleteDiskCache($hash){
+  include_once('core/isValidMd5.php');
+  if(!(isValidMd5($hash))){
+    return false;
+  }
+  $path='cache/'.$hash.'.php';
+  
+  if(!(file_exists($path))){
+    return false;
+  }
+  unlink($path);
+  return true;
+}
+  
+function writeDiskCache($hash,$value){
+  include_once('core/isValidMd5.php');
+  include_once('core/Blowfish.php');
+  if(!(isValidMd5($hash))){
+    return false;
+  }
+  
+  $value=serialize($value);
+  
+  $value=BlowfishEncrypt($value);
+  $value=DISK_CACHE_FILE_PREFIX.$value.DISK_CACHE_FILE_SUFFIX;
+  
+  return file_put_contents('cache/'.$hash.'.php',$value);
+
+}
+
+function readDiskCache($hash,$ttl = DISKCACHETTL){
+  include_once('core/isValidMd5.php');
+  include_once('core/Blowfish.php');
+  if(!(isValidMd5($hash))){
+    return false;
+  }
+  
+  $path='cache/'.$hash.'.php';
+  
+  if(!(file_exists($path))){
+    return false;
+  }
+  
+  if((filemtime($path)+$ttl)<time()){
+    unlink($path);
+    return false;
+  }
+  
+  $value=file_get_contents($path);
+  if($value==false){
+    return false; 
+  }
+  $value=ltrim($value,DISK_CACHE_FILE_PREFIX);
+  $value=rtrim($value,DISK_CACHE_FILE_SUFFIX);
+  
+  $value=BlowfishDecrypt($value);
+  
+  $return=unserialize($value);
+  
+  global $NUMBER_OF_QUERIES_RUN_FROM_DISK_CACHE;
+  $NUMBER_OF_QUERIES_RUN_FROM_DISK_CACHE+=1;
+  
+  return $return;
+}
+
+function DiskCacheCleanup(){
+  global $ASTRIA;
+  $path = 'cache/';
+  if ($handle = opendir($path)) {
+    while (false !== ($file = readdir($handle))) {
+      if ((time()-filectime($path.$file)) > DISKCACHETTL){  
+        if(!(strpos($file, '.php')===false)){
+          unlink($path.$file);
+        }
+      }
+    }
+  }
+}
+
+function DiskCacheExists($hash){
+ if(!(isValidMd5($hash))){
+    return false;
+  }
+  $path='cache/'.$hash.'.php';
+  
+  if(!(file_exists($path))){
+    return false;
+  }
+  return true;
+}
