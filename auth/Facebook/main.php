697d3c57c652ab733212470cb4eecf87c30ce260
Christopher Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-07 10:49:45 -0800

    Update main.php

--- auth/Facebook/main.php
+++ auth/Facebook/main.php
@@ -19,65 +19,68 @@ function AttemptFacebookAuth(){
     'app_secret' => $ASTRIA['oauth']['Facebook']['FacebookOAuth2AppSecret'],
     'default_graph_version' => 'v2.2',
   ]);
-
-  $helper = $fb->getRedirectLoginHelper();
-
-  try {
-    $accessToken = $helper->getAccessToken();
-  } catch(Facebook\Exceptions\FacebookResponseException $e) {
-    // When Graph returns an error
-    echo 'Graph returned an error: ' . $e->getMessage();
-    exit;
-  } catch(Facebook\Exceptions\FacebookSDKException $e) {
-    // When validation fails or other local issues
-    echo 'Facebook SDK returned an error: ' . $e->getMessage();
-    exit;
-  }
-
-  if (! isset($accessToken)) {
-    if ($helper->getError()) {
-      header('HTTP/1.0 401 Unauthorized');
-      echo "Error: " . $helper->getError() . "\n";
-      echo "Error Code: " . $helper->getErrorCode() . "\n";
-      echo "Error Reason: " . $helper->getErrorReason() . "\n";
-      echo "Error Description: " . $helper->getErrorDescription() . "\n";
-    } else {
-      header('HTTP/1.0 400 Bad Request');
-      echo 'Bad request';
-    }
-    exit;
-  }
-
-  // Logged in
-  echo '<h3>Access Token</h3>';
-  var_dump($accessToken->getValue());
-
-  // The OAuth 2.0 client handler helps us manage access tokens
-  $oAuth2Client = $fb->getOAuth2Client();
-
-  // Get the access token metadata from /debug_token
-  $tokenMetadata = $oAuth2Client->debugToken($accessToken);
-  echo '<h3>Metadata</h3>';
-  var_dump($tokenMetadata);
-
-  // Validation (these will throw FacebookSDKException's when they fail)
-  $tokenMetadata->validateAppId($ASTRIA['oauth']['Facebook']['FacebookOAuth2AppID']); // Replace {app-id} with your app id
-  // If you know the user ID this access token belongs to, you can validate it here
-  //$tokenMetadata->validateUserId('123');
-  $tokenMetadata->validateExpiration();
   
+  if(isset($_GET['facebookAuth'])){
 
-  if (! $accessToken->isLongLived()) {
-    // Exchanges a short-lived access token for a long-lived one
+    $helper = $fb->getRedirectLoginHelper();
+
     try {
-      $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
-    } catch (Facebook\Exceptions\FacebookSDKException $e) {
-      echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
+      $accessToken = $helper->getAccessToken();
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
+      // When Graph returns an error
+      echo 'Graph returned an error: ' . $e->getMessage();
+      exit;
+    } catch(Facebook\Exceptions\FacebookSDKException $e) {
+      // When validation fails or other local issues
+      echo 'Facebook SDK returned an error: ' . $e->getMessage();
       exit;
     }
 
-    //echo '<h3>Long-lived</h3>';
-    //var_dump($accessToken->getValue());
+    if (! isset($accessToken)) {
+      if ($helper->getError()) {
+        header('HTTP/1.0 401 Unauthorized');
+        echo "Error: " . $helper->getError() . "\n";
+        echo "Error Code: " . $helper->getErrorCode() . "\n";
+        echo "Error Reason: " . $helper->getErrorReason() . "\n";
+        echo "Error Description: " . $helper->getErrorDescription() . "\n";
+      } else {
+        header('HTTP/1.0 400 Bad Request');
+        echo 'Bad request';
+      }
+      exit;
+    }
+
+    // Logged in
+    echo '<h3>Access Token</h3>';
+    var_dump($accessToken->getValue());
+
+    // The OAuth 2.0 client handler helps us manage access tokens
+    $oAuth2Client = $fb->getOAuth2Client();
+
+    // Get the access token metadata from /debug_token
+    $tokenMetadata = $oAuth2Client->debugToken($accessToken);
+    echo '<h3>Metadata</h3>';
+    var_dump($tokenMetadata);
+
+    // Validation (these will throw FacebookSDKException's when they fail)
+    $tokenMetadata->validateAppId($ASTRIA['oauth']['Facebook']['FacebookOAuth2AppID']); // Replace {app-id} with your app id
+    // If you know the user ID this access token belongs to, you can validate it here
+    //$tokenMetadata->validateUserId('123');
+    $tokenMetadata->validateExpiration();
+
+
+    if (! $accessToken->isLongLived()) {
+      // Exchanges a short-lived access token for a long-lived one
+      try {
+        $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+      } catch (Facebook\Exceptions\FacebookSDKException $e) {
+        echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
+        exit;
+    }
+
+    echo '<h3>Long-lived</h3>';
+    var_dump($accessToken->getValue());
+      
   }
   
   $_SESSION['fb_access_token'] = (string) $accessToken;
@@ -102,24 +105,17 @@ function AttemptFacebookAuth(){
   echo 'Logged in as ' . $me->getName();
   pd($me);
   
+  exit;
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  }
   
   //make a login link
   $helper = $fb->getRedirectLoginHelper();
 
   $permissions = ['email']; // Optional permissions
-  $loginUrl = $helper->getLoginUrl('https://example.com/fb-callback.php', $permissions);
+  $loginUrl = $helper->getLoginUrl('https://astria.io/?facebookAuth', $permissions);
 
-  echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+  $ASTRIA['Session']['facebook_oauth2']['auth_url']=htmlspecialchars($loginUrl)'
   
 
 /*
