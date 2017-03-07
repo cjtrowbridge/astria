acf7122080ca9a8065324a05e157860d16b09bcd
Christopher Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-07 10:38:06 -0800

    Update and rename core/PromptForLogin.php to defaultViews/PromptForLogin.php

--- defaultViews/PromptForLogin.php
+++ defaultViews/PromptForLogin.php
@@ -1,8 +1,9 @@
 <?php 
 
+Hook('User Is Not Logged In','PromptForLogin();');
+
 function PromptForLogin(){
-	Hook('Template Body','PromptForLoginBodyCallback();');
-	TemplateBootstrap4();
+	TemplateBootstrap4('Please Log In','PromptForLoginBodyCallback();');
 }
 
 function PromptForLoginBodyCallback(){
@@ -11,6 +12,7 @@ function PromptForLoginBodyCallback(){
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12">
+				<h1>Please Log In</h1>
 				<?php Event('Auth Login Options'); ?>
 			</div>
 		</div>
