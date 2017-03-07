8547fba6fb8fd5bbf7da58926350e6a9ae3b074b
Christopher Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-07 09:12:29 -0800

    Update GithubAttribution.php

--- core/GithubAttribution.php
+++ core/GithubAttribution.php
@@ -1,11 +1,15 @@
 <?php
 
 /*
+
   Astria Github Attribution
   
-  Feel free to comment or remove this, or to leave it in! ;P
+  This adds a link to the Astria Github to the navbar at the top of the page. Feel free to comment or remove this, or to leave it in!
+  
 */
+Hook('User Is Logged In - Before Presentation','AstriaGithubAttribution();');
 
-
-global $ASTRIA;
-$ASTRIA['nav']['main']['Github']='https://github.com/cjtrowbridge/astria';
+function AstriaGithubAttribution(){
+  global $ASTRIA;
+  $ASTRIA['nav']['main']['Github']='https://github.com/cjtrowbridge/astria';
+}
