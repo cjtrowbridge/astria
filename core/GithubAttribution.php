0213782d8b7027ca5b7df69b1797dd92761a59ae
Christopher Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-07 09:10:19 -0800

    Create GithubAttribution.php

---
+++ core/GithubAttribution.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+  Astria Github Attribution
+  
+  Feel free to comment or remove this, or to leave it in! ;P
+*/
+
+
+global $ASTRIA;
+$ASTRIA['nav']['main']['Github']='https://github.com/cjtrowbridge/astria';
