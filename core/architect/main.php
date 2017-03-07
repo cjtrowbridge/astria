5eed3b2b81b37530f69f57d4b231e1629d8db9c4
Christopher Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-07 09:54:50 -0800

    Update main.php

--- core/architect/main.php
+++ core/architect/main.php
@@ -1,28 +1,20 @@
 <?php
 
-include_once('Show.php');
-
-global $ASTRIA;
-if(isset($ASTRIA['Session'])){
-  if(isset($ASTRIA['Session']['User'])){
-    if(isset($ASTRIA['Session']['User']['Memberships'])){ //TODO abstract this better
-      if(isset($ASTRIA['Session']['User']['Memberships'][2])){
-        if(($ASTRIA['Session']['User']['Memberships'][2]==2)){
-          
-          Hook('User Is Logged In - Before Presentation','prepareArchitect();');
-
-          $ASTRIA['nav']['main']=array(
-            'Architect' => '/architect'
-          );
-        }
-      }
-    }
-  }
+if(HasMembership(2)){
+  
+  Hook('User Is Logged In - Before Presentation','prepareArchitect();');
+  
+  global $ASTRIA;
+  $ASTRIA['nav']['main']=array(
+    'Architect' => '/architect'
+  );
+  
 }
   
 
 function prepareArchitect(){
   if(path(0)=='architect'){
+    include_once('Show.php');
     switch(path(1)){
       case 'user':
         switch(path(2)){
