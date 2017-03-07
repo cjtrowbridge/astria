7e99d334f8933d437b713614be1f879b5fbb81e2
Christopher Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-07 09:52:41 -0800

    Update HasMembership.php

--- core/HasMembership.php
+++ core/HasMembership.php
@@ -1,9 +1,23 @@
 <?php
 
 function HasMembership($UserID,$Group){
-  
+
   //TODO add something to look up the id if a group name is passed
   $GroupID=intval($Group);
+  
+  if(!(isset($ASTRIA['Session']))){
+    return false;
+  }
+  if(!(isset($ASTRIA['Session']['User']))){
+    return false;
+  }
+  if(!(isset($ASTRIA['Session']['User']['Memberships']))){
+    return false;
+  }
+  if(!(isset($ASTRIA['Session']['User']['Memberships'][$GroupID]))){
+    return false;
+  }
+  
   
   if(($ASTRIA['Session']['User']['Memberships'][$GroupID]==$GroupID)){
     return true;
