74406f10d811891f688eaacbfea4198fa6ae4508
Christopher Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-07 09:33:32 -0800

    Update main.php

--- auth/Facebook/main.php
+++ auth/Facebook/main.php
@@ -12,11 +12,117 @@ function AttemptFacebookAuth(){
   include_once('auth/Facebook/src/Facebook/autoload.php');
   global $ASTRIA;
   
+  //$client->setRedirectUri($ASTRIA['app']['appURL']);
+  
+  $fb = new Facebook\Facebook([
+    'app_id' => $ASTRIA['oauth']['Facebook']['FacebookOAuth2AppID'],
+    'app_secret' => $ASTRIA['oauth']['Facebook']['FacebookOAuth2AppSecret'],
+    'default_graph_version' => 'v2.2',
+  ]);
 
-  $client = new Google_Client();
-  $client->setClientId($ASTRIA['oauth']['Facebook']['FacebookOAuth2ClientID']);
-  $client->setClientSecret($ASTRIA['oauth']['Facebook']['FacebookOAuth2ClientSecret']);
-  $client->setRedirectUri($ASTRIA['app']['appURL']);
+  $helper = $fb->getRedirectLoginHelper();
+
+  try {
+    $accessToken = $helper->getAccessToken();
+  } catch(Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+  } catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+  }
+
+  if (! isset($accessToken)) {
+    if ($helper->getError()) {
+      header('HTTP/1.0 401 Unauthorized');
+      echo "Error: " . $helper->getError() . "\n";
+      echo "Error Code: " . $helper->getErrorCode() . "\n";
+      echo "Error Reason: " . $helper->getErrorReason() . "\n";
+      echo "Error Description: " . $helper->getErrorDescription() . "\n";
+    } else {
+      header('HTTP/1.0 400 Bad Request');
+      echo 'Bad request';
+    }
+    exit;
+  }
+
+  // Logged in
+  echo '<h3>Access Token</h3>';
+  var_dump($accessToken->getValue());
+
+  // The OAuth 2.0 client handler helps us manage access tokens
+  $oAuth2Client = $fb->getOAuth2Client();
+
+  // Get the access token metadata from /debug_token
+  $tokenMetadata = $oAuth2Client->debugToken($accessToken);
+  echo '<h3>Metadata</h3>';
+  var_dump($tokenMetadata);
+
+  // Validation (these will throw FacebookSDKException's when they fail)
+  $tokenMetadata->validateAppId({app-id}); // Replace {app-id} with your app id
+  // If you know the user ID this access token belongs to, you can validate it here
+  //$tokenMetadata->validateUserId('123');
+  $tokenMetadata->validateExpiration();
+  
+
+  if (! $accessToken->isLongLived()) {
+    // Exchanges a short-lived access token for a long-lived one
+    try {
+      $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+    } catch (Facebook\Exceptions\FacebookSDKException $e) {
+      echo "<p>Error getting long-lived access token: " . $helper->getMessage() . "</p>\n\n";
+      exit;
+    }
+
+    //echo '<h3>Long-lived</h3>';
+    //var_dump($accessToken->getValue());
+  }
+  
+  $_SESSION['fb_access_token'] = (string) $accessToken;
+  
+  //Try getting the user's info
+  
+  try {
+    // Get the Facebook\GraphNodes\GraphUser object for the current user.
+    // If you provided a 'default_access_token', the '{access-token}' is optional.
+    $response = $fb->get('/me', $accessToken);
+  } catch(Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+  } catch(Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+  }
+
+  $me = $response->getGraphUser();
+  echo 'Logged in as ' . $me->getName();
+  pd($me);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  //make a login link
+  $helper = $fb->getRedirectLoginHelper();
+
+  $permissions = ['email']; // Optional permissions
+  $loginUrl = $helper->getLoginUrl('https://example.com/fb-callback.php', $permissions);
+
+  echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+  
+
+/*
   
   if(isset($_GET['code'])){
     $client->authenticate($_GET['code']);
@@ -70,7 +176,7 @@ function AttemptFacebookAuth(){
     $ASTRIA['Session']['facebook_oauth2']=array('auth_url'=>$authUrl);
     AstriaSessionSave();
   }
-  
+  */
 }
 Hook('Auth Login Options','authFacebookCallback();');
 function authFacebookCallback(){
