4c5a39d1eed7b4cda1937abad8b47592d4145cb0
Christopher Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-09 10:22:38 -0800

    Update main.php

--- auth/Facebook/main.php
+++ auth/Facebook/main.php
@@ -6,6 +6,10 @@
   
 */
 
+//Astria does not use PHP sessions because they are less scalable and fault-tolerant than Astria's hybrid cached sessions, but Facebook's SDK needs them.
+session_start();
+
+
 Hook('Attempt Auth','AttemptFacebookAuth();');
 function AttemptFacebookAuth(){
   if(isset($_GET['verbose'])){
