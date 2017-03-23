a15f7c991bf7c8be6f85197b64806f27328c1485
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-23 10:12:28 -0700

    Create SignupEmailConfirmation.php

---
+++ auth/Email/SignupEmailConfirmation.php
@@ -0,0 +1,16 @@
+<?php
+
+Hook('User Is Logged In - Before Presentation','SignupEmailConfirmation();');
+function SignupEmailConfirmation(){
+  if(path(0)==='signupemailconfirmation'){
+    Hook('Template Body','SignupEmailConfirmationBodyCallback();');
+    TemplateBootstrap4('Thanks For Signing Up!');
+  }
+}
+
+
+function SignupEmailConfirmationBodyCallback(){
+  ?><h1>Thanks For Signing Up!</h1>
+  <p>Check your email for the confirmation link to complete the signup process.</p>
+  <?php
+}
