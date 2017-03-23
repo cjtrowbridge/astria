40542316fd17c37cdae9f092c8b40d63b45e7944
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-23 09:44:08 -0700

    Update main.php

--- auth/Google/main.php
+++ auth/Google/main.php
@@ -102,6 +102,12 @@ Hook('Auth Login Options','authGoogleCallback();');
 function authGoogleCallback(){
   global $ASTRIA;
   ?>
-    <p><a class="loginButton" href="<?php echo $ASTRIA['Session']['google_oauth2']['auth_url']; ?>"><img src="/img/google-login-button.png" alt="Login with Google" /></a></p>
+
+  <div class="row">
+    <div class="col-xs-12 text-center">
+      <a class="loginButton" href="<?php echo $ASTRIA['Session']['google_oauth2']['auth_url']; ?>"><img src="/img/google-login-button.png" alt="Login with Google" /></a>
+    </div>
+  </div>
+
   <?php
 }
