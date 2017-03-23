112a22408492ec60ebb066880cd01f0bf2fb8553
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-23 11:13:12 -0700

    Update SignupEmailUser.php

--- auth/Email/SignupEmailUser.php
+++ auth/Email/SignupEmailUser.php
@@ -1,5 +1,32 @@
 <?php
 
 function SignupEmailUser(){
-
+  
+  $Hash  = sha2(uniqid(true));
+  $Email = $_POST['signupEmail'];
+  
+  $Hash  = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],$Hash);
+  $Email = mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],$Email);
+  
+  $Existing = Query("SELECT UserID FROM `User` WHERE Email LIKE '".$Email."'");
+  
+  if(count($Existing)==0){
+  
+    //New User
+    Query("
+      INSERT INTO `User` 
+        (`Email`, `PasswordExpires`, `LoginHash`) 
+      VALUES 
+        ('".$Email."', NOW(), '".$Hash."')
+    ");
+    
+    
+  }else{
+    
+    //Existing User
+    Query("UPDATE `User` SET `LoginHash` = '".$Hash."' WHERE `Email` LIKE ".$Existing[0]['Email'].";");
+    
+    
+  }
+  
 }
