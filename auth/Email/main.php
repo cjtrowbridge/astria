941fd2da8c4820f5add7adceccd7245ad04830e9
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-23 10:13:02 -0700

    Update main.php

--- auth/Email/main.php
+++ auth/Email/main.php
@@ -3,10 +3,22 @@
 Hook('Attempt Auth','AttemptEmailAuth();');
 function AttemptEmailAuth(){
   include_once('core/Session.php');
-  include_once('auth/Google/autoload.php');
+  include_once('core/SignupEmailConfirmation.php');
   global $ASTRIA;
   Event('Starting Email Auth Check');
-
+  if(
+    (path(0)=='authEmail')&&
+    isset($_POST['loginEmail'])
+  ){
+    include_once('auth/Email/ConfirmEmailUser.php');
+    ConfirmEmailUser();
+    
+  }elseif(isset($_POST['signupEmail'])){
+    include_once('auth/Email/SignupEmailUser.php');
+    SignupEmailUser($_POST['signupEmail']);
+    header('Location: /signupemailconfirmation');
+    exit;
+  }
 }
 
 Hook('Auth Login Options','authEmailCallback();');
@@ -17,17 +29,17 @@ function authEmailCallback(){
       <div class="col-xs-12 col-md-6">
         <h4>Log In By Email</h4>
         <div class="container">
-          <form>
+          <form action="/authEmail" method="post">
             <div class="form-group row">
-              <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+              <label for="loginEmail" class="col-sm-2 col-form-label">Email</label>
               <div class="col-sm-10">
-                <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                <input type="email" class="form-control" id="loginEmail" name="loginEmail" placeholder="Email">
               </div>
             </div>
             <div class="form-group row">
-              <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+              <label for="loginPassword" class="col-sm-2 col-form-label">Password</label>
               <div class="col-sm-10">
-                <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                <input type="password" class="form-control" id="loginPassword" name="loginPassword" placeholder="Password">
               </div>
             </div>
             <div class="form-group row">
@@ -41,11 +53,11 @@ function authEmailCallback(){
       <div class="col-xs-12 col-md-6">
         <h4>Sign Up By Email</h4>
         <div class="container">
-          <form>
+          <form action="/authEmail" method="post">
             <div class="form-group row">
-              <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+              <label for="signupEmail" class="col-sm-2 col-form-label">Email</label>
               <div class="col-sm-10">
-                <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                <input type="email" class="form-control" id="signupEmail" name="signupEmail" placeholder="Email">
               </div>
             </div>
             <div class="form-group row">
