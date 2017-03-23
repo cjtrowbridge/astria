6261b66d4b9763a046544e99909b8752a384f6b5
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-23 10:23:03 -0700

    Update SignupEmailConfirmation.php

--- auth/Email/SignupEmailConfirmation.php
+++ auth/Email/SignupEmailConfirmation.php
@@ -1,13 +1,12 @@
-wat<?php
+<?php
 
-Hook('User Is Logged In - Before Presentation','SignupEmailConfirmation();');
+Hook('User Is Not Logged In','SignupEmailConfirmation();');
 function SignupEmailConfirmation(){
   if(path(0)=='signupemailconfirmation'){
     Hook('Template Body','SignupEmailConfirmationBodyCallback();');
     TemplateBootstrap4('Thanks For Signing Up!');
   }else{echo 'no';}
 }
-
 
 function SignupEmailConfirmationBodyCallback(){
   ?><h1>Thanks For Signing Up!</h1>
