14fdec8860747c2af8a3dfd19c37ffb15d6aaf98
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-28 09:47:34 -0700

    Update main.php

--- auth/Email/main.php
+++ auth/Email/main.php
@@ -55,13 +55,13 @@ function authEmailCallback(){
         <h4>Log In Or Sign Up By Email</h4>
         <div class="container">
           <form action="/authEmail" method="post">
-            <div class="form-group row">
+            <div class="form-group">
               <label for="email" class="col-sm-2 col-form-label">Email</label>
               <div class="col-sm-10">
                 <input type="email" class="form-control" id="email" name="email" placeholder="Email">
               </div>
             </div>
-            <div class="form-group row">
+            <div class="form-group">
               <div class="offset-sm-2 col-sm-10">
                 <button type="submit" class="btn btn-primary">Submit</button>
               </div>
