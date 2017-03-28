a379cd38db077106d05c27304ead1616b672ad55
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-28 10:24:03 -0700

    Update EmailNotConfirmed.php

--- auth/Email/EmailNotConfirmed.php
+++ auth/Email/EmailNotConfirmed.php
@@ -1,9 +1,13 @@
 <?php
 
+Hook('User Is Not Logged In','EmailNotConfirmed();');
 function EmailNotConfirmed(){
-  Hook('Template Body','EmailNotConfirmedBodyCallback();',true);
+  if(path(0)=='emailnotconfirmed'){
+    Hook('Template Body','EmailNotConfirmedBodyCallback();');
   TemplateBootstrap4('Something Went Wrong');
+  }
 }
+
 function EmailNotConfirmedBodyCallback(){
   ?><h1>Welcome Back!</h1>
   <p>It looks like we still need to confirm your email address. I sent you a fresh confirmation link. Please check your email and click on that link to confirm your email!</p>
