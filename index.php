30cccecc081c0cfbba4ddb4f94feed45c97241c4
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-03-30 09:40:28 -0700

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ include('core/Loader.php');
 Loader('core');
 Loader('core/architect');
 Loader('auth/Email');
-//Loader('auth/Facebook');
+Loader('auth/Facebook');
 Loader('auth/Google');
 Loader('defaultViews');
 
