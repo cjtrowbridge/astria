6f8282e9ac1db9f6c18dea64df37cdf73a3a3e4f
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-02 10:41:44 -0700

    Update ArrTabler.php

--- core/ArrTabler.php
+++ core/ArrTabler.php
@@ -1,6 +1,6 @@
 <?php
 
-function ArrTabler($arr, $table_id = null, $table_class = 'tablesorter tablesorter-dark'){
+function ArrTabler($arr, $table_id = null, $table_class = 'table tablesorter tablesorter-dark'){
   $return='';
   if($table_id==null){
     $table_id=md5(uniqid(true));
