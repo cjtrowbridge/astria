3055ab0b2e240ac4f7187e3cfb7e418c951a7a4b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-09 11:08:31 -0700

    Update Nav.php

--- core/Nav.php
+++ core/Nav.php
@@ -1,5 +1,8 @@
 <?php
-  
+
+global $ASTRIA;
+$ASTRIA['nav']=array();
+
 function Nav($Which,$Type,$Text,$Link){
   global $ASTRIA;
   if(isset($ASTRIA['nav'][strtolower($Which)])){
@@ -11,4 +14,23 @@ function Nav($Which,$Type,$Text,$Link){
     'text'=> $Text,
     'link'=> $Link
   );
+}
+
+function ShowNav($Which){
+ if(
+  isset($ASTRIA['nav'])&&
+  isset($ASTRIA['nav']['main'])
+){
+  foreach($ASTRIA['nav']['main'] as $Nav){
+    if(strtolower($Nav['type'])=='link'){
+    ?>
+          
+      <li class="nav-item<?php if(path()==ltrim($path,"/")){ echo ' active';} ?>">
+        <a class="nav-link" href="<?php echo $path; ?>"><?php echo $link; ?></a>
+      </li>
+
+      <?php
+      }
+    }
+  }
 }
