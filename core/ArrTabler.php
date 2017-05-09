9b29c48cff24d050ee9e3ff45ffacfb7184cd7bc
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-09 10:17:46 -0700

    Update ArrTabler.php

--- core/ArrTabler.php
+++ core/ArrTabler.php
@@ -1,6 +1,6 @@
 <?php
 
-function ArrTabler($arr, $table_id = null, $table_class = 'table tablesorter tablesorter-default'){
+function ArrTabler($arr, $table_id = null, $table_class = 'table tablesorter tablesorter-default tablesorter-bootstrap'){
   $return='';
   if($table_id==null){
     $table_id=md5(uniqid(true));
