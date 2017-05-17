b921fa0306c52b638372d2142a93b1eca0981a6e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-16 20:27:19 -0700

    Create Webhook.PullSubrepository.php

---
+++ core/Webhook.PullSubrepository.php
@@ -0,0 +1,26 @@
+<?php
+
+Hook('Webhook','WebhookPullSubrepository');
+
+function WebhookPullSubrepository(){
+  $MagicWord=BlowfishEncrypt('Pull Subrepository From Github');
+  if(
+    isset($_GET[$MagicWord])
+  ){
+    $Path=dirname(__FILE__);
+    $Path=str_replace('/core','',$Path);
+    $Subrepository=BlowfishDecrypt($_GET[$MagicWord]);;
+    $Path.=$Subrepository;
+    
+    echo 'Pulling Subrepository: '.$Subrepository.'<br>';
+    
+    if(is_dir($Path)){
+      die('Path not found: '.$Path);
+    }
+    
+    $Command = 'cd '.$Path.' && git reset --hard && git pull';
+    echo shell_exec($Command);
+    
+    exit;
+  }
+}
