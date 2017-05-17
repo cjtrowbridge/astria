90baac09b8ef2b5a1888a0f2f814460166d645ab
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-16 20:37:52 -0700

    Update GetSubrepositoryPullWebhook.php

--- core/architect/GetSubrepositoryPullWebhook.php
+++ core/architect/GetSubrepositoryPullWebhook.php
@@ -1,18 +1,21 @@
 <?php
 
 function GetSubrepositoryPullWebhook(){
-  if(!isset($_GET['path'])){
+  $Path=dirname(__FILE__);
+  $Path=str_replace('/core/architect','',$Path);
+  $Path.=$_GET['path'];
+  
+  
+  if(!isset($Path)){
     die('Subdirectory path not specified.');
   }
   
-  if(!is_dir($_GET['path'])){
+  if(!is_dir($Path)){
     die('Invalid directory: '.$_GET['path']);
   }
   
   
-  $Path=dirname(__FILE__);
-  $Path=str_replace('/core/architect','',$Path);
-  $Path.=$_GET['path'];
+  
   die($Path);
   
   
