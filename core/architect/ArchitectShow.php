d9f4788902120c889e2cfb526967e0fd95af968e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-16 20:06:03 -0700

    Update ArchitectShow.php

--- core/architect/ArchitectShow.php
+++ core/architect/ArchitectShow.php
@@ -11,24 +11,23 @@ function ArchitectBodyCallback(){
 <h1>Architect <a href="/architect/configuration" target="_blank" style="float: right;"><i class="material-icons">settings</i></a></h1>
 <div class="row">
   <div class="col-xs-12">
-    <form class="form-inline">
+    <div class="form-inline">
       <button onclick="document.location='/architect/schema'" type="button" class="btn btn-outline-warning">Schema</button>
       <button onclick="Cardify('Events','events');" type="button" class="btn btn-outline-warning">Events</button>
       <button onclick="Cardify('Databases','databases');" type="button" class="btn btn-outline-warning">Databases</button>
       <button onclick="Cardify('Users','users');" type="button" class="btn btn-outline-warning">Users</button>
       <button onclick="Cardify('Groups','groups');" type="button" class="btn btn-outline-warning">Groups</button>
       <button onclick="Cardify('Session','session');" type="button" class="btn btn-outline-warning">Session</button>
-      <button onclick="Cardify('Webhook: Pull Mainline','PullMainlineWebhook');" type="button" class="btn btn-outline-danger">Session</button>
+    </div>
+    <div class="form-inline">
+      Webhooks: <button onclick="Cardify('Webhook: Pull Mainline','PullMainlineWebhook');" type="button" class="btn btn-outline-danger">Pull Mainline</button>
       
-    </form>
+    </div>
   </div>
 </div><br>
 <div class="row">
   <div class="hidden" id="PullMainlineWebhook">
-    <textarea><?php
-     echo $ASTRIA['app']['appURL'].'/';
-     echo encrypt('Pull Mainline From Github');
-    ?></textarea>
+    <a target="_blank" href="<?php echo $ASTRIA['app']['appURL'].'/'.encrypt('Pull Mainline From Github'); ?>"><?php echo $ASTRIA['app']['appURL'].'/'.encrypt('Pull Mainline From Github'); ?></a>
   </div>
   <div class="hidden" id="session">
     <?php
