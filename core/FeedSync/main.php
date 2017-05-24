105147a9a167fd4698a93c75c22dff8dbda5d7d5
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-23 21:19:32 -0700

    Update main.php

--- core/FeedSync/main.php
+++ core/FeedSync/main.php
@@ -2,6 +2,11 @@
 
 Hook('Hourly Cron','FeedSyncFetchServiceCron();');
 function FeedSyncFetchServiceCron(){
+  
+  //Include the files
   include('Fetch.php');
+  
+  //Call each service in the appropriate order
   FeedSyncFetchService();
+  
 }
