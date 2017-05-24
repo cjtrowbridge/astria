ef89223fe215feaac06c43308d88b7e10d736cbf
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-23 20:57:22 -0700

    Update Cron.php

--- core/Cron.php
+++ core/Cron.php
@@ -11,21 +11,21 @@ function Cron(){
     return;
   }
  
-  $LastHourlyCron = intval(ReadCache(md5('Last Hourly Cron')));
+  $LastHourlyCron = intval(CacheDatabaseRead(md5('Last Hourly Cron')));
   if($LastHourlyCron < (time()-60*60)){
-    WriteCache(md5('Last Hourly Cron'),time());
+    CacheDatabaseWrite(md5('Last Hourly Cron'),time());
     Event('Hourly Cron');
   }
   
-  $LastDailyCron = intval(ReadCache(md5('Last Daily Cron')));
+  $LastDailyCron = intval(CacheDatabaseRead(md5('Last Daily Cron')));
   if($LastDailyCron < (time()-60*60*24)){
-    WriteCache(md5('Last Daily Cron'),time());
+    CacheDatabaseWrite(md5('Last Daily Cron'),time());
     Event('Daily Cron');
   }
   
-  $LastWeeklyCron = intval(ReadCache(md5('Last Weekly Cron')));
+  $LastWeeklyCron = intval(CacheDatabaseRead(md5('Last Weekly Cron')));
   if($LastWeeklyCron < (time()-60*60*24*7)){
-    WriteCache(md5('Last Weekly Cron'),time());
+    CacheDatabaseWrite(md5('Last Weekly Cron'),time());
     Event('Weekly Cron');
   }
   
