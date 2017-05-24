6cc93c5261363a82b50df2da530861fa96e679ea
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-23 21:44:43 -0700

    Update main.php

--- plugins/FeedSync/main.php
+++ plugins/FeedSync/main.php
@@ -3,6 +3,10 @@
 Hook('Hourly Cron','FeedSyncFetchServiceCron();');
 function FeedSyncFetchServiceCron(){
   
+  //This might take a while, and thats fine.
+  set_time_limit(0);
+  //TODO logging for cron runtimes
+  
   //Include the files
   include('Fetch.php');
   
