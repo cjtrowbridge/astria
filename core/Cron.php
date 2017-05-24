bf13c04a8687aec9fca29ed6feb3b0b16ade5c0e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-23 21:10:31 -0700

    Update Cron.php

--- core/Cron.php
+++ core/Cron.php
@@ -16,33 +16,33 @@ function Cron(){
   $LastHourlyCron = intval(CacheDatabaseRead(md5('Last Hourly Cron')));
   if($LastHourlyCron < (time()-60*60)){
     CacheDatabaseWrite(md5('Last Hourly Cron'),time());
-    echo '<p>Hourly Cron Last Ran '.ago($LastHourlyCron).'. Running Now...</p>';
+    echo '<p>Hourly cron last ran '.ago($LastHourlyCron).'. Running now...</p>';
     Event('Hourly Cron');
   }else{
-    echo '<p>Skipping Hourly Cron Because It Last Ran '.ago($LastHourlyCron).'.</p>';
+    echo '<p>Skipping hourly cron because it last ran '.ago($LastHourlyCron).'.</p>';
   }
   
   $LastDailyCron = intval(CacheDatabaseRead(md5('Last Daily Cron')));
   if($LastDailyCron < (time()-60*60*24)){
     CacheDatabaseWrite(md5('Last Daily Cron'),time());
-    echo '<p>Daily Cron Last Ran '.ago($LastDailyCron).'. Running Now...</p>';
+    echo '<p>Daily cron last ran '.ago($LastDailyCron).'. Running now...</p>';
     Event('Daily Cron');
   }else{
-    echo '<p>Skipping Daily Cron Because It Last Ran '.ago($LastDailyCron).'.</p>';
+    echo '<p>Skipping daily cron because it last ran '.ago($LastDailyCron).'.</p>';
   }
   
   $LastWeeklyCron = intval(CacheDatabaseRead(md5('Last Weekly Cron')));
   if($LastWeeklyCron < (time()-60*60*24*7)){
     CacheDatabaseWrite(md5('Last Weekly Cron'),time());
-    echo '<p>Weekly Cron Last Ran '.ago($LastWeeklyCron).'. Running Now...</p>';
+    echo '<p>Weekly cron last ran '.ago($LastWeeklyCron).'. Running now...</p>';
     Event('Weekly Cron');
   }else{
-    echo '<p>Skipping Weekly Cron Because It Last Ran '.ago($LastWeeklyCron).'.</p>';
+    echo '<p>Skipping weekly cron because it last ran '.ago($LastWeeklyCron).'.</p>';
   }
   
   $CronEnd  = microtime(true);
   $Duration = $CronEnd-$CronStart;
   $Duration = round($Duration,4);
   
-  die('<p>Cron Finished In '.$Duration.' Seconds.</p>');
+  die('<p>Cron finished in '.$Duration.' seconds.</p>');
 }
