24419dfc36785db3b4920ea20b066e2342b7905e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-23 19:44:53 -0700

    Create Cron.php

---
+++ core/Cron.php
@@ -0,0 +1,30 @@
+<?php
+
+if(
+  isset($_GET['cron'])||
+  path(0)=='cron'
+){
+  Hook('Before Login','Cron();');
+}
+
+function Cron(){
+  
+  $LastHourlyCron = intval(ReadCache(md5('Last Hourly Cron')));
+  if($LastHourlyCron < (time()-60*60)){
+    WriteCache(md5('Last Hourly Cron'),time());
+    Event('Hourly Cron');
+  }
+  
+  $LastDailyCron = intval(ReadCache(md5('Last Daily Cron')));
+  if($LastDailyCron < (time()-60*60*24)){
+    WriteCache(md5('Last Daily Cron'),time());
+    Event('Daily Cron');
+  }
+  
+  $LastWeeklyCron = intval(ReadCache(md5('Last Weekly Cron')));
+  if($LastWeeklyCron < (time()-60*60*24*7)){
+    WriteCache(md5('Last Weekly Cron'),time());
+    Event('Weekly Cron');
+  }
+  
+}
