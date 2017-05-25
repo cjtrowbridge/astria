86ef1291c8557e99c7710877f70a230a0a0e6f13
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-25 10:29:45 -0700

    Update Nav.php

--- core/Nav.php
+++ core/Nav.php
@@ -3,16 +3,21 @@
 global $ASTRIA;
 $ASTRIA['nav']=array();
 
-function Nav($Which,$Type,$Text,$Link){
+function Nav($Which,$Type,$Text,$Link,$Children = null){
+  if($Children==null){
+    $Children=array();
+  }
+  
   global $ASTRIA;
   if(!isset($ASTRIA['nav'][strtolower($Which)])){
     $ASTRIA['nav'][strtolower($Which)]=array();
   }
     
   $ASTRIA['nav'][strtolower($Which)][]=array(
-    'type'=> strtolower($Type),
-    'text'=> $Text,
-    'link'=> $Link
+    'type'     => strtolower($Type),
+    'text'     => $Text,
+    'link'     => $Link,
+    'children' => $Children
   );
 }
 
@@ -22,19 +27,40 @@ function ShowNav($Which){
   pd($ASTRIA['nav']);
   echo "\n-->\n";
   if(
-   isset($ASTRIA['nav'])&&
-   isset($ASTRIA['nav'][strtolower($Which)])
- ){
-   foreach($ASTRIA['nav'][strtolower($Which)] as $Nav){
-     if(strtolower($Nav['type'])=='link'){
-     ?>
+    isset($ASTRIA['nav'])&&
+    isset($ASTRIA['nav'][strtolower($Which)])
+  ){
+    foreach($ASTRIA['nav'][strtolower($Which)] as $Nav){
+      if(strtolower($Nav['type'])=='link'){
+      ?>
            
-       <li class="nav-item<?php if(path()==ltrim($Nav['link'],"/")){ echo ' active';} ?>">
-         <a class="nav-link" href="<?php echo $Nav['link']; ?>"><?php echo $Nav['text']; ?></a>
-       </li>
+        <li class="nav-item<?php if(path()==ltrim($Nav['link'],"/")){ echo ' active';} ?>">
+          <a class="nav-link" href="<?php echo $Nav['link']; ?>"><?php echo $Nav['text']; ?></a>
+        </li>
  
-       <?php
-       }
-     }
-   }
- }
+      <?php
+      }
+      if(strtolower($Nav['type']=='dropdown){
+      ?>
+      
+      <div class="dropdown show">
+        <a class="btn btn-secondary dropdown-toggle" href="<?php echo $Nav['link']; ?>" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <?php echo $Nav['text']; ?>
+        </a>
+
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+          <?php
+            foreach($Nav['Children'] as $Child){
+          ?>
+          <a class="dropdown-item" href="<?php echo $Child['link']; ?>"><?php echo $Child['text']; ?></a>
+          <?php
+          }
+          ?>
+        </div>
+      </div>
+      
+      <?php
+      }
+    }
+  }
+}
