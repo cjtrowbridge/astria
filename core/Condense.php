89f2c4215c8c54c05b02952f13eb340f2f82772c
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-25 17:59:45 -0700

    Update Condense.php

--- core/Condense.php
+++ core/Condense.php
@@ -1,7 +1,7 @@
 <?php
 
 function PickBest($Array,$NumberOfSentences = 1){
-  //TODO add array for words to ignore
+  //TODO remove from list of headlines each word which was a previous top word. ie. "trump" is most popular word in first headline, so remove any headlines with that word before calculating second headline.
   $Text = '';
   foreach($Array as $RawSentence){
     $Text.= ' '.$RawSentence;
