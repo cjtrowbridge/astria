3be5c7ac045a75d201edf229544cfcb5036d89be
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-05-31 21:02:49 -0700

    Update Session.php

--- core/Session.php
+++ core/Session.php
@@ -80,7 +80,7 @@ function AstriaSessionDestroy(){
 }
 
 
-Hook('Daily Cron','SessionCron();');
+//Hook('Daily Cron','SessionCron();');
 
 function SessionCron(){
   Query("DELETE FROM `Session` WHERE Expires < NOW()");
