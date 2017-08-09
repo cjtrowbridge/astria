dcce9592c8acc4aaa75a9a33d2bbf0e25c9286d5
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-08-09 16:16:17 -0700

    Update ArchitectFileCreate.php

--- core/architect/Files/ArchitectFileCreate.php
+++ core/architect/Files/ArchitectFileCreate.php
@@ -8,7 +8,7 @@ function ArchitectFileCreateBodyCallback(){
   
   <h1>Create File</h1>
   <form action="/architect/files/edit/" method="get" class="form">
-    <input type="text" class="form-control" name="path" value="<?php echo $_GET['path']; ?>">
+    <input type="text" class="form-control" name="path" value="<?php echo $_GET['path']; ?>"><br>
     <input type="submit" class="btn btn-block btn-success" value="Create File">
   </form>
   
