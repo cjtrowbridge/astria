3be5909e4587a9b24ea880644c0e6c69d3faef1d
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-08-09 16:57:50 -0700

    Update ArchitectDirectoryCreate.php

--- core/architect/Files/ArchitectDirectoryCreate.php
+++ core/architect/Files/ArchitectDirectoryCreate.php
@@ -1,7 +1,10 @@
 <?php
 function ArchitectDirectoryCreate(){
-  if(isset($_POST['path'])){
-    pd($_POST);
+  if(isset($_POST['newDirectory'])){
+    if(mkdir($_SERVER['DOCUMENT_ROOT'].$_POST['newDirectory'],775)==false){
+      die('<p>Unable to create: '.$_GET['path'].'</p>');
+    }
+    header('Location: /architect/files/?path='.$_POST['newDirectory']);
     exit;
   }
   TemplateBootstrap4('Create Directory - Architect','ArchitectDirectoryCreateBodyCallback();'); 
@@ -11,7 +14,7 @@ function ArchitectDirectoryCreateBodyCallback(){
   
   <h1>Create Directory</h1>
   <form action="/architect/files/create-directory/" method="post" class="form">
-    <input type="text" class="form-control" name="path" id="path" value="<?php echo $_GET['path']; ?>"><br>
+    <input type="text" class="form-control" name="newDirectory" id="newDirectory" value="<?php echo $_GET['path']; ?>"><br>
     <input type="submit" class="btn btn-block btn-success" value="Create Directory">
   </form>
   
