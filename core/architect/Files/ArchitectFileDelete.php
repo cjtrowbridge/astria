f79e53f91a7c2c18e20adb793c30a68561505feb
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-08-09 16:48:22 -0700

    Update ArchitectFileDelete.php

--- core/architect/Files/ArchitectFileDelete.php
+++ core/architect/Files/ArchitectFileDelete.php
@@ -1,13 +1,22 @@
 <?php
 
 function ArchitectFileDelete(){
+  if(isset($_GET['yesimsure'])){
+    $Parent=dirname($_GET['path']);
+    echo $Parent;
+    //if(unlink($_GET['path'])==false){
+      //die('<p>Unable to delete: '.$_GET['path'].'</p>');
+    //}
+    //header('Location: /architect/files/?path='.$Parent);
+    exit;
+  }
   TemplateBootstrap4('File Delete - Architect','ArchitectFileDeleteBodyCallback();'); 
 }
 function ArchitectFileDeleteBodyCallback(){
   ?>
   
   <h1>Delete File</h1>
-  <h2>Are you sure you want to delete: <?php echo $_GET['path']; ?></h2>
+  <h2>Are you sure you want to delete: <?php echo $_GET['path']; ?></h2><br>
   
   <div class="row">
     <div class="col-xs-12 col-md-6">
