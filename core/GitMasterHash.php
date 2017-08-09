7a0278db1b65c14ba3949992e07808963023363b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-08-09 15:32:31 -0700

    Update GitMasterHash.php

--- core/GitMasterHash.php
+++ core/GitMasterHash.php
@@ -4,10 +4,14 @@ Hook('Before Login','GitMasterHash();');
   
 function GitMasterHash(){
   if(isset($_GET['gitHash'])){
-    if($Hash = file_get_contents('../.git/refs/heads/master'))){
-      die($Hash);
-    }else{
-      die('unknown');
-    }
+    die(gitHash());
+  }
+}
+
+function gitHash(){
+  if($Hash = file_get_contents('../.git/refs/heads/master'))){
+    return $Hash;
+  }else{
+    return 'unknown';
   }
 }
