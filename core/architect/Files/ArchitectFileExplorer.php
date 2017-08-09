04405e05d58b33a889c0fd18963b08a13910f202
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-08-09 16:38:03 -0700

    Update ArchitectFileExplorer.php

--- core/architect/Files/ArchitectFileExplorer.php
+++ core/architect/Files/ArchitectFileExplorer.php
@@ -47,7 +47,7 @@ function ArchitectFileExplorerFile(){
     case 'htm':
     case 'shtm':
     case 'sql':
-      pd(file_get_contents($_SERVER['DOCUMENT_ROOT'].$_GET['path']));
+      echo '<pre>'.file_get_contents($_SERVER['DOCUMENT_ROOT'].$_GET['path']).'</pre>';
       break;
     case 'jpg':
     case 'jpeg':
