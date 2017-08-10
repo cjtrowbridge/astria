1650fe52fb247cf17eabd3ea71ce6c322f397993
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-08-10 01:00:53 -0700

    Create CreateUser.php

---
+++ core/CreateUser.php
@@ -0,0 +1,19 @@
+<?php
+
+function CreateUser($Email){
+  global $ASTRIA;
+  MakeSureDBConnected();
+  $cleanEmail=mysqli_real_escape_string($ASTRIA['databases']['astria']['resource'],$ASTRIA['Session']['google_oauth2']['user_object']->email);
+  
+  //Add user to users table
+  Query("INSERT INTO `User`(`Email`,`SignupDate`)VALUES('".$cleanEmail."',NOW());");
+  
+  //Add this new user to the default group 
+  CreateMembership(mysqli_insert_id($ASTRIA['databases']['astria']['resource']),1);
+  
+  //Notify Admin of signup
+  $message = '<h1>'.$ASTRIA['app']['appName'].': New User Signup!</h1><p>'.htmlentities($cleanEmail).'</p>';
+  $subject = $ASTRIA['app']['appName'].': New Signup';
+  $to      = $ASTRIA['smtp']['adminEmail'];
+  SendEmail($message, $subject, $to);
+}
