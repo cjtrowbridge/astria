a2d224065c3fef632792aa6e670d93f27619bf33
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-08-13 02:33:27 -0700

    Update TemplateBootstrap4.php

--- core/TemplateBootstrap4.php
+++ core/TemplateBootstrap4.php
@@ -1,9 +1,7 @@
 <?php
-
 function TemplateBootstrap4($title='',$BodyCallback = '',$Fluid=false){
   global $HAMBURGER, $ASTRIA, $USER;
   $HAMBURGER=array('Home'=>'/');
-
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,16 +18,15 @@ function TemplateBootstrap4($title='',$BodyCallback = '',$Fluid=false){
     echo $ASTRIA['app']['appName']; 
   ?></title>
 
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
   
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
+  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-timepicker/1.10.0/jquery.timepicker.min.js"></script>
   
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.28.8/js/jquery.tablesorter.combined.min.js"></script>
@@ -40,6 +37,7 @@ function TemplateBootstrap4($title='',$BodyCallback = '',$Fluid=false){
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.28.8/css/theme.metro-dark.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.28.3/css/theme.dark.min.css">
 
+  <link rel="stylesheet" href="/css/treeview.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-timepicker/1.10.0/jquery.timepicker.min.css">
 
   <!--Theme-->
@@ -81,7 +79,7 @@ function TemplateBootstrap4($title='',$BodyCallback = '',$Fluid=false){
     <?php } ?>
   </nav>
 
-  <div class="container<?php if($Fluid){echo '-fluid';} ?>" id="bodyContainer">
+  <div class="container<?php if($Fluid){echo '-fluid';} ?> no-gutters" id="bodyContainer">
 
     <?php 
   
@@ -104,7 +102,6 @@ function TemplateBootstrap4($title='',$BodyCallback = '',$Fluid=false){
     <a href="https://github.com/cjtrowbridge/astria" target="_blank">Astria</a> Loaded in <?php echo  $Runtime; ?> Seconds
   </div>
   <script>
-    /*
     FixTopPadding();
     $(window).resize(function(){
       FixTopPadding();
@@ -115,7 +112,6 @@ function TemplateBootstrap4($title='',$BodyCallback = '',$Fluid=false){
         $('#topNav').outerHeight()
       );
     }
-    */
   </script>
 
 </body>
