c27917bc4e470bff8bbca508e9fc881c8d79dcc0
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-08-13 01:30:33 -0700

    Create IntegrationTest.php

---
+++ core/IntegrationTest.php
@@ -0,0 +1,14 @@
+<?php
+
+Hook('User Is Not Logged In','IntegrationTest();');
+
+function IntegrationTest(){
+  if(path(0)=='test'){
+    switch(path(1)){
+      case 'integration':
+        die(ok);
+      case false:
+        die('Specify a test to run.');
+    }
+  }
+}
