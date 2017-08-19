05ee578004ee51d2fedfc4296cf9b79cc41e0f89
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-08-19 12:21:13 -0700

    Update ExternalAnalytics.php

--- core/ExternalAnalytics.php
+++ core/ExternalAnalytics.php
@@ -1,5 +1,8 @@
 <?php
 
+//This should always run regardless of the state or route. It will only do anything if appropriate and it never outputs anything.
+ExternalAnalytics();
+
 function ExternalAnalytics(){
   global $ASTRIA;
   if(isset($ASTRIA['analytics'])){
