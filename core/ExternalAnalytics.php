224dc5ce1c432937699266665b5dbd91dfb09008
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-08-19 12:25:47 -0700

    Update ExternalAnalytics.php

--- core/ExternalAnalytics.php
+++ core/ExternalAnalytics.php
@@ -9,14 +9,30 @@ function ExternalAnalytics(){
   
     if(isset($ASTRIA['analytics']['Google'])){
       Hook('Template Head','GoogleAnalyticsSnippet();');
+    }else{
+      Hook('Architect Homepage','AlertAdminMissingAnalytics();');
     }
     
     
   
+  }else{
+    Hook('Architect Homepage','AlertAdminMissingAnalytics();');
   }
 }
 
 function GoogleAnalyticsSnippet(){
   global $ASTRIA;
   echo PHP_EOL.$ASTRIA['analytics']['Google'].PHP_EOL;
+}
+
+
+function AlertAdminMissingAnalytics(){
+  ?>
+    <div class="card">
+      <div class="card-block">
+        <h4 class="card-title">Missing Analytics Configuration</h4>
+        <p class="card-text"><a href="/architect/configuration" class="card-link">Click Here</a> to update missing configuration data.</p>
+      </div>
+    </div>
+  <?php
 }
