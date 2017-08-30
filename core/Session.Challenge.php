05d23257b5c2d3ba3175e9f79857e5d329cefb57
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-08-30 13:29:01 -0700

    Create Session.Challenge.php

---
+++ core/Session.Challenge.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once('core/Hook.php');
+Hook('User Is Logged In','MaybeChallengeSession();');
+
+function MaybeChallengeSession(){
+  die('WHY');
+  //if(isset($_GET['challengeSession'])){
+    AstriaChallengeSession();
+    exit;
+  //}
+}
+
+function AstriaChallengeSession(){
+  //If for some reason we are skeptical of the user's session, challenge it.
+  Event('Challenge Session');
+}
