413d6e300a5171711a6292fd2d624cf2d6875c67
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-08-30 14:15:27 -0700

    Update VerifyAgentAndIP.php

--- core/VerifyAgentAndIP.php
+++ core/VerifyAgentAndIP.php
@@ -1,13 +1,12 @@
 <?php
 
-//TODO convert destruction to session challenges
-
 function VerifyAgentAndIP(){
 	//Make sure that the user agent and ip have not changed and that the sessions is not expired
 	global $ASTRIA;
 	
 	if($ASTRIA['Session']==null){
-		//die('Session was null');	
+		//die('Session was null');
+		//This probably should only happen the first time you load the configuration page's post-handler, but it's difficult to be sure of that. For now, leave this blank.
 	}
 	
 	
@@ -16,20 +15,23 @@ function VerifyAgentAndIP(){
 			echo $ASTRIA['Session']['UserAgentHash'].'<br><br> DOES NOT MATCH<br><br>'.$_SERVER['HTTP_USER_AGENT'];
 			exit;
 		}
-		AstriaSessionDestroy();
+		AstriaChallengeSession();
+		//AstriaSessionDestroy();
 	}
 	if(!($ASTRIA['Session']['RemoteAddrHash'] == md5($_SERVER['REMOTE_ADDR']))){
 		if(isset($_GET['verbose'])){
 			echo $ASTRIA['Session']['RemoteAddrHash'].'<br><br> DOES NOT MATCH<br><br>'.md5($_SERVER['REMOTE_ADDR']);
 			exit;
 		}
-		AstriaSessionDestroy();
+		AstriaChallengeSession();
+		//AstriaSessionDestroy();
 	}
 	if(!($ASTRIA['Session']['Expires']>time())){
 		if(isset($_GET['verbose'])){
 			echo 'Session expired';
 			exit;
 		}
-		AstriaSessionDestroy();
+		AstriaChallengeSession();
+		//AstriaSessionDestroy();
 	}
 }
