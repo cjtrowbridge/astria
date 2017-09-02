bd3fa5d35a1e6ab5720cb4abc3b4e6728d0131e3
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-02 16:42:52 -0700

    Create Category.php

---
+++ core/architect/FeedSyncUI/Category.php
@@ -0,0 +1,43 @@
+<?php
+
+function FeedSyncCategoryPage(){
+  
+  
+  
+  TemplateBootstrap4('Category - FeedSync','FeedSyncCategoryPageBodyCallback();');
+}
+
+
+function FeedSyncCategoryPageBodyCallback(){
+  $CategoryID = Sanitize(path(3));
+  if($CategoryID == 'add'){
+    FeedSyncCategoryNew();
+  }else{
+    $Category = Query('SELECT * FROM FeedCategory WHERE FeedCateoryID LIKE "'.$CategoryID.'" OR Name LIKE "'.$CategoryID.'"');
+    if(count($Category)==0{
+      ?>
+      <h1><a href="/architect/feedsync">FeedSync</a> - Category Not Found</h1>
+      <?php
+      exit;
+    }else{
+      $Category = $Category[0];
+      FeedSyncCategoryEdit($Category);
+    }
+  
+  
+}
+
+function FeedSyncCategoryEdit($Category){
+  ?>
+  <h1><a href="/architect/feedsync">FeedSync</a> - </h1>
+  <?php
+  
+  pd($Category);
+  //AstriaBootstrapAutoForm($Editable,$Readable = array(),$Hidden = array());
+}
+
+function FeedSyncCategoryNew($Category){
+  pd($Category);
+  
+  //AstriaBootstrapAutoForm($Editable,$Readable = array(),$Hidden = array());
+}
