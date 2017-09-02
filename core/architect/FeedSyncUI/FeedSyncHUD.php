4f6c4e1ebfd73311b22d01666df2cecdc72e2461
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-02 15:58:41 -0700

    Update FeedSyncHUD.php

--- core/architect/FeedSyncUI/FeedSyncHUD.php
+++ core/architect/FeedSyncUI/FeedSyncHUD.php
@@ -6,13 +6,9 @@ function FeedSyncHUD(){
 
 function FeedSyncUIBodyCallback(){
   
-  //This is intended to catch an error if it is run before setup has completed so we need to turn off error reporting, but then set it back to whatever it was before.
-  $OldErrorReportingLevel = error_reporting();
-  //error_reporting(0);
-  $Check = Query('SELECT 1 FROM `Feed` LIMIT 1');
-  //error_reporting($OldErrorReportingLevel);
+  $Check = Query("SHOW TABLES LIKE 'Feed';");
   
-  if($Check == FALSE){
+  if(count($Check) == 0){
     ?><h1>FeedSync Setup</h1>
     <p>To add the required tables to use FeedSync, <a href="/architect/feedsync/setup">click here</a>.</p>
     <?php
