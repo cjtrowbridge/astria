084b6416116d5f84dbb5981d372af216eb00b1bb
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-02 15:32:40 -0700

    Update FeedSyncHUD.php

--- core/architect/FeedSyncUI/FeedSyncHUD.php
+++ core/architect/FeedSyncUI/FeedSyncHUD.php
@@ -7,7 +7,13 @@ function FeedSyncHUD(){
 function FeedSyncUIBodyCallback(){
   ?><h1>Your Feeds</h1>
   <ul>
+    <?php
+      $Feeds = Query('SELECT * FROM Feed');
+      foreach($Feeds as $Feed){
+        pd($Feed);
+    ?>
     <li></li>
+    <?php } ?>
   </ul>
   <?php
 }
