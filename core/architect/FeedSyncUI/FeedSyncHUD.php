3473aaca971f2def55c97ad0e9b0630033c1a5a5
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-02 15:29:59 -0700

    Create FeedSyncHUD.php

---
+++ core/architect/FeedSyncUI/FeedSyncHUD.php
@@ -0,0 +1,13 @@
+<?php
+
+function FeedSyncHUD(){
+  Template4('FeedSyncUI','FeedSyncUIBodyCallback();');
+}
+
+function FeedSyncUIBodyCallback(){
+  ?><h1>Your Feeds</h1>
+  <ul>
+    <li></li>
+  </ul>
+  <?php
+}
