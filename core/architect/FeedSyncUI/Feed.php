61bd152ae3cfd0e2aa51516e89de438ef201e26f
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-02 17:29:07 -0700

    Create Feed.php

---
+++ core/architect/FeedSyncUI/Feed.php
@@ -0,0 +1,129 @@
+<?php
+
+function FeedSyncFeedPage(){
+  if(
+    isset($_POST['Name'])
+  ){
+      if($_POST['ParentID']==''){
+        $MaybeParentID = "NULL";
+      }else{
+        $MaybeParentID = "'".intval($_POST['ParentID'])."'";
+      }
+    
+    //Something being submitted
+    //But is it a new or an update?
+    
+    if(isset($_POST['FeedCategoryID'])){
+      
+      //Update
+      Query("
+        UPDATE `FeedCategory` SET 
+          `Name`        = '".Sanitize($_POST['Name'])."', 
+          `Description` = '".Sanitize($_POST['Description'])."', 
+          `Path`        = '".Sanitize($_POST['Path'])."', 
+          `ParentID`    = ".$MaybeParentID."
+        WHERE `FeedCategory`.`FeedCategoryID` = ".intval(Sanitize($_POST['FeedCategoryID'])).";
+      ");
+      $DestinationID = intval(Sanitize($_POST['FeedCategoryID']));
+      
+    }else{
+      
+      //New
+      Query("
+        INSERT INTO `FeedCategory`(
+          `Name`, 
+          `Description`, 
+          `Path`, 
+          `ParentID`
+        )VALUES(
+          '".Sanitize($_POST['Name'])."', 
+          '".Sanitize($_POST['Description'])."', 
+          '".Sanitize($_POST['Path'])."', 
+          ".$MaybeParentID."
+        );
+      ");
+      global $ASTRIA;
+      $DestinationID = mysqli_insert_id($ASTRIA['databases']['astria']['resource']);
+    }
+    
+    header('Location: /architect/feedsync/category/'.$DestinationID);
+    exit;
+  }
+  
+  TemplateBootstrap4('Feed - FeedSync','FeedSyncFeedPageBodyCallback();');
+}
+function FeedSyncFeedPageBodyCallback(){
+  //TODO false should probably show a list of feeds instead of creating a new feed
+  if(
+    $FeedID == 'add'||
+    $FeedID == false
+  ){
+    FeedSyncCategoryNew();
+  }else{
+    $FeedID = intval(path(3));
+    $Feed = Query('SELECT * FROM Feed WHERE FeedID LIKE '.$FeedID.');
+    if(count($Feed)==0){
+      ?>
+      <h1><a href="/architect/feedsync">FeedSync</a> - Category Not Found</h1>
+      <?php
+      exit;
+    }else{
+      $Feed = $Feed[0];
+      FeedSyncFeedEdit($Feed);
+    }
+  }
+  
+}
+function FeedSyncFeedEdit($Feed){
+  ?>
+  <h1><a href="/architect/feedsync">FeedSync</a> - Feed Editor</h1>
+  <?php
+  
+  pd($Feed);
+  return;
+  
+  $Editable = array(
+    'Name'        => $Category['Name'],
+    'Description' => $Category['Description'],
+    'Path'        => $Category['Path'],
+    'ParentID'    => $Category['ParentID']
+  );
+  $Readable = array(
+    'FeedCategoryID' => $Category['FeedCategoryID']
+  );
+  $Hidden = $Readable;
+  
+  echo AstriaBootstrapAutoForm(
+    $Editable,
+    $Readable,
+    $Hidden
+  );
+}
+function FeedSyncFeedNew(){
+  ?>
+  <h1><a href="/architect/feedsync">FeedSync</a> - Feed Editor - New</h1>
+  <?php
+  
+  $Editable = array(
+    'Name'        => '',
+    'Description' => '',
+    'Path'        => '',
+    'ParentID'    => ''
+  );
+  
+  echo AstriaBootstrapAutoForm(
+    $Editable
+  );
+  ?>
+  <script>
+    $("#Name").change(function(){
+      var NewPath = $("#Name").val();
+      
+      NewPath = NewPath.toLowerCase();
+      NewPath = NewPath.replace(' ','-');
+      
+      $("#Path").val(NewPath);
+    });
+  </script>
+  <?php
+}
