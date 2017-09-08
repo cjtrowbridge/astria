327f4eea44640229a7d84cd8f1a5c1eb15e1703e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-08 16:58:26 -0700

    Update ArchitectSchemaHUD.php

--- core/architect/Schema/ArchitectSchemaHUD.php
+++ core/architect/Schema/ArchitectSchemaHUD.php
@@ -9,15 +9,20 @@ function ArchitectSchemaHUDBodyCallback(){
   ?><h1>Schema - Architect</h1>
   
   <?php
-  foreach($ASTRIA['databases'] as $Database){
+  foreach($ASTRIA['databases'] as $Alias => $Database){
     ?>
     
     <div class="card">
       <div class="card-block">
-        <h4 class="card-title"><?php echo $Database['name']; ?></h4>
-        <?php
-          pd($Database);
-        ?>
+        <h4 class="card-title"><?php echo $Alias; ?></h4>
+        <p><b><?php echo $Database['type'].'://'.$Database['hostname']; ?></b></p>
+        <p><?php  
+          if($Database['resource']===false){
+            echo '<p>Not Connected</p>';
+          }else{
+            echo '<p>Connected</p>';
+          }
+        ?></p>
       </div>
     </div>
 
