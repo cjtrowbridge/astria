fee0807d35409a18f4db20ea304d3870aa2e82df
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-09 14:02:01 -0700

    Update MySQL.Query.php

--- core/architect/Schema/MySQL.Query.php
+++ core/architect/Schema/MySQL.Query.php
@@ -15,7 +15,7 @@ function ArchitectSchemaMySQLQuery($Alias){
 
   <div class="card">
     <div class="card-block">
-      <h4 class="card-title">Query Database: '<a href="/architect/schema/<?php echo $Alias; ?>/"><?php echo $Name; ?></a>'</h4>
+      <b class="card-title">Query Database: '<a href="/architect/schema/<?php echo $Alias; ?>/"><?php echo $Name; ?></a>'</b><br>
       <form action="/architect/schema/<?php echo $Alias; ?>/?query" method="post">
         <textarea class="form-control" name="query" rows="8"><?php echo $Query; ?></textarea><br>
         <input type="submit" class="btn btn-success" value="Run">
@@ -25,7 +25,7 @@ function ArchitectSchemaMySQLQuery($Alias){
 
   <div class="card">
     <div class="card-block">
-      <h4 class="card-title">Results</h4>
+      <b class="card-title">Results</b><br>
       <?php echo ArrTabler(Query($Query,$Alias)); ?>
     </div>
   </div>
