c3408167b5e58a83c07b452787a1c98955e4e105
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-08 17:18:21 -0700

    Update MySQL.DescribeDatabase.php

--- core/architect/Schema/MySQL.DescribeDatabase.php
+++ core/architect/Schema/MySQL.DescribeDatabase.php
@@ -4,6 +4,6 @@ function MySQLDescribeDatabase($Alias){
   $Tables = Query('SHOW TABLES',$Alias);
   foreach($Tables as $Table){
     $Table = array_shift($Table);
-    echo $Table;
+    echo '<p><a href="/architect/schema/'.path(2).'/'.path(3).'/'.$Table'">'.$Table.'</a></p>';
   }
 }
