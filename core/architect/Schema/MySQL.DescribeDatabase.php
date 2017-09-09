fb4e1b65b4a1c5f85d21747d53715c787b70e295
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-08 17:22:06 -0700

    Update MySQL.DescribeDatabase.php

--- core/architect/Schema/MySQL.DescribeDatabase.php
+++ core/architect/Schema/MySQL.DescribeDatabase.php
@@ -1,12 +1,16 @@
 <?php
 
 function MySQLDescribeDatabase($Alias){
+  global $ASTRIA;
+  $Name = $ASTRIA['databases'][$Alias]['name'];
+  $Type = $ASTRIA['databases'][$Alias]['type'];
   $Tables = Query('SHOW TABLES',$Alias);
   
-  echo PHP_EOL."<ol>".PHP_EOL;
+  echo '<h1>Database: '.$Type.'://'.$Name.'</h1>';
+  echo PHP_EOL."<ul>".PHP_EOL;
   foreach($Tables as $Table){
     $Table = array_shift($Table);
     echo '  <li><a href="/architect/schema/'.path(2).'/'.path(3).'/'.$Table.'">'.$Table.'</a></li>'.PHP_EOL;
   }
-  echo "</ol>".PHP_EOL;
+  echo "</ul>".PHP_EOL;
 }
