f48fa4feef8d8436dd48c88a008f8998853c4625
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-08 17:01:19 -0700

    Update Routing.php

--- core/architect/Schema/Routing.php
+++ core/architect/Schema/Routing.php
@@ -2,10 +2,14 @@
 
 function ArchitectSchema(){
   switch(path(2)){
-    
-    default:
-      include('ArchitectSchemaHUD.php');
+    case false:
+      include_once('ArchitectSchemaHUD.php');
       ArchitectSchemaHUD();
+      break;
+    default:
+      //try elaborating on this as a database alias
+      include_once('ArchitectSchemaElaborate.php');
+      ArchitectSchemaElaborate(path(2));
       break;
   }
 }
