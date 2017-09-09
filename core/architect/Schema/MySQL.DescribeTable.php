ae23f8e6aeeab5f1bf32b9037901cd8e1b0ba3c1
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-08 17:52:32 -0700

    Update MySQL.DescribeTable.php

--- core/architect/Schema/MySQL.DescribeTable.php
+++ core/architect/Schema/MySQL.DescribeTable.php
@@ -6,6 +6,10 @@ function MySQLDescribeTable($Alias,$Table){
   $Description = Query('DESCRIBE `'.Sanitize($Table).'`',$Alias);
   
   echo '<h1>Database: '." '".$DBName."'.'".$Table."' </h1>";
+  echo '<h2>Description</h2>';
   echo ArrTabler($Description);
+  
+  echo '<h2>Data</h2>';
+  echo ArrTabler(Query("SELECT * FROM `".$Table."` ORDER BY 1 DESC"));
   
 }
