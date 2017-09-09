c78521a09d525e46114c8e42687173b6355e284a
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-09 13:33:45 -0700

    Update MySQL.DescribeTable.php

--- core/architect/Schema/MySQL.DescribeTable.php
+++ core/architect/Schema/MySQL.DescribeTable.php
@@ -6,10 +6,37 @@ function MySQLDescribeTable($Alias,$Table){
   $Description = Query('DESCRIBE `'.Sanitize($Table).'`',$Alias);
   
   echo '<h1>Database: '." '".$DBName."'.'".$Table."' </h1>";
-  echo '<h2>Description</h2>';
-  echo ArrTabler($Description);
   
-  echo '<h2>Data</h2>';
+  ?>
+  <div>
+    <a class="btn btn-outline-success" href="/architect/schema/<?php echo $DBName; ?>/table/<?php echo $Table; ?>/?csv">Dump to CSV</a>
+    <a class="btn btn-outline-success" href="/architect/schema/<?php echo $DBName; ?>/table/<?php echo $Table; ?>/?show-all">Show All</a>
+    <a class="btn btn-outline-success" href="javascript:void(0);" onclick="$('#tableDescription').slideToggle();">Show Description</a>  
+  </div><br>
+  
+
+  <div class="card" id="tableDescription" style="display: none;">
+    <div class="card-block">
+      <h2 class="card-title">Description</h2>
+      <div class="card-text">
+        <?php echo ArrTabler($Description); ?>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="card">
+    <div class="card-block">
+      <h1 class="card-title">Database: '<?php echo $DBName; ?>'.'<?php echo $Table; ?>'</h1>
+      <div class="card-text">
+        <?php echo ArrTabler($Description); ?>
+      </div>
+    </div>
+  </div>
+
+  <?php
+  
+  
   echo ArrTabler(Query("SELECT * FROM `".$Table."` ORDER BY 1 DESC"));
   
 }
