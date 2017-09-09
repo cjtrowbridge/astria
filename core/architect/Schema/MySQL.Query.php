17be1961abe54cc172d2db406357df175cd5e0b4
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-09 13:57:23 -0700

    Update MySQL.Query.php

--- core/architect/Schema/MySQL.Query.php
+++ core/architect/Schema/MySQL.Query.php
@@ -4,28 +4,29 @@ function ArchitectSchemaMySQLQuery($Alias){
   global $ASTRIA;
   $Name = $ASTRIA['databases'][$Alias]['database'];
   
+  if(!(isset($_POST['query']))){
+    $Query='SHOW TABLES';
+  }else{
+    $Query=$_POST['query'];
+  }
+  
   ?>
+
 
   <div class="card">
     <div class="card-block">
       <h4 class="card-title">Query Database: '<a href="/architect/schema/<?php echo $Name; ?>/"><?php echo $Name; ?></a>'</h4>
       <form action="/architect/schema/<?php echo $Name; ?>/?query" method="post">
-        <?php AstriaEditor('SHOW TABLES','query'); ?>
+        <textarea class="form-control"><?php echo $Query; ?></textarea>
         <input type="submit" class="btn btn-success" value="Run">
       </form>
     </div>
   </div>
 
-
   <div class="card">
     <div class="card-block">
       <h4 class="card-title">Results</h4>
-      <?php 
-        if(!(isset($_POST['query']))){
-          $_POST['query']='SHOW TABLES';
-        }
-        echo ArrTabler(Query($Query,$Alias)); 
-      ?>
+      <?php echo ArrTabler(Query($Query,$Alias)); ?>
     </div>
   </div>
 
