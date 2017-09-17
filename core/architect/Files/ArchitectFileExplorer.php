5a2595bcfe5cc3230326f65da5d3ff280f86abe2
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-16 20:19:15 -0700

    Update ArchitectFileExplorer.php

--- core/architect/Files/ArchitectFileExplorer.php
+++ core/architect/Files/ArchitectFileExplorer.php
@@ -71,6 +71,7 @@ function ArchitectFileExplorerFile(){
     case 'mov':
     case 'flv':
     case 'mpeg':
+    case 'wmv':
       //TODO make some way of opening a relative path
       $AbsolutePath = '';
       echo '<div class="card"><div class="card-block"><div class="card-text"><video controls preload><source src="'.$AbsolutePath.'"></video></div></div></div>';
