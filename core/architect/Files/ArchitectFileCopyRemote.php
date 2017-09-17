309df129e2145d382450476b2ef925f74d328914
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-16 18:36:13 -0700

    Update ArchitectFileCopyRemote.php

--- core/architect/Files/ArchitectFileCopyRemote.php
+++ core/architect/Files/ArchitectFileCopyRemote.php
@@ -6,15 +6,25 @@ function ArchitectFileCopyRemote(){
 
 function ArchitectFileCopyRemoteBodyCallback(){
   if(isset($_POST['url'])){
-  
+    
+    $Source = $_POST['url'];
+    $Destination = $_POST['destination'];
+    
+    if(is_dir($Destination)){
+      $URL = parse_url($Source);
+      pd($URL);
+    }
+    
+    //copy($Source,$Destination);
     return;
+    
   }
   ?>
   
   <h1>Copy Remote File</h1>
   <form action="/architect/files/copy-remote/" method="get" class="form">
     <input type="text" class="form-control" name="url" id="url" placeholder="URL"><br>
-    <input type="text" class="form-control" name="url" id="url" value="<?php echo $_SERVER['DOCUMENT_ROOT'].$_GET['path'] ?>"><br>
+    <input type="text" class="form-control" name="destination" id="destination" value="<?php echo $_SERVER['DOCUMENT_ROOT'].$_GET['path'] ?>"><br>
     <input type="submit" class="btn btn-block btn-success" value="Create File">
   </form>
   
