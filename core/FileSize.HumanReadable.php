1e821a2bc83dbbe17b98d0140344bb1cea3361cc
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-16 19:24:56 -0700

    Update FileSize.HumanReadable.php

--- core/FileSize.HumanReadable.php
+++ core/FileSize.HumanReadable.php
@@ -4,10 +4,10 @@
   From https://secure.php.net/manual/en/function.filesize.php#120250
 */
 
-function hFileSize($File){
+function hFileSize($File,$Precision = 2){
   $bytes = filesize($File);
   
   $factor = floor((strlen($bytes) - 1) / 3);
   if ($factor > 0) $sz = 'KMGT';
-  return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor - 1] . 'B';
+  return sprintf("%.{$Precision}f", $bytes / pow(1024, $factor)) . @$sz[$factor - 1] . 'B';
 }
