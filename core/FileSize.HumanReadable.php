15de6619c9b6edf575e2dd2745a272d1199d9835
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-16 19:24:18 -0700

    Update FileSize.HumanReadable.php

--- core/FileSize.HumanReadable.php
+++ core/FileSize.HumanReadable.php
@@ -5,6 +5,8 @@
 */
 
 function hFileSize($File){
+  $bytes = filesize($File);
+  
   $factor = floor((strlen($bytes) - 1) / 3);
   if ($factor > 0) $sz = 'KMGT';
   return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor - 1] . 'B';
