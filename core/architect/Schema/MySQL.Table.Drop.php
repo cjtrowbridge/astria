2120b7bde66e5dbb6c93b421e00dc79f1c4ad34d
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-18 16:03:22 -0700

    Update MySQL.Table.Drop.php

--- core/architect/Schema/MySQL.Table.Drop.php
+++ core/architect/Schema/MySQL.Table.Drop.php
@@ -6,13 +6,11 @@ function MySQLTableDropConfirmed($Alias,$Table){
 
 function MySQLTableDrop($Alias,$Table){
   globla $ASTRIA;
-  
 ?>
   <div class="container">
   <div class="row no-gutters">
     <div class="col-xs-12">
-      <h1><a href="/architect">Architect</a> / <a href="/architect/schema">Schema</a> / '<a href="/architect/schema/<?php echo $Alias; ?>/"><?php echo $ASTRIA['databases'][$Alias]['name']; ?></a>'.'<?php echo $Table; ?>'</h1> 
-      <h2>Truncate Table</h2>
+      <h1><a href="/architect">Architect</a> / <a href="/architect/schema">Schema</a> / '<a href="/architect/schema/<?php echo $Alias; ?>/"><?php echo $ASTRIA['databases'][$Alias]['name']; ?></a>' TRUNCATE '<?php echo $Table; ?>'</h1> 
       <p>THIS WILL DROP THE TABLE.</p>
       <form action="/architect/schema/<?php echo $Alias; ?>/table/<?php echo $Table; ?>/" method="post">
         <p>Type "drop" into the box and click submit in order to drop the table.</p>
