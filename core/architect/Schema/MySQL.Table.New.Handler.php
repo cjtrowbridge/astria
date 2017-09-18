12179574009b4fb9a37644e05638e8cb3ad83938
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-18 14:37:14 -0700

    Update MySQL.Table.New.Handler.php

--- core/architect/Schema/MySQL.Table.New.Handler.php
+++ core/architect/Schema/MySQL.Table.New.Handler.php
@@ -1,8 +1,37 @@
 <?php
 
-function MySQLTableNewHandler(){
+function MySQLTableNewHandler($Alias){
   if(isset($_POST['newTableName'])){
     pd($_POST);
+    //TODO validate $_POST['newTableName']
+    $SQL="
+      CREATE TABLE `".$_POST['newTableName']."` (
+        `".$_POST['newTableName']."ID` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        
+    ";
+    
+    if($_GET['commonColumnNameVarchar255']=='true'){
+      $SQL.="
+        `Name` varchar(255) DEFAULT NULL,";
+    }
+    
+    if($_GET['commonColumnDescriptionText']=='true'){
+      $SQL.="
+        `Description` text,";
+    }
+    
+    
+    //Remove Any Leading Comma If Present
+    $SQL=rtrim($SQL,',');
+    
+    $SQL.="
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    ";
+    
+    pd($SQL);
+    Query($SQL,$Alias);
+    
+    
     exit;
   }
 }
