57ae851a505249baa01de3acc380d37c82c5137b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-18 16:00:35 -0700

    Update MySQL.Table.Drop.php

--- core/architect/Schema/MySQL.Table.Drop.php
+++ core/architect/Schema/MySQL.Table.Drop.php
@@ -5,5 +5,24 @@ function MySQLTableDropConfirmed($Alias,$Table){
 }
 
 function MySQLTableDrop($Alias,$Table){
-  die('TODO');
+  
+  
+?>
+  <div class="container">
+  <div class="row no-gutters">
+    <div class="col-xs-12">
+      <h1>Truncate Table</h1>
+      <p>THIS WILL DROP THE TABLE.</p>
+      <form action="/architect/schema/<?php echo $Alias; ?>/table/<?php echo $Table; ?>/" method="post">
+        <p>Type "drop" into the box and click submit in order to drop the table.</p>
+        <input class="form-control" type="text" name="drop" id="drop" autocomplete="off"><br>
+        <input type="submit" class="btn btn-block btn-danger">
+      </form>
+    </div>
+  </div>
+</div>
+<script>
+  $('#drop').focus();
+</script>
+  <?php
 }
