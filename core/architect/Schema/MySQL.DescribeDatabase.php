bb83bf5ea7f532b984ea72b614a3c75ae373496a
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-18 13:36:29 -0700

    Update MySQL.DescribeDatabase.php

--- core/architect/Schema/MySQL.DescribeDatabase.php
+++ core/architect/Schema/MySQL.DescribeDatabase.php
@@ -7,8 +7,8 @@ function MySQLDescribeDatabase($Alias){
   $Tables = Query('SHOW TABLES',$Alias);
   
   echo '<h1>'."'".$Name."'".' </h1>';
-  echo '<p><a class="btn btn-success" href="/architect/schema/'.$Alias.'/?query">Run A Query</a></p>';
-  echo PHP_EOL."<ul>".PHP_EOL;
+  echo '<p><a class="btn btn-outline-success" href="/architect/schema/'.$Alias.'/?query">Run A Query</a></p>';
+  echo PHP_EOL."<h4>Tables:</h4><ul>".PHP_EOL;
   foreach($Tables as $Table){
     $Table = array_shift($Table);
     echo '  <li><a href="/architect/schema/'.path(2).'/table/'.$Table.'">'.$Table.'</a></li>'.PHP_EOL;
