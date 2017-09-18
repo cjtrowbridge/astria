b4fb1a5fee3550295d575eb41ae6ee565ff63bca
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-18 14:28:28 -0700

    Update MySQL.Table.New.php

--- core/architect/Schema/MySQL.Table.New.php
+++ core/architect/Schema/MySQL.Table.New.php
@@ -7,15 +7,17 @@ function MySQLTableNew($Alias){
     <div class="container no-gutters">
       <div class="row no-gutters">
         <div class="col-xs-12">
+          
           <h1>New Table</h1>
           <p>Object Name: <span class="text-muted">(This is the name of the table. The name of its primary ID will be this, suffixed with ID.)</span></p>
           <input type="text" class="form-control" name="newTableName" id="newTableName" placeholder="Cat">
           
-          <h4>Common Columns:</h4>
+          <h4>Include Common Columns?</h4>
           <p><label><input type="checkbox" name="commonColumnNameVarchar255" value="true"> Name (Size 255)</label></p>
           <p><label><input type="checkbox" name="commonColumnDescriptionText" value="true"> Description (Size 64k)</label></p>
         
           <input type="submit" class="form-control" value="Create">
+          
         </div>
       </div>
     </div>
