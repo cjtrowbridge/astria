7f08c00e9be2e7348e5803de553a49e34c9a6276
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-18 15:52:10 -0700

    Update MySQL.Table.Truncate.php

--- core/architect/Schema/MySQL.Table.Truncate.php
+++ core/architect/Schema/MySQL.Table.Truncate.php
@@ -5,5 +5,24 @@ function MySQLTableTruncateConfirmed($Alias,$Table){
 }
 
 function MySQLTableTruncate($Alias,$Table){
-  die('TODO');
+  ?>
+
+<div class="container">
+  <div class="row no-gutters">
+    <div class="col-xs-12">
+      <h1>Truncate Table</h1>
+      <p>THIS WILL DUMP ALL DATA IN THE TABLE.</p>
+      <form action="/architect/schema/<?php echo $Alias; ?>/table/<?php echo $Table; ?>/" method="get">
+        <p>Type "truncate" into the box and click submit in order to truncate the table.</p>
+        <input class="form-control" type="text name="truncate" id="truncate"><br>
+        <input type="submit class="btn btn-danger">
+      </form>
+    </div>
+  </div>
+</div>
+<script>
+  $('#truncate').focus();
+</script>
+
+<?php
 }
