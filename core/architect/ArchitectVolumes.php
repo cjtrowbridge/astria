0dc519c418f28d3a9c16eb84543c2b47dfbf5bda
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-23 11:10:56 -0700

    Create ArchitectVolumes.php

---
+++ core/architect/ArchitectVolumes.php
@@ -0,0 +1,14 @@
+<?php
+
+function ArchitectVolumes(){
+  TemplateBootstrap4('Volmes - Architect' , 'ArchitectVolumesBodyCallback();');
+}
+
+function ArchitectVolumesBodyCallback(){
+  ?><h1><a href="/architect">Architect</a> / <a href="/architect/volumes">Volumes</a></h1>
+  
+  <h2>df -h</h2>
+  <pre><?php passthru('df -h'); ?></pre>
+    
+  <?php
+}
