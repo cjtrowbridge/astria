3dacd6a22f2781bb2b13b4df2ee26680d56ab945
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-27 16:55:16 -0700

    Update FeedSyncHUD.php

--- core/architect/FeedSyncUI/FeedSyncHUD.php
+++ core/architect/FeedSyncUI/FeedSyncHUD.php
@@ -19,7 +19,7 @@ function FeedSyncUIBodyCallback(){
   <ul>
     <li><a class="btn btn-outline-success" href="/architect/feedsync/category/add">New Category</a></li>
   <?php
-      $Feeds = Query('SELECT * FROM `Feed` LEFT JOIN FeedCategory ON FeedCategory.FeedCategoryID = Feed.FeedCategoryID ORDER BY Name ASC');
+      $Feeds = Query('SELECT * FROM `FeedCategory` LEFT JOIN `Feed` ON FeedCategory.FeedCategoryID = Feed.FeedCategoryID ORDER BY Name ASC');
       $LastCategory = '';
       foreach($Feeds as $Feed){
         if(!($Feed['Name'] == $LastCategory)){
