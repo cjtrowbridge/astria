06d5a7464bb6fb9be58bcd3149182d76f321679b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-27 15:03:00 -0700

    Update IsAstriaAdmin.php

--- core/IsAstriaAdmin.php
+++ core/IsAstriaAdmin.php
@@ -4,6 +4,15 @@
 function IsAstriaAdmin(){
   //TODO
   global $ASTRIA;
+  if(!(isset($ASTRIA['Session']))){
+   return false; 
+  }
+  if(!(isset($ASTRIA['Session']['User']))){
+   return false; 
+  }
+  if(!(isset($ASTRIA['Session']['User']['UserID']))){
+   return false; 
+  }
   if($ASTRIA['Session']['User']['UserID']==1){
     return true;
   }else{
