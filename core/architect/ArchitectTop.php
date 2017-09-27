11882504017799a92f5447a1874c6d0d3d6e7784
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-27 10:37:20 -0700

    Update ArchitectTop.php

--- core/architect/ArchitectTop.php
+++ core/architect/ArchitectTop.php
@@ -8,7 +8,7 @@ function ArchitectTopBodyCallback(){
   ?><h1><a href="/architect">Architect</a> / <a href="/architect/top">top</a></h1>
   
   <h2>df -h</h2>
-  <pre><?php passthru(passthru('/usr/bin/top -b -n 1'); ?></pre>
+  <pre><?php passthru('/usr/bin/top -b -n 1'); ?></pre>
     
   <?php
 }
