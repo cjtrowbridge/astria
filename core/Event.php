600a999886fda6bd2daf0bfe57698e7c76d5c34a
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-03 15:15:55 -0700

    Update Event.php

--- core/Event.php
+++ core/Event.php
@@ -168,7 +168,7 @@ function DebugShowSummary(){
 
 
 Hook('Hourly Webhook', 'DebugServiceDumpToDatabase();' );
-Hook('Architect Homepage', 'DebugServiceDumpToDatabase();' );
+//Hook('Architect Homepage', 'DebugServiceDumpToDatabase();' );
 
 function DebugServiceDumpToDatabase(){
   
