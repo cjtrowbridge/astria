7692b95c265b5eabfe953295bbcbd5ae3d5ee9b4
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-05 10:29:05 -0700

    Update DefaultViews.NoView.php

--- core/DefaultViews.NoView.php
+++ core/DefaultViews.NoView.php
@@ -11,8 +11,8 @@ function DefaultViewNoViewBodyCallback(){
   echo '<h1>404: Page not found.</h1>';
   echo 'User '.$ASTRIA['Session']['User']['Email'].' is logged in, but nothing happened at this url.<br>';
   echo 'Runtime '.round(microtime(true)-$DEBUG[0]['timestamp'],4)." seconds.<br>";
-  echo 'Ran '.$NUMBER_OF_QUERIES_RUN.' Database Queries.<br>';
-  echo 'Ran '.$NUMBER_OF_QUERIES_RUN_FROM_DISK_CACHE.' Queries From Disk Cache.<br>';
+  //echo 'Ran '.$NUMBER_OF_QUERIES_RUN.' Database Queries.<br>';
+  //echo 'Ran '.$NUMBER_OF_QUERIES_RUN_FROM_DISK_CACHE.' Queries From Disk Cache.<br>';
   echo 'Session Expires '.date('r',$ASTRIA['Session']['Auth']['Expires']).'.<br>';
   echo '<a href="/?logout">Log Out</a>.';
 }
