18191fbe71b19a8e2108dfc6ddfacca6b51d9985
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-05 10:36:27 -0700

    Update ArchitectPluginManager.php

--- core/architect/ArchitectPluginManager.php
+++ core/architect/ArchitectPluginManager.php
@@ -2,5 +2,52 @@
 
 function ArchitectPluginManager(){
 
+  TemplateBoostrap4('Plugin Manager','ArchitectPluginManagerBodyCallback();');
 }
 
+function ArchitectPluginManagerBodyCallback(){
+  global $ASTRIA;
+  ?><h2>Plugins</h2>
+
+<?php
+  foreach($ASTRIA['plugin'] as $Index => $Plugin){
+    ?>
+
+<div class="card">
+  <div class="card-block">
+    <h4 class="card-title"><a href="/architect/files/?path=/plugins/<?php echo $Index; ?>/"><?php echo $Plugin['name']; ?></a></h4>
+    <div class="card-text">
+      <p><b>State:</b> <?php 
+    
+      switch($Plugin['state']){
+        case 'test':
+          echo 'Integration test is pending...';
+          break;
+        case 'ready':
+          echo '<b style="color: green;">Passed Integration Test, Ready.</b> (<a href="/?enablePlugin='.$Index.'">Activate Plugin</a>)';
+          break;
+        case 'enabled':
+          echo 'Enabled, Running Normally. (<a href="/?disablePlugin='.$Index.'">Disable</a>)';
+          break;
+        case 'broken':
+          echo '<b style="color: red;">Integration Test Failed</b> (<a href="/?resetTest='.$Index.'">Test Again</a>) or (<a href="/?overrideTest='.$Index.'">Override Test</a>)';
+          break;
+      }
+    
+      ?></p>
+      <?php
+        $Command = 'grep -o -r "TODO" '.$_SERVER["DOCUMENT_ROOT"].'/plugins/'.$Index.' | wc -l';
+        $Todos = shell_exec($Command);
+      if($Todos>0){
+        ?>
+        <p>Number of TODOs: <?php echo $Todos; ?></p>
+        <?php
+      }
+      ?>
+    </div>
+  </div>
+</div>
+
+    <?php
+  }
+}
