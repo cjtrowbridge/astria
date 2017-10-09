7bc68d06788e497317e3ee74fc65d9f4d0aaf2c5
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-09 14:54:21 -0700

    Update ArchitectHomepage.php

--- core/architect/ArchitectHomepage.php
+++ core/architect/ArchitectHomepage.php
@@ -51,25 +51,7 @@ function ArchitectBodyCallback(){
 </div>
 
 
-<?php
-  
-$Local  = gitLocalHash();
-$Global = gitGlobalHash();
-
-if(!($Local==$Global)){
-  ?>
-    <div class="row">
-      <div class="col-xs-12">
-        <h2>Updates are Available!</h2>
-        <a href="<?php echo $ASTRIA['app']['appURL'].'/?'.urlencode(BlowfishEncrypt('Pull Mainline From Github')); ?>">Pull Mainline From Github, pending an integration test.</a>
-        <p>Local Master Head is at: <?php echo $Local; ?></p>
-        <p>Global Master Head is at: <?php echo $Global; ?></p>
-      </div>
-    </div>
-  <?php
-}
-  
-?>
+<?php CheckForUpdates(); ?>
 
 
 <div class="row">
