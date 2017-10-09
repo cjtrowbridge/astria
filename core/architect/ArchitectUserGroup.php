aa3bad3efae7956667794b7122c8acc0e858315f
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-09 14:06:03 -0700

    Update and rename ArchitectGroup.php to ArchitectUserGroup.php

---
+++ core/architect/ArchitectUserGroup.php
@@ -0,0 +1,21 @@
+<?php
+
+function ArchitectUserGroup(){
+  
+  TemplateBootstrap4('User Group - Architect','ArchitectUserBodyCallback();'); 
+}
+function ArchitectUserGroupBodyCallback(){
+  ?>
+  
+  <h1>User Group</h1>
+  
+  <div class="card">
+    <div class="card-block">
+      <div class="card-text">
+        <?php echo ArrTabler(Query("SELECT * FROM UserGroup")); ?>
+      </div>
+    </div>
+  </div>
+  
+  <?php
+}
