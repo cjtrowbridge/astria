ebcf86c0217f511f6e2c2af66f5f120634eb9b42
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-09 13:47:24 -0700

    Update ArchitectGitWebhooks.php

--- core/architect/ArchitectGitWebhooks.php
+++ core/architect/ArchitectGitWebhooks.php
@@ -6,52 +6,64 @@ function ArchitectGitWebhooks(){
 }
 
 function ArchitectGitWebhooksBodyCallback(){
+  global $ASTRIA;
 
 ?>
 
- <div id="PullMainlineWebhook">
-    <h2>Webhook: Pull Mainline from Github</h2>
-    <a target="_blank" href="<?php echo $ASTRIA['app']['appURL'].'/?'.urlencode(BlowfishEncrypt('Pull Mainline From Github')); ?>"><?php echo $ASTRIA['app']['appURL'].'/?'.urlencode(BlowfishEncrypt('Pull Mainline From Github')); ?></a>
+<div class="card">
+  <div class="card-block">
+    <div class="card-text">
+      <div id="PullMainlineWebhook">
+        <h2>Webhook: Pull Mainline from Github</h2>
+        <a target="_blank" href="<?php echo $ASTRIA['app']['appURL'].'/?'.urlencode(BlowfishEncrypt('Pull Mainline From Github')); ?>"><?php echo $ASTRIA['app']['appURL'].'/?'.urlencode(BlowfishEncrypt('Pull Mainline From Github')); ?></a>
+      </div>
+    </div>
   </div>
-  
-  <div id="GetSubrepositoryPullWebhook">
-    <h2>Webhook: Pull Subrepository</h2>
-    <form onsubmit="return false;" class="form">
-      <div class="form-group">
-        <div class="input-group">
-          <div>
-            <p>Here are some potential repositories I found in the plugins directory...</p>
-            <ul>
-            <?php
-              $dir = "plugins";
-              if(is_dir($dir)){
-                if($dh = opendir($dir)){
-                  while(($File = readdir($dh)) !== false){
-                    if($File!=='.'&&$File!=='..'&&$File!=='defaultViews'){
-                      echo '<li><a href="javascript:void(0);" onclick="$(\'#subrepositoryPath\').val(\'/plugins/'.$File.'\');GetSubrepositoryPullWebhook();">'.$File.'</a></li>';
+</div>
+<div class="card">
+  <div class="card-block">
+    <div class="card-text">
+      <div id="GetSubrepositoryPullWebhook">
+        <h2>Webhook: Pull Subrepository</h2>
+        <form onsubmit="return false;" class="form">
+          <div class="form-group">
+            <div class="input-group">
+              <div>
+                <p>Here are some potential repositories I found in the plugins directory...</p>
+                <ul>
+                <?php
+                  $dir = "plugins";
+                  if(is_dir($dir)){
+                    if($dh = opendir($dir)){
+                      while(($File = readdir($dh)) !== false){
+                        if($File!=='.'&&$File!=='..'&&$File!=='defaultViews'){
+                          echo '<li><a href="javascript:void(0);" onclick="$(\'#subrepositoryPath\').val(\'/plugins/'.$File.'\');GetSubrepositoryPullWebhook();">'.$File.'</a></li>';
+                        }
+                      }
+                      closedir($dh);
                     }
                   }
-                  closedir($dh);
-                }
-              }
-            ?>
-            </ul>
+                ?>
+                </ul>
+              </div>
+              <input onkeyup="GetSubrepositoryPullWebhook();" type="text" class="form-control" id="subrepositoryPath" placeholder="Enter path to subrepository">
+            </div>
           </div>
-          <input onkeyup="GetSubrepositoryPullWebhook();" type="text" class="form-control" id="subrepositoryPath" placeholder="Enter path to subrepository">
-        </div>
+          <div id="GetSubrepositoryPullWebhookResult"></div>
+        </form>
+        <script>
+          function GetSubrepositoryPullWebhook(){
+            var theURL="/architect/create-webhook-pull-subrepository/?path="+$('#subrepositoryPath').val();
+            console.log('Fetching: '+theURL);
+            $.get(theURL, function(data){
+              $("#GetSubrepositoryPullWebhookResult").html('<a target="_blank" href="'+data+'">'+data+'</a>');
+            });
+          }
+        </script>
       </div>
-      <div id="GetSubrepositoryPullWebhookResult"></div>
-    </form>
-    <script>
-      function GetSubrepositoryPullWebhook(){
-        var theURL="/architect/create-webhook-pull-subrepository/?path="+$('#subrepositoryPath').val();
-        console.log('Fetching: '+theURL);
-        $.get(theURL, function(data){
-          $("#GetSubrepositoryPullWebhookResult").html('<a target="_blank" href="'+data+'">'+data+'</a>');
-        });
-      }
-    </script>
+    </div>
   </div>
+</div>
 
 <?php
 }
