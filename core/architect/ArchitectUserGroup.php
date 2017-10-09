f4a829557adfed466bba6ae34f9e4d8d12741c72
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-09 14:20:32 -0700

    Update ArchitectUserGroup.php

--- core/architect/ArchitectUserGroup.php
+++ core/architect/ArchitectUserGroup.php
@@ -2,22 +2,23 @@
 
 function ArchitectUserGroup(){
   
-  TemplateBootstrap4('User Group - Architect','ArchitectUserGroupBodyCallback();'); 
+  TemplateBootstrap4('UserGroup - Architect','ArchitectUserGroupBodyCallback();'); 
 }
 function ArchitectUserGroupBodyCallback(){
   ?>
   
-  <h1>User Group</h1>
+  <h1>UserGroup</h1>
   
   <div class="card">
     <div class="card-block">
       <div class="card-text">
         <?php
+          
           global $ASTRIA;
           $DBName = $ASTRIA['databases']['astria']['database'];
           $TableExists = Query("SELECT count(*) as 'Found' FROM information_schema.tables WHERE table_schema = '$DBName' AND table_name = 'UserGroup';");
           if($TableExists[0]['Found']==0){
-            Query("RENAME TABLE 'Group' TO 'UserGroup'");
+            Query("ALTER TABLE `Group` RENAME `UserGroup`;");
           }
           echo ArrTabler(Query("SELECT * FROM UserGroup")); 
           
