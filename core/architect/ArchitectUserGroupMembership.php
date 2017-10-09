6f0762df4dce4e34bdc2351159b8ecf41d0d28f1
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-09 14:21:48 -0700

    Update ArchitectUserGroupMembership.php

--- core/architect/ArchitectUserGroupMembership.php
+++ core/architect/ArchitectUserGroupMembership.php
@@ -7,12 +7,20 @@ function ArchitectUserGroupMembership(){
 function ArchitectUserGroupMembershipBodyCallback(){
   ?>
   
-  <h1>User Group Membership</h1>
+  <h1>UserGroupMembership</h1>
   
   <div class="card">
     <div class="card-block">
       <div class="card-text">
-        <?php echo ArrTabler(Query("SELECT * FROM UserGroupMembership")); ?>
+        <?php 
+          global $ASTRIA;
+          $DBName = $ASTRIA['databases']['astria']['database'];
+          $TableExists = Query("SELECT count(*) as 'Found' FROM information_schema.tables WHERE table_schema = '$DBName' AND table_name = 'UserMembership';");
+          if($TableExists[0]['Found']==0){
+            Query("ALTER TABLE `UserMembership` RENAME `UserGroupMembership`;");
+          }
+          echo ArrTabler(Query("SELECT * FROM UserGroupMembership")); 
+        ?>
       </div>
     </div>
   </div>
