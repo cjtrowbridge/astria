016bdd43943e62ba30a5a2309e37f6771109cc2b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-09 14:54:59 -0700

    Create CheckForUpdates.php

---
+++ core/architect/CheckForUpdates.php
@@ -0,0 +1,20 @@
+<?php
+
+function CheckForUpdate(){
+  global $ASTRIA;
+  $Local  = gitLocalHash();
+  $Global = gitGlobalHash();
+
+  if(!($Local==$Global)){
+    ?>
+      <div class="row">
+        <div class="col-xs-12">
+          <h2>Updates are Available!</h2>
+          <a href="<?php echo $ASTRIA['app']['appURL'].'/?'.urlencode(BlowfishEncrypt('Pull Mainline From Github')); ?>">Pull Mainline From Github, pending an integration test.</a>
+          <p>Local Master Head is at: <?php echo $Local; ?></p>
+          <p>Global Master Head is at: <?php echo $Global; ?></p>
+        </div>
+      </div>
+    <?php
+  }
+}
