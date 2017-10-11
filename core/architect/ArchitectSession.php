2fe19b6d48cddd1e27166308ba9d0d719278e8a7
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-11 15:11:20 -0700

    Update ArchitectSession.php

--- core/architect/ArchitectSession.php
+++ core/architect/ArchitectSession.php
@@ -7,7 +7,7 @@ function ArchitectSessionBodyCallback(){
   global $ASTRIA;
   ?><h1><a href="/architect">Architect</a> / <a href="/architect/session">Session</a></h1>
   
-  <pre><?php ps($ASTRIA['Session']); ?></pre>
+  <pre><?php pd($ASTRIA['Session']); ?></pre>
     
   <?php
 }
