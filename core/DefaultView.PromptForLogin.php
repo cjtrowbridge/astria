59de9e1eeee87b411ab8817374a91345614ddd48
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-12 16:59:34 -0700

    Update DefaultView.PromptForLogin.php

--- core/DefaultView.PromptForLogin.php
+++ core/DefaultView.PromptForLogin.php
@@ -12,7 +12,7 @@ function PromptForLoginBodyCallback(){
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12">
-				<?php Event('Auth Login Options'); ?>
+				<br><?php Event('Auth Login Options'); ?>
 			</div>
 		</div>
 	</div>
