1524ba3db1b029a71d5dd16eafdaa71bec26778f
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-12 16:41:26 -0700

    Update TableExists.php

--- core/TableExists.php
+++ core/TableExists.php
@@ -8,13 +8,11 @@ function TableExists($Table,$Database = 'astria'){
   $DBName = $ASTRIA['databases'][$Database]['database'];
   $Table = Sanitize($Table);
   $SQL="SELECT count(*) as 'Found' FROM information_schema.tables WHERE table_schema = '$DBName' AND table_name = '".$Table."';";
-  Event($SQL);
   $TableExists = Query($SQL);
-  pd($TableExists);
   
   if($TableExists[0]['Found']>0){
-    return false;
-  }else{
     return true;
+  }else{
+    return false;
   }
 }
