a4519ba2a928eae3057dae60bca8d3e4895ec18e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-16 15:23:46 -0700

    Update IsWaiting.php

--- core/IsWaiting.php
+++ core/IsWaiting.php
@@ -2,6 +2,11 @@
 
 function IsWaiting(){
   global $ASTRIA;
+  
+  if(!(isset($ASTRIA['Session']))){return false;}
+  if(!(isset($ASTRIA['Session']['User']))){return false;}
+  if(!(isset($ASTRIA['Session']['User']['IsWaiting']))){return false;}
+  
   if($ASTRIA['Session']['User']['IsWaiting']==1){
     return true;
   }else{
