5074f94ca418c363f659696303ea2fddc3cc831c
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-16 15:58:55 -0700

    Create Default.WaitingRoom.php

---
+++ core/Default.WaitingRoom.php
@@ -0,0 +1,20 @@
+<?php 
+
+function AstriaWaitingRoom(){
+	TemplateBootstrap4('Waiting','PromptForLoginBodyCallback();');
+}
+function PromptForLoginBodyCallback(){
+	?>
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h1>Waiting</h1>
+        <p>Welcome to the waiting room.</p>
+			</div>
+		</div>
+	</div>
+	
+	<?php
+	  
+}
