bfefd77f9f2124848918cdc15db69b4fd76fa0a3
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-16 13:56:53 -0700

    Create Main.php

---
+++ core/patches/Main.php
@@ -0,0 +1,21 @@
+<?php
+
+Hook('Done Updating Astria','AstriaPatches();');
+
+function AstriaPatches(){
+
+ if($handle = opendir('core/patches')){
+    while (false !== ($class = readdir($handle))){
+      $include_path='core/patches/'.$class;
+      if((!(strpos($class,'.php')===false)) && $class != "." && $class != ".." && file_exists($include_path)){
+        Event('Before Loading Patch: '.$include_path);
+        include_once($include_path);
+        Event('After Loading Patch: '.$include_path);
+      }
+    }
+    closedir($handle);
+  }
+  
+  Event('Run Astria Patches');
+
+}
