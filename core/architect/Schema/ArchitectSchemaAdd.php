296ca118ed73ebb092403602673e2e6b7a916980
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-18 10:23:33 -0700

    Update ArchitectSchemaAdd.php

--- core/architect/Schema/ArchitectSchemaAdd.php
+++ core/architect/Schema/ArchitectSchemaAdd.php
@@ -2,7 +2,34 @@
 
 function ArchitectSchemaAdd(){
   if(isset($_POST['dbHost'])){
-    pd($_POST);
+    
+    $NewAlias = preg_replace("/^[a-zA-Z0-9_]+([-.][a-zA-Z0-9_]+)*$/", "", $_POST['dbAlias']);
+    if($NewAlias=='astria'){
+      die('Nonprimary schema can not be aliased to "astria".');
+    }
+    
+    global $ASTRIA;
+    $newSchemaFile="<?php ".PHP_EOL."global \$ASTRIA;".PHP_EOL;
+    foreach($ASTRIA['databases'] as $Index => $Plugin){
+      if($Index != 'astria'){
+        $newSchemaFile.=PHP_EOL.PHP_EOL.
+          "\$ASTRIA['databases']['".$NewAlias."'] = array(";
+          "  'type'                     => 'mysql',".PHP_EOL.
+          "  'hostname'                 => 'localhost',".PHP_EOL.
+          "  'username'                 => 'secscidata',".PHP_EOL.
+          "  'password'                 => 'fzTRbSZWa7Rpjjeg',".PHP_EOL.
+          "  'database'                 => 'securities.science.data',".PHP_EOL.
+          "  'resource'                 => false".PHP_EOL.
+          ");".PHP_EOL.PHP_EOL;
+      }
+    }
+    $result=false;
+    //$result=file_put_contents('schema.php', $newSchemaFile);
+    if($result==false){
+     die("Could not write schema config file. Please give write permission or copy the following into a new schema.php file;\n\n".$newSchemaFile); 
+    } 
+    
+    header('Location: /architect/schema');
     exit;
   }
   TemplateBootstrap4('Add Schema','ArchitectSchemaAddBodyCallback();');
@@ -17,6 +44,12 @@ function ArchitectSchemaAddBodyCallback(){
       <div class="col-xs-10">
         <input class="form-control" type="text" name="dbHost" value="localhost">
         <small class="form-text text-muted">This is usually localhost, but it cann be any hostname, IP, etc..</small>
+      </div>
+    </div>
+    <div class="form-group row">
+      <label class="col-xs-2 col-form-label">Database Alias:</label>
+      <div class="col-xs-10">
+        <input class="form-control" type="text" name="dbAlias" id="dbAlias">
       </div>
     </div>
     <div class="form-group row">
@@ -44,7 +77,7 @@ function ArchitectSchemaAddBodyCallback(){
     </div>
   </div>
   <script>
-    $('#dbUsername').focus();
+    $('#dbAlias').focus();
   </script>
 <?php
 }
