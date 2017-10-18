9605d5d10dffd8b05c2cebfa3fe4c6ec7d44a774
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-18 09:58:39 -0700

    Update SchemaRouter.php

--- core/SchemaRouter.php
+++ core/SchemaRouter.php
@@ -4,6 +4,7 @@ Hook('User Is Logged In - Presentation','SchemaRouter();');
 
 function SchemaRouter(){
   if(
+    !IsWaiting() &&
     path(0)&&
     path(0)!='astria'&&
     path(0)!='architect'&&
