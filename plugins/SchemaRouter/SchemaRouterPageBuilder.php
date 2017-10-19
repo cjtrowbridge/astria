c2dfa21e6f424c5a65ad1519ef5451b7e50b7db8
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-18 17:42:29 -0700

    Create SchemaRouterPageBuilder.php

---
+++ plugins/SchemaRouter/SchemaRouterPageBuilder.php
@@ -0,0 +1,41 @@
+<?php
+
+function SchemaRouterPageBuilder($Schema = false, $Table = false){
+  if($Table == false){
+    
+    //we are displaying $Schema
+    $Title   = $Schema;
+    $Content = SchemaRouterPageBuilderGetSchemaPageContents($Schema);
+    $Event = 'SchemaRouter: View_Schema_'.$Schema;
+    
+  }else{
+    
+    //We are displaying $Table
+    $Title   = $Table;
+    $Content = SchemaRouterPageBuilderGetTablePageContents($Schema, $Table);
+    $Event = 'SchemaRouter: View_Schema_'.$Schema.'_View_Table_'.$Table;
+    
+  }
+  
+  $Page = PHP_EOL."<h1><a href=\"\">".$Title."</a></h1>".PHP_EOL;
+  $Page .="  <div class="card">".PHP_EOL;
+  $Page .="    <div class="card-block">".PHP_EOL;
+  $Page .="      <div class="card-text">".PHP_EOL.PHP_EOL;
+  $Page .="        <?php echo SchemaRouterPageField('".$Schema."', '".$Table."',array(".$Content.")); ?>".PHP_EOL.PHP_EOL;
+  $Page .="      </div>".PHP_EOL;
+  $Page .="    </div>".PHP_EOL;
+  $Page .="  </div>".PHP_EOL;
+  
+  
+  
+}
+
+function SchemaRouterPageBuilderGetSchemaPageContents($Schema){
+  $Data = Query("SELECT TABLE_SCHEMA,TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = '".Sanitize($Schema)."'");
+  return var_export($Data,true);
+}
+
+function SchemaRouterPageBuilderGetTablePageContents($Schema, $Table){
+  $Data = Query("SELECT TABLE_SCHEMA,TABLE_NAME,COLUMN_NAME,DATA_TYPE FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '".Sanitize($Schema)."' AND TABLE_NAME = '".Sanitize($Table)."'");
+  return var_export($Data,true);
+}
