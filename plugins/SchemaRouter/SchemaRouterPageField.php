e16f7026e74a57b1d4d6e48738d33ad0d75af435
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-18 19:42:38 -0700

    Update SchemaRouterPageField.php

--- plugins/SchemaRouter/SchemaRouterPageField.php
+++ plugins/SchemaRouter/SchemaRouterPageField.php
@@ -1,6 +1,16 @@
 <?php
 
 function SchemaRouterPageField($Schema,$Table,$Fields){
+  
+  $Schema = Sanitize(path(0));
+  $Table = Sanitize(path(1,false));
+  $Key = intval(path(2));
+  if($Key){
+    $Table = Sanitize($Table);
+    $SQL = "SELECT * FROM `".$Table."` WHERE ".$Table."ID = ".$Key;
+    $Row = Query($SQL,$Schema);
+  }
+  
   $R  = PHP_EOL;
   foreach($Fields as $Field){
     $R .= '  <div class="row">'.PHP_EOL;
@@ -8,13 +18,17 @@ function SchemaRouterPageField($Schema,$Table,$Fields){
     $R .= '      '.$Field['COLUMN_NAME'].PHP_EOL;
     $R .= '    </div>'.PHP_EOL;
     $R .= '    <div class="col-xs-12 col-lg-10">'.PHP_EOL;
-    $R .= '      '.$Field['DATA_TYPE'].PHP_EOL;
+    
+    switch($Field['DATA_TYPE']){
+      default:
+      //case 'varchar':
+        echo $Row[$Field['COLUMN_NAME']];
+        break;
+    }
+    
     $R .= '    </div>'.PHP_EOL;
     $R .= '  </div>'.PHP_EOL;
-
-
-//    '.var_export($Field,true).'</p>';
-
+    $R .= '  <!--'.PHP_EOL.var_export($Field,true).PHP_EOL.'-->'.PHP_EOL.PHP_EOL;
   }
   return $R;
 }
