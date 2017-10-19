bc90b9c52a803c2a540325ea1d95863bbc6011fb
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-18 20:10:34 -0700

    Update SchemaRouterPageField.php

--- plugins/SchemaRouter/SchemaRouterPageField.php
+++ plugins/SchemaRouter/SchemaRouterPageField.php
@@ -9,21 +9,32 @@ function SchemaRouterPageField($Schema,$Table,$Fields){
     $Table = Sanitize($Table);
     $SQL = "SELECT * FROM `".$Table."` WHERE ".$Table."ID = ".$Key;
     $Row = Query($SQL,$Schema);
+    if(!(isset($Row[0]))){
+      $Key=0;
+    }else{
+      $Row=$Row[0];
+    }
   }
   
   $R  = PHP_EOL;
   foreach($Fields as $Field){
+    
+    $Value='';
+    if($Key){
+      $Value = $Row[$Field['COLUMN_NAME']];
+    }
+    
     $R .= '  <div class="row">'.PHP_EOL;
     $R .= '    <div class="col-xs-12 col-lg-2">'.PHP_EOL;
     $R .= '      '.$Field['COLUMN_NAME'].PHP_EOL;
     $R .= '    </div>'.PHP_EOL;
-    $R .= '    <div class="col-xs-12 col-lg-10">'.PHP_EOL;
+    $R .= '    <div class="col-xs-12 col-lg-10"><input type="text" name="'.$Field['COLUMN_NAME'].'" value="'.$Value.'">'.PHP_EOL;
     
     switch($Field['DATA_TYPE']){
       default:
       //case 'varchar':
         if($Key){
-          echo $Row[$Field['COLUMN_NAME']];
+          //echo $Row[$Field['COLUMN_NAME']];
         }
         break;
     }
