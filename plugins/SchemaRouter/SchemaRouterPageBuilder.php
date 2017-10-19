a34bbb1584883b3edfc89bcdcd13bd6125cec68a
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-19 16:45:21 -0700

    Update SchemaRouterPageBuilder.php

--- plugins/SchemaRouter/SchemaRouterPageBuilder.php
+++ plugins/SchemaRouter/SchemaRouterPageBuilder.php
@@ -28,7 +28,7 @@ function SchemaRouterPageBuilder($Schema = false, $Table = false){
   
   $Func  = "function ".$Event."(){".PHP_EOL.PHP_EOL;
   $Func .= "?>".PHP_EOL;
-  $Func .="<h1><a href=\"".$Schema."\">".$Schema."</a> / <a href=\"".$Schema."/".$Table."/";
+  $Func .="<h1><a href=\"/".$Schema."/\">".$Schema."</a> / <a href=\"".$Schema."/".$Table."/";
   
   if(SchemaRoute('table')!=false){
     $Func .=$Table."/";
