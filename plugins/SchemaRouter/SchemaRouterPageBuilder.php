232ed4c4f9c3c12c2d4cac9d333e4f739b71e3d5
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-18 18:22:14 -0700

    Update SchemaRouterPageBuilder.php

--- plugins/SchemaRouter/SchemaRouterPageBuilder.php
+++ plugins/SchemaRouter/SchemaRouterPageBuilder.php
@@ -7,22 +7,28 @@ function SchemaRouterPageBuilder($Schema = false, $Table = false){
     Event('SchemaRouter: Building $Schema page...');
     $Title   = $Schema;
     $Content = SchemaRouterPageBuilderGetSchemaPageContents($Schema);
-    $Event = 'SchemaRouter: View_Schema_'.$Schema;
+    $Event = 'SchemaRouter_View_Schema_'.$Schema;
     
   }else{
     
     Event('SchemaRouter: Building $Table page.');
     $Title   = $Table;
     $Content = SchemaRouterPageBuilderGetTablePageContents($Schema, $Table);
-    $Event = 'SchemaRouter: View_Schema_'.$Schema.'_View_Table_'.$Table;
+    $Event = 'SchemaRouter_View_Schema_'.$Schema.'_View_Table_'.$Table;
     
   }
   
-  $Page = PHP_EOL."<h1><a href=\"\">".$Title."</a></h1>".PHP_EOL;
+  
+  
+  $Page = "<?php ".PHP_EOL.PHP_EOL;
+  $Page .= " function ";
+  
+  
+  $Page .="<h1><a href=\"\">".$Title."</a></h1>".PHP_EOL;
   $Page .="  <div class=\"card\">".PHP_EOL;
   $Page .="    <div class=\"card-block\">".PHP_EOL;
   $Page .="      <div class=\"card-text\">".PHP_EOL.PHP_EOL;
-  $Page .="        <?php echo SchemaRouterPageField('".$Schema."', '".$Table."',array(".$Content.")); ?>".PHP_EOL.PHP_EOL;
+  $Page .="        <?php echo SchemaRouterPageField('".$Schema."', '".$Table."',".$Content."); ?>".PHP_EOL.PHP_EOL;
   $Page .="      </div>".PHP_EOL;
   $Page .="    </div>".PHP_EOL;
   $Page .="  </div>".PHP_EOL;
@@ -38,8 +44,6 @@ function SchemaRouterPageBuilderGetSchemaPageContents($Schema){
   
   $SQL="SELECT TABLE_SCHEMA,TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = '".Sanitize($DatabaseName)."'";
   $Data = Query($SQL,$Schema);
-  pd($SQL);
-  pd($Data);
   return var_export($Data,true);
 }
 
@@ -51,7 +55,5 @@ function SchemaRouterPageBuilderGetTablePageContents($Schema, $Table){
   
   $SQL="SELECT TABLE_SCHEMA,TABLE_NAME,COLUMN_NAME,DATA_TYPE FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '".Sanitize($Schema)."' AND TABLE_NAME = '".Sanitize($DatabaseName)."'";
   $Data = Query($SQL,$Schema);
-  pd($SQL);
-  pd($Data);
   return var_export($Data,true);
 }
