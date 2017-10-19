7c4c1ff701b271d72e2026cb5f73b5aee4673414
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-18 19:31:42 -0700

    Update SchemaRouterPageField.php

--- plugins/SchemaRouter/SchemaRouterPageField.php
+++ plugins/SchemaRouter/SchemaRouterPageField.php
@@ -5,10 +5,10 @@ function SchemaRouterPageField($Schema,$Table,$Fields){
   foreach($Fields as $Field){
     $R .= '  <div class="row">'.PHP_EOL;
     $R .= '    <div class="col-xs-12 col-lg-2">'.PHP_EOL;
-    $R .= '      '$Field['COLUMN_NAME'].PHP_EOL;
+    $R .= '      '.$Field['COLUMN_NAME'].PHP_EOL;
     $R .= '    </div>'.PHP_EOL;
     $R .= '    <div class="col-xs-12 col-lg-10">'.PHP_EOL;
-    $R .= '      '$Field['DATA_TYPE'].PHP_EOL;
+    $R .= '      '.$Field['DATA_TYPE'].PHP_EOL;
     $R .= '    </div>'.PHP_EOL;
     $R .= '  </div>'.PHP_EOL;
 
