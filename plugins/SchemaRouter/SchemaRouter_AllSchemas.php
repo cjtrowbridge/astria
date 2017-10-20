9ef5a187fafe5446d2c3862628dd586df59516a4
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-20 15:16:16 -0700

    Create SchemaRouter_AllSchemas.php

---
+++ plugins/SchemaRouter/SchemaRouter_AllSchemas.php
@@ -0,0 +1,8 @@
+<?php
+
+function SchemaRouter_AllSchemas(){
+  //this will always simply be a list of all the schemas this user has permission to view
+  //this could be json
+  //or the contents of a dom object
+  //default to a full page with template
+}
