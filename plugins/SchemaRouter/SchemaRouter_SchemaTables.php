943df667a17d62a0c0f38265ca9dfecc1bb9116c
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-20 15:17:25 -0700

    Create SchemaRouter_SchemaTables.php

---
+++ plugins/SchemaRouter/SchemaRouter_SchemaTables.php
@@ -0,0 +1,10 @@
+<?php
+
+function SchemaRouter_SchemaTables($Schema){
+  //this could be an insert of a new table
+  //or an update to a table's structure
+  //default to returning a list of all tables in the given schema
+    //this could be json
+    //or the contents of a dom object
+    //default to a full page with template
+}
