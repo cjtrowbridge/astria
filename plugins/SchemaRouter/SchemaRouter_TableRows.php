8f2cfb8c52146b61cbdbc62d52ab149f8b347e7b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-20 15:17:47 -0700

    Create SchemaRouter_TableRows.php

---
+++ plugins/SchemaRouter/SchemaRouter_TableRows.php
@@ -0,0 +1,9 @@
+<?php
+
+function SchemaRouter_TableRows($Schema, $Table){
+  //this could be an insert of a new row
+  //deafult to returning a list of rows in the table
+    //this could be json
+    //or the contents of a dom object
+    //default to a full page with template
+}
