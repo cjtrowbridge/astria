8a0fccd4fc6c5ca54da0a7558bbbe3102727132f
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-20 15:19:39 -0700

    Create SchemaRouter_RowFields.php

---
+++ plugins/SchemaRouter/SchemaRouter_RowFields.php
@@ -0,0 +1,9 @@
+<?php
+
+function SchemaRouter_RowFields($Schema, $Table, $Row){
+  //this could be an update to an existing row
+  //or a delete of a row
+  //default to returning the row
+    //this could be json
+    //or the contents of a dom object
+}
