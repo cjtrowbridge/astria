b541100dcda8882310079c9e5fd5aaaa9dc650c6
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-22 17:28:48 -0700

    Update SchemaRouter_AllSchemas.php

--- plugins/SchemaRouter/SchemaRouter_AllSchemas.php
+++ plugins/SchemaRouter/SchemaRouter_AllSchemas.php
@@ -6,3 +6,26 @@ function SchemaRouter_AllSchemas(){
   //or the contents of a dom object
   //default to a full page with template
 }
+
+function SchemaRouter_AllSchemas_DOM(){
+  //this function returns a string which shows a dom ui for accessing each schema which the user has any recursive permission to view
+  $AllSchemas = SchemaRouter_AllSchemas_Array()
+  return '<pre>'.var_export($AllSchemas,true).'</pre>';
+}
+
+function SchemaRouter_AllSchemas_Array(){
+  //this function returns an array of each schema which the user has any recursive permission to view
+  global $ASTRIA;
+  
+  //initialize the output list
+  $Schemas = array();
+  
+  foreach($ASTRIA['databases'] as $Alias => $Database){
+    $PermissionString = 'Schema_'.$Alias.'_';
+    if(HasPermissionBeginingWith($PermissionString)){
+      $Schemas[$Alias]=$Database['name'];
+    }
+  }
+  
+  return $Schemas;
+}
