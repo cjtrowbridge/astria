86629109587c323c65230b8878d5bdc0f822bacf
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-22 17:40:12 -0700

    Update ArchitectMyPermissions.php

--- core/architect/ArchitectMyPermissions.php
+++ core/architect/ArchitectMyPermissions.php
@@ -8,7 +8,7 @@ function ArchitectMyPermissionsBodyCallback(){
   global $ASTRIA;
   ?><h1><a href="/architect">Architect</a> / <a href="/architect/my-permissions">My Permissions</a></h1>
   
-  <pre><?php pd($ASTRIA['Session']['User']['Permissions']); ?></pre>
+  <?php pd($ASTRIA['Session']['User']['Permission']); ?>
     
   <?php
 }
