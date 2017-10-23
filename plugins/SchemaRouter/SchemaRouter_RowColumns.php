e27a911ffb8a4202fe4d5e3ab9b105c38b800b7e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-22 19:24:24 -0700

    Update SchemaRouter_RowColumns.php

--- plugins/SchemaRouter/SchemaRouter_RowColumns.php
+++ plugins/SchemaRouter/SchemaRouter_RowColumns.php
@@ -1,9 +1,9 @@
 <?php
 
 function SchemaRouter_RowColumns($Schema, $Table, $Row){
-  //this could be an update to an existing row
-  //or a delete of a row
-  //default to returning the row
-    //this could be json
-    //or the contents of a dom object
+  //TODO this could be an update to an existing row
+  //TODO or a delete of a row
+  //TODO default to returning the row
+    //TODO this could be json
+    //TODO or the contents of a dom object
 }
