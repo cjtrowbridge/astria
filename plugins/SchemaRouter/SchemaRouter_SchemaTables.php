6a77a127e8f227a5397f53f41e68ebfc279f084c
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-22 18:09:47 -0700

    Update SchemaRouter_SchemaTables.php

--- plugins/SchemaRouter/SchemaRouter_SchemaTables.php
+++ plugins/SchemaRouter/SchemaRouter_SchemaTables.php
@@ -8,3 +8,39 @@ function SchemaRouter_SchemaTables($Schema){
     //or the contents of a dom object
     //default to a full page with template
 }
+
+function SchemaRouter_SchemaTables_DOM_UL($Schema){
+  //this function returns a string which shows a dom ul for accessing each table in this schema which the user has any recursive permission to view
+  $AllSchemaTables = SchemaRouter_SchemaTables_Array($Schema);
+  $R= PHP_EOL.PHP_EOL.'<ul>'.PHP_EOL;
+  foreach($AllSchemaTables as $Table){
+    $R.= '  <li><a href="/'.$Schema.'/'.$Table.'/">'.$Table.'</a></li>'.PHP_EOL;
+  }
+  $R.= PHP_EOL.'</ul>'.PHP_EOL.PHP_EOL;
+  return $R;
+}
+
+function SchemaRouter_SchemaTables_Array($Schema){
+  //this function returns an array of each table in this schema which the user has any recursive permission to view
+  global $ASTRIA;
+  
+  //initialize the output list
+  $R = array();
+  
+  $Tables = Query('SHOW TABLES',$Alias);
+  
+  foreach($Tables as $Table){
+    $Table = array_shift($Table);
+    
+    $PermissionString = 'Schema_'.$Schema.'_Table_'.$Table.'_';
+    Event('Checking If User Has Any Permissions Begining with: '.$PermissionString);
+    if(HasPermissionBeginingWith($PermissionString)){
+      Event('Yes');
+      $R[]=$Table;
+    }else{
+      Event('None found.');
+    }
+  }
+  
+  return $R;
+}
