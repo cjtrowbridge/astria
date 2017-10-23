0af99e584dd15156df51ab7c411a1cf4381d331b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-22 18:52:34 -0700

    Update SchemaRouter_TableRows.php

--- plugins/SchemaRouter/SchemaRouter_TableRows.php
+++ plugins/SchemaRouter/SchemaRouter_TableRows.php
@@ -6,4 +6,12 @@ function SchemaRouter_TableRows($Schema, $Table){
     //this could be json
     //or the contents of a dom object
     //default to a full page with template
+    global $ASTRIA;
+    TemplateBootstrap4($Table.' - '.$ASTRIA['databases'][$Schema]['title'],'SchemaRouter_SchemaTables_DOM_Page("'.$Schema.'","'.$Table.'");');
+}
+function SchemaRouter_TableRows_DOM_Page($Schema,$Table){
+  $Table = Sanitize($Table);
+  global $ASTRIA;
+  echo '<h1><a href="/'.$SChema.'/">'.$ASTRIA['databases'][$Schema]['title'].'</a> / <a href="/'.$SChema.'/'.$Table.'/">'.$Table.'</h1>';
+  echo ArrTabler(Query("SELECT * FROM `".$Table."` ORDER BY 1 DESC",$Schema));
 }
