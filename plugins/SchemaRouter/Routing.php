93d0da3d7d0e66cb918d343bb09661772c099c69
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-22 17:22:47 -0700

    Update Routing.php

--- plugins/SchemaRouter/Routing.php
+++ plugins/SchemaRouter/Routing.php
@@ -6,7 +6,10 @@
 function SchemaRouter_Routing(){
 
   //include DOM functions so views can be included in outside pages
-  include_once('');
+  include_once('SchemaRouter_AllSchemas.php');
+  include_once('SchemaRouter_SchemaTables.php');
+  include_once('SchemaRouter_TableRows.php');
+  include_once('SchemaRouter_RowColumns.php');
   
   //Get Route
   include_once('GetSchemaRoute.php');
@@ -15,19 +18,15 @@ function SchemaRouter_Routing(){
   //call the appropriate router for the route's hierarchical level
   switch($Route['RouteType']){
     case 'allschemas':
-      include_once('SchemaRouter_AllSchemas.php');
       SchemaRouter_AllSchemas();
       break;
     case 'schema':
-      include_once('SchemaRouter_SchemaTables.php');
       SchemaRouter_SchemaTables($Route['Schema']);
       break;
     case 'table':
-      include_once('SchemaRouter_TableRows.php');
       SchemaRouter_TableRows($Route['Schema'],$Route['Table']);
       break;
     case 'row':
-      include_once('SchemaRouter_RowColumns.php');
       SchemaRouter_RowColumns($Route['Schema'],$Route['Table'],$Route['Row']);
       break;
   }
