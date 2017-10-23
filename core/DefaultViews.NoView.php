664bd2128ff5a0da7fae781e586d61b0f4d0f23f
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-22 18:23:27 -0700

    Update DefaultViews.NoView.php

--- core/DefaultViews.NoView.php
+++ core/DefaultViews.NoView.php
@@ -9,10 +9,10 @@ function DefaultViewNoView(){
 function DefaultViewNoViewBodyCallback(){
   global $NUMBER_OF_QUERIES_RUN, $QUERIES_RUN, $DEBUG, $NUMBER_OF_QUERIES_RUN_FROM_DISK_CACHE,$ASTRIA;
   echo '<h1>404: Page not found.</h1>';
-  echo 'User '.$ASTRIA['Session']['User']['Email'].' is logged in, but nothing happened at this url.<br>';
-  echo 'Runtime '.round(microtime(true)-$DEBUG[0]['timestamp'],4)." seconds.<br>";
+  //echo 'User '.$ASTRIA['Session']['User']['Email'].' is logged in, but nothing happened at this url.<br>';
+  //echo 'Runtime '.round(microtime(true)-$DEBUG[0]['timestamp'],4)." seconds.<br>";
   //echo 'Ran '.$NUMBER_OF_QUERIES_RUN.' Database Queries.<br>';
   //echo 'Ran '.$NUMBER_OF_QUERIES_RUN_FROM_DISK_CACHE.' Queries From Disk Cache.<br>';
-  echo 'Session Expires '.date('r',$ASTRIA['Session']['Auth']['Expires']).'.<br>';
-  echo '<a href="/?logout">Log Out</a>.';
+  //echo 'Session Expires '.date('r',$ASTRIA['Session']['Auth']['Expires']).'.<br>';
+  //echo '<a href="/?logout">Log Out</a>.';
 }
