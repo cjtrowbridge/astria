2bc90bd7d5c6fc4f0013f93b0e6d56648efeb3de
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-22 18:41:55 -0700

    Update GetSchemaRoute.php

--- plugins/SchemaRouter/GetSchemaRoute.php
+++ plugins/SchemaRouter/GetSchemaRoute.php
@@ -1,24 +1,40 @@
 <?php
 
-function GetSchemaRoute(){
-  $Schema = path(0,false);
-  $Table  = path(1,false);
-  $Row    = path(2,false);
+function GetSchemaRoute($Schema = false, $Table = false, $Row = false){
   
-  if(!( $Row === false )){
-    $RouteType = 'row';
+  //TODO set global variables for these so that this function can be called to set them, and then later called to recall the values passed into it rather than parsing new values from the path. This way, the default behavior can be overridden.
+  
+  if($Schema == false){
+    $Schema = path(0,false);
+  }
+  if($Table == false){
+    $Table  = path(1,false);
+  }
+  if($Row == false){
+    $Row    = path(2,false);
+  }
+  
+  global $ASTRIA;
+  if(!(isset($ASTRIA['databases'][$Schema]))){
+    $RouteType = false;
+    $Schema    = false;
+    $Table     = false;
+    $Row       = false;
   }else{
-    if(!( $Table === false )){
-      $RouteType = 'table';
+    if(!( $Row === false )){
+      $RouteType = 'row';
     }else{
-      if(!( $Schema === false )){
-        $RouteType = 'schema';
+      if(!( $Table === false )){
+        $RouteType = 'table';
       }else{
-        $RouteType = 'allschemas';
+        if(!( $Schema === false )){
+          $RouteType = 'schema';
+        }else{
+          $RouteType = 'allschemas';
+        }
       }
     }
   }
-  
   return array(
     'Schema'    => $Schema,
     'Table'     => $Table,
