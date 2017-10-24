66cddd5c2523326664a2bf313472fba7650b71ee
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-24 12:04:36 -0700

    Update Routing.php

--- plugins/SmartMailer/Routing.php
+++ plugins/SmartMailer/Routing.php
@@ -2,7 +2,7 @@
 
 Hook('User Is Logged In - Before Presentation','SmartMailerRouting();');
 function SmartMailerRouting(){
-  if(path(0))=='smartmailer'){
+  if(path(0)=='smartmailer'){
     switch(path(1)){
       case false
         include_once('SmartMailer_Homepage.php');
