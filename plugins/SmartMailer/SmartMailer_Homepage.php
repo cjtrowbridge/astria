09e8087a6bed570f11a0677153245e0ada40ea9e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-24 12:03:23 -0700

    Rename plugins/SmartMailer to plugins/SmartMailer/SmartMailer_Homepage.php

---
+++ plugins/SmartMailer/SmartMailer_Homepage.php
@@ -0,0 +1,15 @@
+<?php
+
+function SmartMailer_Homepage(){
+  
+  TemplateBootstrap4('SmartMailer','SmartMailer_Homepage_BodyCallback();');
+}
+
+function SmartMailer_Homepage_BodyCallback(){
+  ?><h1>SmartMailer</h1>
+  
+  <h2><div style="float: right;">New</div>Campaigns</h2>
+  <p><Show a list of campaigns and stats/p>
+  
+  <?php
+}
