9912e208e92f3ff8305e515197bcb12bd3b59600
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-26 12:02:57 -0700

    Update Routing.php

--- plugins/SchemaRouter/Routing.php
+++ plugins/SchemaRouter/Routing.php
@@ -3,6 +3,7 @@
 // example permission 
 //   Schema_firebridgecrm_Table_Customer_Column_BillingZIP
 
+Hook('Challenge Session','SchemaRouter_SchemaDescription(true);');
 
 Hook('User Is Logged In - Before Presentation','SchemaRouter_Routing();');
 Hook('Done Reloading User Permissions Into Session','SchemaRouter_SchemaDescription();');
