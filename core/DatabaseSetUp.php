84f083720430d2f0977376840c93ec3305ecf5e6
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-28 12:42:58 -0700

    Update DatabaseSetUp.php

--- core/DatabaseSetUp.php
+++ core/DatabaseSetUp.php
@@ -17,14 +17,21 @@ function DatabaseSetUp(){
       `Created` datetime NOT NULL,
       `Expires` datetime DEFAULT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=utf16;
-
+    
+    ALTER TABLE `Cache` ADD PRIMARY KEY (`CacheID`), ADD UNIQUE KEY `Hash` (`Hash`) USING BTREE;
+    ALTER TABLE `Cache` MODIFY `CacheID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+    
     CREATE TABLE `UserGroup` (
       `GroupID` int(11) NOT NULL,
       `ParentID` int(11) DEFAULT NULL,
       `Name` varchar(255) NOT NULL,
       `Description` text
     ) ENGINE=InnoDB DEFAULT CHARSET=utf16;
-
+    
+    ALTER TABLE `UserGroup` ADD PRIMARY KEY (`GroupID`), ADD KEY `ParentID` (`ParentID`);
+    ALTER TABLE `UserGroup` MODIFY `GroupID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+    
+    
     CREATE TABLE `Permission` (
       `PermissionID` int(11) NOT NULL,
       `ViewID` int(11) NOT NULL,
@@ -34,7 +41,12 @@ function DatabaseSetUp(){
       `InsertedTime` datetime NOT NULL,
       `InsertedUser` int(11) NOT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=utf16;
-
+    
+    ALTER TABLE `Permission` ADD PRIMARY KEY (`PermissionID`);
+    ALTER TABLE `Permission` MODIFY `PermissionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+    ALTER TABLE `Permission` ADD UNIQUE(`UserID`,`Text`);
+    
+    
     CREATE TABLE `Session` (
       `SessionID` int(11) NOT NULL,
       `SessionHash` varchar(32) NOT NULL,
@@ -45,7 +57,11 @@ function DatabaseSetUp(){
       `UserIP` text NOT NULL,
       `Expires` datetime NOT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=utf16;
-
+    
+    ALTER TABLE `Session` ADD PRIMARY KEY (`SessionID`);
+    ALTER TABLE `Session` MODIFY `SessionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+    
+    
     CREATE TABLE `User` (
       `UserID` int(11) NOT NULL,
       `Email` varchar(255) NOT NULL,
@@ -60,25 +76,18 @@ function DatabaseSetUp(){
       `IsAstriaAdmin` BOOLEAN NOT NULL DEFAULT FALSE,
       `IsWaiting` BOOLEAN NOT NULL DEFAULT TRUE
     ) ENGINE=InnoDB DEFAULT CHARSET=utf16;
-
+    
+    ALTER TABLE `User` ADD PRIMARY KEY (`UserID`);
+    ALTER TABLE `User` MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+    
+    
     CREATE TABLE `UserGroupMembership` (
       `UserMembershipID` int(11) NOT NULL,
       `UserID` int(11) NOT NULL,
       `GroupID` int(11) NOT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=utf16 ROW_FORMAT=COMPACT;
     
-    ALTER TABLE `Cache` ADD PRIMARY KEY (`CacheID`), ADD UNIQUE KEY `Hash` (`Hash`) USING BTREE;
-    ALTER TABLE `Group` ADD PRIMARY KEY (`GroupID`), ADD KEY `ParentID` (`ParentID`);
-    ALTER TABLE `Permission` ADD PRIMARY KEY (`PermissionID`);
-    ALTER TABLE `Permission` MODIFY `PermissionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-    ALTER TABLE `Permission` ADD UNIQUE(`UserID`,`Text`);
-    ALTER TABLE `Session` ADD PRIMARY KEY (`SessionID`);
-    ALTER TABLE `User` ADD PRIMARY KEY (`UserID`);
     ALTER TABLE `UserMembership` ADD PRIMARY KEY (`UserMembershipID`), ADD KEY `UserID` (`UserID`,`GroupID`);
-    ALTER TABLE `Cache` MODIFY `CacheID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-    ALTER TABLE `Group` MODIFY `GroupID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-    ALTER TABLE `Session` MODIFY `SessionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-    ALTER TABLE `User` MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
     ALTER TABLE `UserMembership` MODIFY `UserMembershipID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
   ";
   
