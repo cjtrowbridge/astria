c59d11affbade74be39adfad24c127ca3297a62b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-30 13:53:14 -0700

    Update ArchitectFileSearch.php

--- core/architect/Files/ArchitectFileSearch.php
+++ core/architect/Files/ArchitectFileSearch.php
@@ -46,7 +46,7 @@ function ArchitectFileCopyRemoteBodyCallback(){
     $Query = $_GET['query'];
     $Query = preg_replace("/[^A-Za-z0-9 ]/", "", $Query);
 
-    $Command = 'grep -RHn "'.$Query.'" '.$Path;
+    $Command = 'grep -RHln "'.$Query.'" '.$Path;
     echo '<h2>'.$Command.'</h2>';
     
     $Results = shell_exec($Command);
