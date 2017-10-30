9f02fe702fc8cbda5d5de1ca4857d956f3140e09
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-30 12:47:25 -0700

    Update ArchitectFileSearch.php

--- core/architect/Files/ArchitectFileSearch.php
+++ core/architect/Files/ArchitectFileSearch.php
@@ -1,42 +1,7 @@
 <?php
 
 function ArchitectFileSearch(){
-  if(isset($_GET['query'])){
-    
-    pd($_GET);
-    die('k');
-    
-    $Source = $_POST['url'];
-    $Destination = $_POST['destination'];
-    
-    if(is_dir($Destination)){
-      $URL = parse_url($Source);
-      if(!(isset($URL['path']))){
-        die("<h1>Invalid url. Can't find a filename.</h1>");
-      }
-      $FilePath = $URL['path'];
-      $LastSlashPosition = strrpos($FilePath,'/')+1;
-      
-      if($LastSlashPosition<=0){
-        die("<h1>Invalid url. Can't find a filename.</h1>");
-      }
-      
-      $Filename = substr($FilePath,$LastSlashPosition);
-      
-      $Destination.='/'.$Filename;
-      $Destination=str_replace('//','/',$Destination);
-    }
-    if(file_exists($Destination)){
-      die('<h1>File Already Exists.</h1>');
-    }
-    $Result = copy($Source,$Destination);
-    if($Result){
-      header('Location: /architect/files/?path='.$_POST['redirect']);
-      exit;
-    }else{
-      die('<h1>Error</h1>'); 
-    }
-  }
+  
   TemplateBootstrap4('File Search - Architect','ArchitectFileCopyRemoteBodyCallback();'); 
 }
 function ArchitectFileCopyRemoteBodyCallback(){
@@ -44,10 +9,36 @@ function ArchitectFileCopyRemoteBodyCallback(){
   
   <h1>File Search</h1>
   <form action="/architect/files/search/" method="get" class="form">
-    <input type="text" class="form-control" name="path" id="pat" value="<?php echo $_SERVER['DOCUMENT_ROOT']; if(isset($_GET['path'])){echo $_GET['path'];} ?>"><br>
-    <input type="text" class="form-control" name="query" id="query" placeholder="Query"><br>
+    <label>
+      Path:
+      <input type="text" class="form-control" name="path" id="pat" value="<?php echo $_SERVER['DOCUMENT_ROOT']; if(isset($_GET['path'])){echo $_GET['path'];} ?>"><br>
+    </label>
+    <label>
+      Query: (Only alphanumeric characters and spaces are allowed)
+      <input type="text" class="form-control" name="query" id="query" value="<?php echo if(isset($_GET['query'])){echo $_GET['query'];} ?>" placeholder="Query"><br>
+    </label>
     <input type="submit" class="btn btn-block btn-success" value="File Search">
   </form>
-  
+  <script>
+    $('#query').focus();
+  </script>
+
   <?php
+  
+  if(isset($_GET['query'])){
+    echo '<h2>Search Results</h2>';
+    
+    $Path = $_GET['path'];
+    if(!(is_dir($Path))){
+      echo "Path is not a directory!";
+      exit;
+    }
+    
+    $Query = $_POST['query'];
+    $Query = preg_match('/^[a-z0-9 \-]+$/i', $Query);
+    $Results = shell_exec('grep -R "'.$Query.'"');
+    
+    pd($Results);
+    
+  }
 }
