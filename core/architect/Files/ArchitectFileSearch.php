447eefe5224b77f0279a49b0689b7bb50f2bcdea
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-30 13:31:12 -0700

    Update ArchitectFileSearch.php

--- core/architect/Files/ArchitectFileSearch.php
+++ core/architect/Files/ArchitectFileSearch.php
@@ -32,13 +32,8 @@ function ArchitectFileCopyRemoteBodyCallback(){
     $Path = $_GET['path'];
     $Path = realpath($Path);
     if(!(is_dir($Path))){
-      if(is_dir(realpath($_SERVER['DOCUMENT_ROOT'].$Path))){
-        $Path = realpath($_SERVER['DOCUMENT_ROOT'].$Path);
-        echo '<p>Altering Path To: ('.$_SERVER['DOCUMENT_ROOT'].')('.$Path.')</p>';
-      }else{
-        echo "Path is not a directory: ".$Path;
-        exit;
-      }
+      echo "Path is not a directory: ".$Path;
+      exit;
     }
     
     $Query = $_GET['query'];
