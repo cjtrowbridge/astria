6a3e00fb7f11fc7fdde45f5dc81d03577820e125
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-01 13:02:02 -0700

    Update Page.Home.php

--- plugins/RepoTracker/Page.Home.php
+++ plugins/RepoTracker/Page.Home.php
@@ -1,6 +1,10 @@
 <?php
 
 function RepoTracker_Homepage(){
+  TemplateBootstrap4('RepoTracker','RepoTracker_Homepage_BodyCallback();');
+}
+
+function RepoTracker_Homepage_BodyCallback(){
   ?><h1>ReopTracker</h1>
   show a list of all repositories found locally.
   <?php
