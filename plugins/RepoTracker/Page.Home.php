c47e8101633be88934863a22b1321ea834d0a61f
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-01 14:46:17 -0700

    Update Page.Home.php

--- plugins/RepoTracker/Page.Home.php
+++ plugins/RepoTracker/Page.Home.php
@@ -8,6 +8,7 @@ function RepoTracker_Homepage_BodyCallback(){
   ?><h1><a href="/repotracker">RepoTracker</a></h1>
   <a class="btn btn-success" href="/repotracker/refresh">Refresh</a>
 
+<br>
 <div class="row">
   <div class="col-xs-12">
     <?php echo ArrTabler(Query("SELECT * FROM `Repository`")); ?>
