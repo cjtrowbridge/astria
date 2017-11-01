55b55f731ebb677de6e62218550bdf552c0d3a49
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-01 13:59:18 -0700

    Update Routing.php

--- plugins/RepoTracker/Routing.php
+++ plugins/RepoTracker/Routing.php
@@ -72,7 +72,7 @@ function FindGitRepositoriesRecursive($Path = false){
   if($Handle = opendir( $Path )){
     while (false !== ($File = readdir( $Handle ))){
       $FullPath = $Path . $File;
-      
+      echo '<p>Checking: '.$FullPath.'</p>';
       if( $File != "." && $File != ".." && is_dir( $FullPath )){
         $Temp += FindGitRepositoriesRecursive( $FullPath );
       }
