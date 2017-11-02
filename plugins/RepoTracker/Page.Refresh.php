090bf61a4ba3700a731096cffe5d9d6b0485ef9d
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-01 18:29:01 -0700

    Update Page.Refresh.php

--- plugins/RepoTracker/Page.Refresh.php
+++ plugins/RepoTracker/Page.Refresh.php
@@ -10,7 +10,7 @@ function RepoTracker_Refresh_BodyCallback(){
   <ul>
     <li>Looking for new repositories...     <br> <?php RepoTrackerRefresh(true); ?></li>
     <li>Confirming Local Hashes...          <br> <?php RepoTracker_VerifyLocalHashes(true); ?></li>
-    <li>Confirming remote Hashes...         <br> </li>
+    <li>Confirming remote Hashes...         <br> <?php RepoTracker_VerifyRemoteHashes(true); ?></li>
     <li>Pulling Bleeding Edges...           <br> </li>
     <li>Done.</li>
   </ul>
