b4633ec23d7e7a2a75653ed7d730c468d8f3e342
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-04 15:14:33 -0700

    Update SchemaRouter_RowColumns.php

--- plugins/SchemaRouter/SchemaRouter_RowColumns.php
+++ plugins/SchemaRouter/SchemaRouter_RowColumns.php
@@ -1,6 +1,7 @@
 <?php
 
 function SchemaRouter_RowColumns($Schema, $Table, $Row){
+  global $ASTRIA;
   $PrimaryKey = $ASTRIA['Session']['Schema'][$Schema][$Table]['Primary Key'];
   //TODO this could be an update to an existing row
   //TODO or a delete of a row
