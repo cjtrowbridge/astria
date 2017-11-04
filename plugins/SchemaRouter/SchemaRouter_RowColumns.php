20d1f3e8502b8de0aae55b8e30f3e6396e09d6b2
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-03 17:13:28 -0700

    Update SchemaRouter_RowColumns.php

--- plugins/SchemaRouter/SchemaRouter_RowColumns.php
+++ plugins/SchemaRouter/SchemaRouter_RowColumns.php
@@ -14,14 +14,35 @@ function SchemaRouter_RowColumns($Schema, $Table, $Row){
 
 function SchemaRouter_RowColumns_Fields_BodyCallback($Schema, $Table, $Row){
   
+  pd($Row);
+  
   global $ASTRIA;
-  $Columns = $ASTRIA['Session']['Schema'][$Schema][$Table];
+  $FirstTextField = $ASTRIA['Session']['Schema'][$Schema][$Table]['FirstTextField'];
+  $Columns        = $ASTRIA['Session']['Schema'][$Schema][$Table];
   
   foreach($Columns as $Column){
+    
+    if($Column['IsConstraint']['PRIMARY KEY'] == true){
+      SchemaRouter_RowColumns_Fields_BodyCallback_ReadOnlyWithHidden($FirstTextField, $Column['COLUMN_NAME'], '');
+    }
     
     pd($Column);
     echo '<hr>';
     
   }
   
+}
+
+function SchemaRouter_RowColumns_Fields_BodyCallback_ReadOnlyWithHidden($Label, $Name, $Value = ''){
+  ?>
+
+<div class="form-group row">
+  <label for="<?php echo $Name; ?>" class="col-2 col-form-label"><?php echo $Caption; ?></label>
+  <div class="col-10">
+    <input class="form-control" type="hidden" value="<?php echo $Value; ?>" id="<?php echo $Name; ?>" name="<?php echo $Name; ?>">
+    <?php echo $Value; ?>
+  </div>
+</div>
+
+  <?php
 }
