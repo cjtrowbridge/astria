53fecd11594be4d030800fb7968403fde1560bc2
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-04 17:34:07 -0700

    Update SchemaRouter_RowColumns.php

--- plugins/SchemaRouter/SchemaRouter_RowColumns.php
+++ plugins/SchemaRouter/SchemaRouter_RowColumns.php
@@ -178,9 +178,10 @@ function SchemaRouter_RowColumns_Fields_BodyCallback($Schema, $Table, $Row = 0){
     echo '</div>'.PHP_EOL.'<div class="col-xs-12 col-lg-6">'.PHP_EOL;
     
     foreach($Referencees as $Table => $Referencee){
-      $SQL ="SELECT CONCAT('<a href=\"/".$Schema."/".$Table."/',".Sanitize($ASTRIA['Session']['Schema'][$Schema][$Table]['PRIMARY KEY']).",'\">',".Sanitize($ASTRIA['Session']['Schema'][$Schema][$Table]['FirstTextField']).",'</a>') as 'Connections To ".QualifiedPlural( SpacesBeforeCapitals( $Table ) )."'".PHP_EOL;
+      $SQL ="SELECT CONCAT('<a href=\"/".$Schema."/".$Table."/',".Sanitize($ASTRIA['Session']['Schema'][$Schema][$Table]['PRIMARY KEY']).",'\">',`".Sanitize($ASTRIA['Session']['Schema'][$Schema][$Table]['FirstTextField'])."`,'</a>') as 'Connections To ".QualifiedPlural( SpacesBeforeCapitals( $Table ) )."'".PHP_EOL;
       $SQL.="FROM `".$Table."`".PHP_EOL;
       $SQL.="WHERE `".Sanitize($Referencee['REFERENCED_COLUMN_NAME'])."` = '".intval($Row)."';";
+      pd(htmlentities($SQL));
       $Links = Query( $SQL,$Schema );
       if(count($Links)==0){
         echo '<p><b>No Linked '.QualifiedPlural( SpacesBeforeCapitals( $Table ) ).' Found</b></p>';
