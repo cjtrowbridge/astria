451d1fafacad27e0928a13f150adf77cc30c6fd5
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-04 18:09:23 -0700

    Update astria.js

--- js/astria.js
+++ js/astria.js
@@ -1,23 +1,3 @@
-function AddCard(
-  title,
-  text
-){
-  body = $('#bodyContainer');
-  body.append(
-'    <div class="card card-block">'+
-'      <h4 class="card-title">'+title+' <button type="button" class="btn btn-link float-xs-right muted" onclick="$(this).parent().parent().fadeOut(100);">x</button></h4>'+
-'      <p class="card-text">'+text+'</p>'+
-'    </div>'
-  );
-  $('card:last-of-type input:first-of-type').focus();
-}
-function Cardify(title,id){
-  AddCard(
-    title,
-    $('#'+id).html()
-  );
-}
-
 function AstriaEditor(){
   var x = $('.AstriaEditor.ready');
   $(x).keyup(function(){
@@ -37,4 +17,9 @@ function AstriaEditor(){
   });
   $(x).keyup();
   $(this).focus();
+}
+
+function AstriaToggleEditable(){
+  $('.AstriaToggleEditableInputs').slideToggle('fast');
+  $('.AstriaToggleEditableLabels').slideToggle('fast');
 }
