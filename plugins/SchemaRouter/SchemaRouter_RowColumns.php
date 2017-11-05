fac0b59caab5be48ac8d6db7b7349f7b52d97b6a
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-04 17:03:15 -0700

    Update SchemaRouter_RowColumns.php

--- plugins/SchemaRouter/SchemaRouter_RowColumns.php
+++ plugins/SchemaRouter/SchemaRouter_RowColumns.php
@@ -188,7 +188,7 @@ function SchemaRouter_RowColumns_Fields_BodyCallback($Schema, $Table, $Row = 0){
       
       $SQL ="SELECT '<a href=\"/".$Schema."/".$Table."/'+".Sanitize($ASTRIA['Session']['Schema'][$Table]['PRIMARY KEY'])."+'\">'+".Sanitize($ASTRIA['Session']['Schema'][$Table]['FirstTextField'])."+'</a>' as '".$Table."'".PHP_EOL;
       $SQL.="FROM `".$Table."`".PHP_EOL;
-      $SQL.="WHERE `".Sanitize($Referencee['REFERENCED_COLUMN_NAME'])." = '".intval($Row)."';"
+      $SQL.="WHERE `".Sanitize($Referencee['REFERENCED_COLUMN_NAME'])." = '".intval($Row)."';";
       pd($SQL);
       
       ?>
