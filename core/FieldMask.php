54a32b18f30205fe4056e6f929dce3a959ad175d
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-08 10:58:11 -0800

    Create FieldMask.php

---
+++ core/FieldMask.php
@@ -0,0 +1,33 @@
+<?php
+
+funciton FieldMask($Input){
+  
+  foreach($Input as $Row => &$Field){
+    
+    foreach($Field as $Key => $Value){
+      
+      //Custom functions can hook in here
+      Event('FieldMask Before');
+
+      switch($Key){
+        case 'UserInserted':
+        case 'UserUpdated':
+          //If this field is an integer UserID, change it to a user's name and link to the user's information.
+          if(intval($Value)==0){break;}
+          //TODO link to the user's profile once there are user profiles. for now, simply link to the schema route for that user which will probably become user profiles later.
+          $Results = Query( "SELECT FirstName, LastName FROM User WHERE UserID = ".intval($Value) );
+          //If a result is found, make the change
+          if(count($Results)==1){
+            $Value = '<a href="/astria/User/'.$Value.'">'.$Results[0]['FirstName'].' '.$Results[0]['LastName'].'</a>';
+          }
+          break;
+      }
+      
+      //Custom functions can hook in here
+      Event('FieldMask After');
+    }
+    
+  }
+  
+  return $Input;
+}
