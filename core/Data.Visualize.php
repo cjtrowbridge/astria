4f0a19c084d7b142ad35a34166febd377dc32e70
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-08 12:56:39 -0800

    Update Data.Visualize.php

--- core/Data.Visualize.php
+++ core/Data.Visualize.php
@@ -1,6 +1,6 @@
 <?php
 
-function Visualize($Data, $Type = 'line',$ID = false,$Color = false){
+function Visualize($Data, $Type = 'line',$ID = false,$Color = false, $Height = 250){
   
   //Maybe make up a unique id for the chart container
   if($ID = false){
@@ -42,30 +42,14 @@ function Visualize($Data, $Type = 'line',$ID = false,$Color = false){
   return '';
   
   return '
-    let data = {
-      labels: ["12am-3am", "3am-6am", "6am-9am", "9am-12pm", "12pm-3pm", "3pm-6pm", "6pm-9pm", "9pm-12am"],
-      datasets: [
-        {
-          title: "Some Data", color: "light-blue",
-          values: [25, 40, 30, 35, 8, 52, 17, -4]
-        },
-        {
-          title: "Another Set", color: "violet",
-          values: [25, 50, -10, 15, 18, 32, 27, 14]
-        },
-        {
-          title: "Yet Another", color: "blue",
-          values: [15, 20, -3, -15, 58, 12, -17, 37]
-        }
-      ]
-    };
+    let '.$ID.'_Data = '.$Output.';
     <div id="'.$ID.'">Loading Chart...</div>
     let chart = new Chart({
       parent: "#'.$ID.'",
       /*title: "My Awesome Chart",*/
-      data: data,
+      data: '.$ID.'_Data,
       type: "bar", // or "line", "scatter", "pie", "percentage"
-      height: 250
+      height: '.$Height.'
     });
   ';
 }
