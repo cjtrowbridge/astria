d16c9c09267a8eb200a0eeebb6df73807601ac44
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-08 11:04:11 -0800

    Update SchemaRouter_RowColumns.php

--- plugins/SchemaRouter/SchemaRouter_RowColumns.php
+++ plugins/SchemaRouter/SchemaRouter_RowColumns.php
@@ -228,7 +228,7 @@ function SchemaRouter_RowColumns_Fields_BodyCallback_ReadOnlyWithHidden($Label, 
         <div class="form-group row<?php echo $AdditionalClasses; ?>">
           <label for="<?php echo $Name; ?>" class="col-xs-12 col-lg-4 col-form-label"><?php echo $Label; ?>:</label>
           <div class="col-xs-12 col-lg-8">
-            <input class="form-control" type="hidden" value="<?php echo $Value; ?>" id="<?php echo $Name; ?>" name="<?php echo $Name; ?>">
+            <input class="form-control" type="hidden" value="<?php echo htmlentities($Value); ?>" id="<?php echo $Name; ?>" name="<?php echo $Name; ?>">
             <label class="col-form-label"><?php echo $Value; ?></label>
           </div>
         </div>
