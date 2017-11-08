25ac95a6ceb86dfa4fa7da2f7f412f681c07c094
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-08 11:08:46 -0800

    Update Patch.UserTableAddColumns.php

--- core/patches/Patch.UserTableAddColumns.php
+++ core/patches/Patch.UserTableAddColumns.php
@@ -32,6 +32,22 @@ function PatchUserTableAddColumns(){
     Event('IsWaiting column already exists');
   }
   
+  $Check = Query("
+    SELECT COUNT(*) as 'Check'
+    FROM information_schema.columns 
+    WHERE table_name = 'User'
+    AND column_name = 'TimeUpdated';
+  ");
+  if($Check[0]['Check']==0){
+    Event('Adding User UserInsertedColumn');
+    Query("ALTER TABLE `User` ADD `UserInserted` INT NULL;");
+    Query("ALTER TABLE `User` ADD `TimeInserted` DATETIME NULL;");
+    Query("ALTER TABLE `User` ADD `UserUpdated` INT NULL;");
+    Query("ALTER TABLE `User` ADD `TimeUpdated` DATETIME NULL;");
+  }else{
+    Event('User UserInserted column already exists');
+  }
+  
   //Need to update the first user to still be an admin
   //This assumes the first user will always be an admin
   Query("UPDATE User SET IsAstriaAdmin = TRUE, IsWaiting = FALSE WHERE UserID = 1;");
