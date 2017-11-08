507668e6aaed34f27eb06317669435cc9f62e9a1
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-08 12:15:15 -0800

    Update Data.Visualize.php

--- core/Data.Visualize.php
+++ core/Data.Visualize.php
@@ -1,10 +1,76 @@
 <?php
 
-function Visualize($Array){
-  //TODO reorganize the data
+function Visualize($Array, $Type = 'line',$ID = false){
   
-  //TODO generate a unique id for the chart
+  //Maybe make up a unique id for the chart container
+  if($ID = false){
+    $ID = 'chart_'.md5(uniqid());
+  }
   
-  //TODO return the html
+  $Data = json_encode($Data);
   
+  pd($Data);
+  return '';
+  
+  return '
+    let data = {
+      labels: ["12am-3am", "3am-6am", "6am-9am", "9am-12pm", "12pm-3pm", "3pm-6pm", "6pm-9pm", "9pm-12am"],
+      datasets: [
+        {
+          title: "Some Data", color: "light-blue",
+          values: [25, 40, 30, 35, 8, 52, 17, -4]
+        },
+        {
+          title: "Another Set", color: "violet",
+          values: [25, 50, -10, 15, 18, 32, 27, 14]
+        },
+        {
+          title: "Yet Another", color: "blue",
+          values: [15, 20, -3, -15, 58, 12, -17, 37]
+        }
+      ]
+    };
+    <div id="'.$ID.'">Loading Chart...</div>
+    let chart = new Chart({
+      parent: "#'.$ID.'",
+      /*title: "My Awesome Chart",*/
+      data: data,
+      type: "bar", // or "line", "scatter", "pie", "percentage"
+      height: 250
+    });
+  ';
 }
+
+/*
+
+
+  // Javascript
+  let data = {
+    labels: ["12am-3am", "3am-6am", "6am-9am", "9am-12pm",
+      "12pm-3pm", "3pm-6pm", "6pm-9pm", "9pm-12am"],
+
+    datasets: [
+      {
+        title: "Some Data", color: "light-blue",
+        values: [25, 40, 30, 35, 8, 52, 17, -4]
+      },
+      {
+        title: "Another Set", color: "violet",
+        values: [25, 50, -10, 15, 18, 32, 27, 14]
+      },
+      {
+        title: "Yet Another", color: "blue",
+        values: [15, 20, -3, -15, 58, 12, -17, 37]
+      }
+    ]
+  };
+
+  let chart = new Chart({
+    parent: "#chart", // or a DOM element
+    title: "My Awesome Chart",
+    data: data,
+    type: 'bar', // or 'line', 'scatter', 'pie', 'percentage'
+    height: 250
+  });
+
+*/
