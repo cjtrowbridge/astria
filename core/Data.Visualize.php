0f95c247af8f8d656f979f94d2230a5705a32a68
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-08 13:52:53 -0800

    Update Data.Visualize.php

--- core/Data.Visualize.php
+++ core/Data.Visualize.php
@@ -80,7 +80,7 @@ function VisualizeRandomColor($Key){
     'fucshia',
     'purple'
   );
-  $VisualizeRandomColor[$Key] = $WebSafeColors[rand(0,count($WebSafeColors))];
+  $VisualizeRandomColor[$Key] = $WebSafeColors[rand(0,count($WebSafeColors)-1)];
   return $VisualizeRandomColor[$Key];
 }
 
@@ -96,37 +96,3 @@ function VisualizeOrdinalize($Key){
   $VisualizeOrdinalize[$Key] = count($VisualizeOrdinalize);
   return $VisualizeOrdinalize[$Key];
 }
-
-/*
-
-
-  // Javascript
-  let data = {
-    labels: ["12am-3am", "3am-6am", "6am-9am", "9am-12pm",
-      "12pm-3pm", "3pm-6pm", "6pm-9pm", "9pm-12am"],
-
-    datasets: [
-      {
-        title: "Some Data", color: "light-blue",
-        values: [25, 40, 30, 35, 8, 52, 17, -4]
-      },
-      {
-        title: "Another Set", color: "violet",
-        values: [25, 50, -10, 15, 18, 32, 27, 14]
-      },
-      {
-        title: "Yet Another", color: "blue",
-        values: [15, 20, -3, -15, 58, 12, -17, 37]
-      }
-    ]
-  };
-
-  let chart = new Chart({
-    parent: "#chart", // or a DOM element
-    title: "My Awesome Chart",
-    data: data,
-    type: 'bar', // or 'line', 'scatter', 'pie', 'percentage'
-    height: 250
-  });
-
-*/
