4a854744a3dc69a9e31614409b8a6626787968c0
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-15 16:37:53 -0800

    Update SchemaRouter_RowColumns.php

--- plugins/SchemaRouter/SchemaRouter_RowColumns.php
+++ plugins/SchemaRouter/SchemaRouter_RowColumns.php
@@ -191,7 +191,11 @@ function SchemaRouter_RowColumns_Fields_BodyCallback($Schema, $Table, $Row = 0){
     echo '</div>'.PHP_EOL.'<div class="col-xs-12 col-lg-6">'.PHP_EOL;
     
     foreach($Referencees as $Table => $Referencee){
-      echo '<h3>'.rtrim($Referencee['TABLE_NAME'],'s').'s</h3>';
+      echo '<h3>';
+        echo '<span style="float: right;"><a class="text-muted" href="/'.$Schema.'/'.$Table.'/?insert&'.$PrimaryKey.'='.$Row.'">Insert New '.SpacesBeforeCapitals( $Table ).'</a>';
+        echo '</span>';
+      echo rtrim($Referencee['TABLE_NAME'],'s').'s';
+      echo '</h3>';
       $SQL ="SELECT CONCAT('<a href=\"/".$Schema."/".$Table."/',".Sanitize($ASTRIA['Session']['Schema'][$Schema][$Table]['PRIMARY KEY']).",'\">',`".Sanitize($ASTRIA['Session']['Schema'][$Schema][$Table]['FirstTextField'])."`,'</a>') as 'Connections To ".QualifiedPlural( SpacesBeforeCapitals( $Table ) )."'".PHP_EOL;
       $SQL.="FROM `".$Table."`".PHP_EOL;
       $SQL.="WHERE `".Sanitize($Referencee['REFERENCED_COLUMN_NAME'])."` = '".intval($Row)."';";
@@ -207,7 +211,7 @@ function SchemaRouter_RowColumns_Fields_BodyCallback($Schema, $Table, $Row = 0){
           echo '</p>';
         }
       }
-      echo '<p style="text-align: right;"><a class="text-muted" href="/'.$Schema.'/'.$Table.'/?insert&'.$PrimaryKey.'='.$Row.'">Insert New '.SpacesBeforeCapitals( $Table ).'</a></p><br>';
+      //echo '<p style="text-align: right;"><a class="text-muted" href="/'.$Schema.'/'.$Table.'/?insert&'.$PrimaryKey.'='.$Row.'">Insert New '.SpacesBeforeCapitals( $Table ).'</a></p><br>';
     }
     
   }
