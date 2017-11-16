36b885a120fafca07bd10f2c994afb9a087f3e2c
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-15 16:29:50 -0800

    Update SchemaRouter_RowColumns.php

--- plugins/SchemaRouter/SchemaRouter_RowColumns.php
+++ plugins/SchemaRouter/SchemaRouter_RowColumns.php
@@ -189,9 +189,9 @@ function SchemaRouter_RowColumns_Fields_BodyCallback($Schema, $Table, $Row = 0){
   if( count($Referencees)>0 ){
     //display two columns, one for this table, and one for things that have foreign keys referencing it
     echo '</div>'.PHP_EOL.'<div class="col-xs-12 col-lg-6">'.PHP_EOL;
-    echo '<h4>'.rtrim('s',$Table).'s</h4>';
     
     foreach($Referencees as $Table => $Referencee){
+      echo '<h4>'.rtrim('s',$Table).'s</h4>';
       $SQL ="SELECT CONCAT('<a href=\"/".$Schema."/".$Table."/',".Sanitize($ASTRIA['Session']['Schema'][$Schema][$Table]['PRIMARY KEY']).",'\">',`".Sanitize($ASTRIA['Session']['Schema'][$Schema][$Table]['FirstTextField'])."`,'</a>') as 'Connections To ".QualifiedPlural( SpacesBeforeCapitals( $Table ) )."'".PHP_EOL;
       $SQL.="FROM `".$Table."`".PHP_EOL;
       $SQL.="WHERE `".Sanitize($Referencee['REFERENCED_COLUMN_NAME'])."` = '".intval($Row)."';";
@@ -199,7 +199,9 @@ function SchemaRouter_RowColumns_Fields_BodyCallback($Schema, $Table, $Row = 0){
       if(count($Links)==0){
         echo '<p><b>No Linked '.QualifiedPlural( SpacesBeforeCapitals( $Table ) ).' Found</b></p>';
       }else{
-        echo ArrTabler( $Links );
+        foreach($Links as $Link){
+          echo '<p>'.$Link.'</p>';
+        }
       }
       echo '<p style="text-align: right;"><a class="text-muted" href="/'.$Schema.'/'.$Table.'/?insert&'.$PrimaryKey.'='.$Row.'">Insert New '.SpacesBeforeCapitals( $Table ).'</a></p><br>';
     }
