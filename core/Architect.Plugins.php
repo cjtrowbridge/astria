2a7f79fa2e2c5f527b9d209a55c5a256050e4aaa
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-16 11:27:27 -0800

    Update and rename FeedSync.php to Architect.Plugins.php

---
+++ core/Architect.Plugins.php
@@ -0,0 +1,5 @@
+<?php
+
+include_once('core/Condensr/Routing.php');
+include_once('core/FeedSync/Routing.php');
+include_once('core/RepoTracker/Routing.php');
--- core/FeedSync.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-include_once('core/FeedSync/Routing.php');
