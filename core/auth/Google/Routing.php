88adcad7e3e141f12af4abc1c6bc659f5e848b4a
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-15 17:28:55 -0800

    Update Routing.php

--- core/auth/Google/Routing.php
+++ core/auth/Google/Routing.php
@@ -35,7 +35,7 @@ function AlertAdminMissingGoogleOAuthCredentials(){
 }
 
 function GoogleChallengeSession(){
-  include_once('plugins/authGoogle/autoload.php');
+  include_once('core/auth/Google/autoload.php');
   global $ASTRIA;
   
   //Check if google was how we authenticated this session
@@ -73,7 +73,7 @@ function GoogleChallengeSession(){
 
 function AttemptGoogleAuth(){
   include_once('core/Session.php');
-  include_once('plugins/authGoogle/autoload.php');
+  include_once('core/auth/Google/autoload.php');
   global $ASTRIA;
   Event('Starting Google Auth Check');
   /************************************************
