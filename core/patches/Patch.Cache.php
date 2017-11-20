d56ce503d553a3fc77ea7ecde66fa1e7e4cfa488
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-20 14:34:19 -0800

    Create Patch.Cache.php

---
+++ core/patches/Patch.Cache.php
@@ -0,0 +1,18 @@
+<?php
+Hook('Run Astria Patches','PatchSchemaCacheTable();');
+
+function PatchSchemaCacheTable(){
+  Event('Patch: Looking for SchemaMeta table.');
+  
+  $Check = Query("SELECT DATA_TYPE FROM `information_schema`.`columns` WHERE TABLE_SCHEMA LIKE 'astria.io' AND TABLE_NAME LIKE 'Cache' AND COLUMN_NAME LIKE 'Content'");
+  if($Check[0]['Check']==0){
+    Event('Updating Cache content from text to longtext');
+    Query("ALTER TABLE `Cache` CHANGE `Content` `Content` LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL;");
+  }else{
+    Event('Cache content is already longtext');
+  }
+}
+
+
+
+
