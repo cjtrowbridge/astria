61f86bfca6f1f07869107a30cc1b5e7999aa7c4a
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-20 12:43:21 -0800

    Update Patch.SchemaMeta.php

--- core/patches/Patch.SchemaMeta.php
+++ core/patches/Patch.SchemaMeta.php
@@ -1,20 +1,16 @@
 <?php
-
+/*
 Hook('Run Astria Patches','PatchSchemaMetaTable();');
 
 function PatchSchemaMetaTable(){
   Event('Patch: Looking for SchemaMeta table.');
   
-  $Check = Query("
-    SELECT COUNT(*) as 'Check'
-    FROM information_schema.columns 
-    WHERE table_name = 'SchemaMeta';
-  ");
+  $Check = Query("SELECT COUNT(*) as 'Check' FROM information_schema.columns WHERE table_name = 'SchemaMeta';");
   if($Check[0]['Check']==0){
     Event('Adding SchemaMeta table');
-    Query("CREATE TABLE `astria.io`.`SchemaMeta` ( `SchemaMetaID` INT NOT NULL AUTO_INCREMENT , `Database` VARCHAR(255) NOT NULL , `Table` VARCHAR(255) NOT NULL , `Column` VARCHAR(255) NOT NULL , `Description` VARCHAR(255) NULL , `UserInserted` INT NOT NULL , `TimeInserted` DATETIME NOT NULL , `UserUpdated` INT NULL , `TimeUpdated` DATE NULL , PRIMARY KEY (`SchemaMetaID`)) ENGINE = InnoDB;
-");
+    Query("CREATE TABLE `astria.io`.`SchemaMeta` ( `SchemaMetaID` INT NOT NULL AUTO_INCREMENT , `Database` VARCHAR(255) NOT NULL , `Table` VARCHAR(255) NOT NULL , `Column` VARCHAR(255) NOT NULL , `Description` VARCHAR(255) NULL , `UserInserted` INT NOT NULL , `TimeInserted` DATETIME NOT NULL , `UserUpdated` INT NULL , `TimeUpdated` DATE NULL , PRIMARY KEY (`SchemaMetaID`)) ENGINE = InnoDB;");
   }else{
     Event('SchemaMeta table already exists');
   }
 }
+*/
