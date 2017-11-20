11666e9d6acbec28cb3ef8c8f2416b3e01b66234
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-19 20:48:11 -0800

    Update Routing.php

--- core/auth/Google/Routing.php
+++ core/auth/Google/Routing.php
@@ -110,9 +110,10 @@ function AttemptGoogleAuth(){
     Event('Google Auth Check: User is attempting to log in. Check with google and refresh.');
     $client->authenticate($_GET['code']);
     $ASTRIA['Session']['google_oauth2']=array('access_token' => $client->getAccessToken());
-    AstriaSessionSave();
-    header('Location: /');
-    exit;
+    //AstriaSessionSave();
+    //header('Location: /');
+    //exit;
+    //there is no reason to redirect. the next if statement will take care of everything...
   }
   
   /************************************************
