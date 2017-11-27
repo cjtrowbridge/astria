7e69ad17c81ded8d3569c58af5c1b4df33813d66
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-26 17:49:48 -0800

    Update Patch.Cache.php

--- core/patches/Patch.Cache.php
+++ core/patches/Patch.Cache.php
@@ -5,7 +5,7 @@ function PatchSchemaCacheTable(){
   Event('Patch: Looking for SchemaMeta table.');
   
   $Check = Query("SELECT DATA_TYPE FROM `information_schema`.`columns` WHERE TABLE_SCHEMA LIKE 'astria.io' AND TABLE_NAME LIKE 'Cache' AND COLUMN_NAME LIKE 'Content'");
-  if($Check[0]['Check']==0){
+  if($Check[0]['DATA_TYPE']=='text'){
     Event('Updating Cache content from text to longtext');
     Query("ALTER TABLE `Cache` CHANGE `Content` `Content` LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL;");
   }else{
