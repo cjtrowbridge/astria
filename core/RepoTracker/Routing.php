b1ef09687e479d9c9cc633570e44352560cb6eb2
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-26 17:55:23 -0800

    Update Routing.php

--- core/RepoTracker/Routing.php
+++ core/RepoTracker/Routing.php
@@ -1,5 +1,6 @@
 <?php
 
+Hook('Architect Notifications','RepoTracker_CheckForUpdates();');
 Hook('User Is Logged In','RepoTrackerLoad();');
 
 function RepoTrackerLoad(){
@@ -8,6 +9,54 @@ function RepoTrackerLoad(){
     Hook('Architect Tools 1','RepoTracker_ArchitectButton();');
   }
 }
+
+function RepoTracker_CheckForUpdates(){
+  
+}
+
+Hook('User Is Logged In','RepoTracker_MaybeCheckForUpdates();');
+function RepoTracker_MaybeCheckForUpdates(){
+  if(isset($_GET['RepoTrackerCheckForUpdates'])){
+    if(
+      IsAstriaAdmin()
+    ){
+      RepoTracker_CheckNowForUpdates();
+      exit;
+    }else{
+      die('You do not have permission to check for updates with RepoTracker. ');
+    }
+  }
+}
+function RepoTracker_CheckForUpdates(){
+  ?>
+<span id="RepoTrackerUpdatesChecker"><img src="/img/ajax-loader.gif" title="Checking With RepoTracker For Updates..."></span>
+<script>
+  $.get("/?RepoTrackerCheckForUpdates",function(data){
+    $('#RepoTrackerUpdatesChecker').html(data);
+  });
+</script>
+
+  <?php
+}
+
+function RepoTracker_CheckNowForUpdates(){
+  
+  /*
+  RepoTrackerRefresh();
+  RepoTracker_VerifyLocalHashes();
+  RepoTracker_VerifyRemoteHashes();
+  RepoTracker_PullBleedingEdgeRepos();
+  */
+  
+  $AvailableUpdates = Query("SELECT COUNT(*) AS 'Updates' FROM Repository WHERE LocalHash NOT LIKE RemoteHash;");
+  
+  if($AvailableUpdates[0]['Updates']>0){
+    echo '<a href="/repotracker">Updates Available <i class="material-icons" title="Updates Available" style="color: red;">system_update_alt</i></a>';
+  }else{
+    ?>Checked Today; Up To Date <i class="material-icons" title="Up To Date">done</i><?php
+  }
+}
+
 
 function RepoTracker_ArchitectButton(){
   ?>
@@ -46,7 +95,7 @@ function RepoTracker_VerifyTables(){
         `Path` VARCHAR(255) NOT NULL , 
         `LocalHash` VARCHAR(255) NULL , 
         `RemoteHash` VARCHAR(255) NULL ,  
-        `BleedingEdge` BOOLEAN NOT NULL DEFAULT FALSE ,
+        `BleedingEdge` BOOLEAN NOT NULL DEFAULT FALSE , 
         `LastChecked` DATETIME NULL,
         PRIMARY KEY (`RepositoryID`), 
         INDEX (`Path`)
