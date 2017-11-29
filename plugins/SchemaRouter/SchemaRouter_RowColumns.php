625304e5a95dcc0b6bf132e933606892eded3d04
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-29 15:25:14 -0800

    Update SchemaRouter_RowColumns.php

--- plugins/SchemaRouter/SchemaRouter_RowColumns.php
+++ plugins/SchemaRouter/SchemaRouter_RowColumns.php
@@ -424,6 +424,11 @@ function SchemaRouter_RowColumns_Fields_BodyCallback_EditableText($Label, $Name,
                   <?php
                   break;
                 case 'text':
+                case 'longtext':
+                  ?>
+                  <textarea class="form-control AstriaToggleEditableInputs" type="text" <?php if($Required){echo 'required="true" ';} ?>id="<?php echo $Name; ?>" name="<?php echo $Name; ?>"><?php if(isset($_GET[$Name])){echo $_GET[$Name];}else{echo $Value;} ?></textarea>
+                  <?php
+                  break;
                 case 'varchar':
                   ?>
                   <input class="form-control AstriaToggleEditableInputs" type="text" <?php if($Required){echo 'required="true" ';} ?>value="<?php if(isset($_GET[$Name])){echo $_GET[$Name];}else{echo $Value;} ?>" id="<?php echo $Name; ?>" name="<?php echo $Name; ?>">
