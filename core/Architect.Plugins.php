09beb70ef471694e8563b7dd536d3b639e591de2
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-12-01 14:44:48 -0800

    Update Architect.Plugins.php

--- core/Architect.Plugins.php
+++ core/Architect.Plugins.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once('core/Crawler/Routing.php');
+include_once('core/architect/Crawler/Routing.php');
 include_once('core/Condensr/Routing.php');
 include_once('core/FeedSync/Routing.php');
 include_once('core/RepoTracker/Routing.php');
