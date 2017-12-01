89175cedbbdb1d12d4193a2129045dfbbc07c366
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-12-01 14:54:45 -0800

    Create Page.New.php

---
+++ core/architect/Crawler/Page.New.php
@@ -0,0 +1,13 @@
+<?php
+
+function Architect_Crawler_New(){
+  
+  
+  TemplateBootstrap4('New - Crawler - Architect','Architect_Crawler_New_BodyCallback();');
+}
+
+function Architect_Crawler_New_BodyCallback(){
+  ?><h1><a href="/architect">Architect</a> / <a href="/architect/crawler">Crawler</a> / <a href="/architect/crawler/new">New</a></h1>
+
+  <?php
+}
