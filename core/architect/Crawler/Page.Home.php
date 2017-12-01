c6e9e1b86a3fc2cd388b22cab643ee425ef6a44b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-12-01 14:44:09 -0800

    Create Page.Home.php

---
+++ core/architect/Crawler/Page.Home.php
@@ -0,0 +1,13 @@
+<?php
+
+function Architect_Crawler_Homepage(){
+  
+  
+  TemplateBootstrap4('New - Crawler - Architect','Architect_Crawler_Homepage_BodyCallback();');
+}
+
+function Architect_Crawler_Homepage_BodyCallback(){
+  ?>
+  <h1>Architect / Crawler / New</h1>
+  <?php
+}
