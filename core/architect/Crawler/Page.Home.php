ac62b2934804fc7cdf1b6a9ee39dc9352f40ff5b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-12-01 14:55:11 -0800

    Update Page.Home.php

--- core/architect/Crawler/Page.Home.php
+++ core/architect/Crawler/Page.Home.php
@@ -7,7 +7,7 @@ function Architect_Crawler_Homepage(){
 }
 
 function Architect_Crawler_Homepage_BodyCallback(){
-  ?><h1><a href="/architect">Architect</a> / <a href="/architect/crawler">Crawler</a> / <a href="/architect/crawler/new">New</a></h1>
-
+  ?><h1><a href="/architect">Architect</a> / <a href="/architect/crawler">Crawler</a></h1>
+  <p>This should be a status page of all the details about the crawls we are doing.</p>
   <?php
 }
