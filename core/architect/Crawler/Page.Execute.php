aa1a34fe9ee81004a2a8faa3243730eadd6fefb8
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-12-01 16:42:03 -0800

    Create Page.Execute.php

---
+++ core/architect/Crawler/Page.Execute.php
@@ -0,0 +1,23 @@
+<?php
+
+function Architect_Crawler_Execute(){
+  $CrawlerID = intval(path(3));
+  if($CrawlerID == false){
+    die('Invalid Crawler ID');
+  }
+  
+  $Crawler = Query('SELECT * FROM Crawler WHERE CrawlerID = '.$CrawlerID);
+  if(!(isset($Crawler[0]))){
+    die('Crawler Not Found');
+  }
+  $Crawler=$Crawler[0];
+  
+  $Tasks = Query("SELECT URL FROM CrawlerTask WHERE CrawlerID = ".intval($CrawlerID)." ORDER BY CrawlerTaskID ASC");
+  
+  foreach($Tasks as $Task){
+    ?>
+    <p><a href="/architect/Crawler/execute/<?php echo $Task['CrawlerID']; ?>/?execute=<?php echo $Task['CrawlerTaskID']; ?>">Execute Task <?php echo $Task['CrawlerTaskID']; ?>: <?php echo $Task['URL']; ?></a></p>
+    <?php
+  }
+  
+}
