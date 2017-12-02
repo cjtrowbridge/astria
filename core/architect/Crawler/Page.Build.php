77dea8ac2a7570c08c68998c19ba5676ff897549
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-12-01 16:22:08 -0800

    Update Page.Build.php

--- core/architect/Crawler/Page.Build.php
+++ core/architect/Crawler/Page.Build.php
@@ -14,5 +14,9 @@ function Architect_Crawler_Build(){
   
   pd($Crawler);
   
+  for($i = $Crawler['RangeMin']; $i < $Crawler['RangeMax']; $i+=$Crawler['RangeIncrement']){
+    echo $Crawler['Protocol'].'://'.$Crawler['Domain'].'/'.$Crawler['Path'].'?'.$Crawler['QueryVariable1'].'='.$Query1Value.'&'.$Crawler['QueryVariable2'].'='.$Query2Value.'&'.$Crawler['RangeVariable'].'='.$i.'<br>';
+  }
+  
   exit;
 }
