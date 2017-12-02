0e45488c1482017a26dd4cedcad6dc198e831d78
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-12-01 21:22:54 -0800

    Update Page.Execute.php

--- core/architect/Crawler/Page.Execute.php
+++ core/architect/Crawler/Page.Execute.php
@@ -64,7 +64,7 @@ function Architect_Crawler_Execute_BodyCallback(){
   $Tasks = Query("SELECT CrawlerTaskID, CrawlerID, URL FROM CrawlerTask WHERE Message IS NULL AND CrawlerID = ".intval($CrawlerID)." ORDER BY CrawlerTaskID ASC");
   foreach($Tasks as $Task){
     ?>
-    <p><a href="/architect/Crawler/execute/<?php echo $Task['CrawlerID']; ?>/?execute=<?php echo $Task['CrawlerTaskID']; ?>" target="_blank" onclick="callLink();">Execute Task <?php echo $Task['CrawlerTaskID']; ?>: <?php echo $Task['URL']; ?></a></p>
+    <p><a href="/architect/Crawler/execute/<?php echo $Task['CrawlerID']; ?>/?execute=<?php echo $Task['CrawlerTaskID']; ?>" target="_blank">Execute Task <?php echo $Task['CrawlerTaskID']; ?>: <?php echo $Task['URL']; ?></a></p>
     <?php
   }
   
