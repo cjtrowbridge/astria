ce26b4302d6c07101c6d793fef7db1c57b0ec9bc
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-12-01 21:09:15 -0800

    Update Page.Home.php

--- core/architect/Crawler/Page.Home.php
+++ core/architect/Crawler/Page.Home.php
@@ -10,7 +10,7 @@ function Architect_Crawler_Homepage_BodyCallback(){
   ?><h1><a href="/architect">Architect</a> / <a href="/architect/crawler">Crawler</a></h1>
   
   <?php 
-    $Crawlers = Query('SELECT *,(SELECT COUNT(*) FROM CrawlerTask WHERE CrawlerTask.CrawlerID = Crawler.CrawlerID AND CrawlerDataset.CrawlerID = CrawlerTask.CrawlerID) as "Tasks" FROM Crawler LEFT JOIN CrawlerDataset ON Crawler.CrawlerID = CrawlerDataset.CrawlerID');
+    $Crawlers = Query('SELECT *,(SELECT COUNT(*) FROM CrawlerTask WHERE CrawlerDataset.CrawlerID = CrawlerTask.CrawlerID) as "Tasks" FROM Crawler LEFT JOIN CrawlerDataset ON Crawler.CrawlerID = CrawlerDataset.CrawlerID');
     foreach($Crawlers as $Crawler){
       
       $Query1Value = 'plumbing';
@@ -23,8 +23,8 @@ function Architect_Crawler_Homepage_BodyCallback(){
             <h4><a href="/astria/Crawler/<?php echo $Crawler['CrawlerID']; ?>"><?php echo $Crawler['Name']; ?></a>: <a href="/astria/CrawlerDataset/<?php echo $Crawler['CrawlerDatasetID']; ?>"><?php echo $Crawler['Query1'].' '.$Crawler['Query2']; ?></a></h4>
             <p><?php echo $Crawler['Description']; ?></p>
             
-            <a class="btn btn-success btn-block" target="_blank" href="<?php echo $Crawler['Protocol'].'://'.$Crawler['Domain'].'/'.$Crawler['Path'].'?'.$Crawler['QueryVariable1'].'='.$Query1Value.'&'.$Crawler['QueryVariable2'].'='.$Query2Value.'&'.$Crawler['RangeVariable'].'='.$Crawler['RangeMin']; ?>">Test First Page Link</a>
-            <a class="btn btn-success btn-block" target="_blank" href="<?php echo $Crawler['Protocol'].'://'.$Crawler['Domain'].'/'.$Crawler['Path'].'?'.$Crawler['QueryVariable1'].'='.$Query1Value.'&'.$Crawler['QueryVariable2'].'='.$Query2Value.'&'.$Crawler['RangeVariable'].'='.($Crawler['RangeMax']-$Crawler['RangeMax']%$Crawler['RangeIncrement']); ?>">Test Last Page Link</a>
+            <a class="btn btn-success" target="_blank" href="<?php echo $Crawler['Protocol'].'://'.$Crawler['Domain'].'/'.$Crawler['Path'].'?'.$Crawler['QueryVariable1'].'='.$Query1Value.'&'.$Crawler['QueryVariable2'].'='.$Query2Value.'&'.$Crawler['RangeVariable'].'='.$Crawler['RangeMin']; ?>">Test First Page Link</a>
+            <a class="btn btn-success" target="_blank" href="<?php echo $Crawler['Protocol'].'://'.$Crawler['Domain'].'/'.$Crawler['Path'].'?'.$Crawler['QueryVariable1'].'='.$Query1Value.'&'.$Crawler['QueryVariable2'].'='.$Query2Value.'&'.$Crawler['RangeVariable'].'='.($Crawler['RangeMax']-$Crawler['RangeMax']%$Crawler['RangeIncrement']); ?>">Test Last Page Link</a>
             <br>
             Incomplete Tasks: <?php echo $Crawler['Tasks']; ?> <a class="btn btn-danger" href="/architect/crawler/build/<?php echo $Crawler['CrawlerID']; ?>">Build Task List</a>
             
