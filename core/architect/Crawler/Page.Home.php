534084d5bb35f44d43fd32e0a84516a96d6bec89
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-12-01 16:06:01 -0800

    Update Page.Home.php

--- core/architect/Crawler/Page.Home.php
+++ core/architect/Crawler/Page.Home.php
@@ -10,7 +10,7 @@ function Architect_Crawler_Homepage_BodyCallback(){
   ?><h1><a href="/architect">Architect</a> / <a href="/architect/crawler">Crawler</a></h1>
   
   <?php 
-    $Crawlers = Query('SELECT * FROM Crawler');
+    $Crawlers = Query('SELECT *,(SELECT COUNT(*) FROM CrawlerTask WHERE CrawlerTask.CrawlerID = Crawler.CrawlerID) as "Tasks" FROM Crawler');
     foreach($Crawlers as $Crawler){
       
       $Query1Value = 'plumbing';
@@ -25,6 +25,8 @@ function Architect_Crawler_Homepage_BodyCallback(){
             
             <a class="btn btn-success btn-block" href="<?php echo $Crawler['Protocol'].'://'.$Crawler['Domain'].'/'.$Crawler['Path'].'?'.$Crawler['QueryVariable1'].'='.$Query1Value.'&'.$Crawler['QueryVariable2'].'='.$Query2Value.'&'.$Crawler['RangeVariable'].'='.$Crawler['RangeMin']; ?>">Test First Page Link</a>
             <a class="btn btn-success btn-block" href="<?php echo $Crawler['Protocol'].'://'.$Crawler['Domain'].'/'.$Crawler['Path'].'?'.$Crawler['QueryVariable1'].'='.$Query1Value.'&'.$Crawler['QueryVariable2'].'='.$Query2Value.'&'.$Crawler['RangeVariable'].'='.($Crawler['RangeMax']-$Crawler['RangeMax']%$Crawler['RangeIncrement']); ?>">Test Last Page Link</a>
+            
+            <a class="btn btn-danger" href="/architect/crawler/build/<?php echo $Crawler['CrawlerID']; ?>">Build Task List</a> Incomplete Tasks: <?php echo $Crawler['Tasks']; ?>
             
           </div>
         </div>
