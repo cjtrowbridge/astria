665e463f61e54a950c6845dead36f9f34db83184
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-12-01 21:11:01 -0800

    Update Page.Home.php

--- core/architect/Crawler/Page.Home.php
+++ core/architect/Crawler/Page.Home.php
@@ -23,7 +23,7 @@ function Architect_Crawler_Homepage_BodyCallback(){
             <h4><a href="/astria/Crawler/<?php echo $Crawler['CrawlerID']; ?>"><?php echo $Crawler['Name']; ?></a>: <a href="/astria/CrawlerDataset/<?php echo $Crawler['CrawlerDatasetID']; ?>"><?php echo $Crawler['Query1'].' '.$Crawler['Query2']; ?></a></h4>
             <p><?php echo $Crawler['Description']; ?></p>
             
-            <a class="btn btn-success" target="_blank" href="<?php echo $Crawler['Protocol'].'://'.$Crawler['Domain'].'/'.$Crawler['Path'].'?'.$Crawler['QueryVariable1'].'='.$Query1Value.'&'.$Crawler['QueryVariable2'].'='.$Query2Value.'&'.$Crawler['RangeVariable'].'='.$Crawler['RangeMin']; ?>">Test First Page Link</a>
+            <a class="btn btn-success" target="_blank" href="<?php echo $Crawler['Protocol'].'://'.$Crawler['Domain'].'/'.$Crawler['Path'].'?'.$Crawler['QueryVariable1'].'='.$Query1Value.'&'.$Crawler['QueryVariable2'].'='.$Query2Value.'&'.$Crawler['RangeVariable'].'='.$Crawler['RangeMin'].'&dataset='.$Crawler['CrawlerDatasetID']; ?>">Test First Page Link</a>
             <a class="btn btn-success" target="_blank" href="<?php echo $Crawler['Protocol'].'://'.$Crawler['Domain'].'/'.$Crawler['Path'].'?'.$Crawler['QueryVariable1'].'='.$Query1Value.'&'.$Crawler['QueryVariable2'].'='.$Query2Value.'&'.$Crawler['RangeVariable'].'='.($Crawler['RangeMax']-$Crawler['RangeMax']%$Crawler['RangeIncrement']); ?>">Test Last Page Link</a>
             <a class="btn btn-danger" href="/architect/crawler/build/<?php echo $Crawler['CrawlerID']; ?>">Build Task List</a>
             
