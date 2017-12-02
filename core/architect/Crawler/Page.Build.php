6e36324d864b539862e6bfce92b41f2163558424
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-12-01 16:10:52 -0800

    Create Page.Build.php

---
+++ core/architect/Crawler/Page.Build.php
@@ -0,0 +1,18 @@
+<?php
+
+function Architect_Crawler_Build(){
+  $CrawlerID = intval(path(3));
+  if($CrawlerID == false){
+    die('Invalid Crawler ID');
+  }
+  
+  $Crawler = Query('SELECT * FROM Crawler WHERE CrawlerID = '.$CrawlerID);
+  if(!(isset($Crawler[0]))){
+    die('Crawler Not Found');
+  }
+  $Crawler=$Crawler[0];
+  
+  pd($Crawler);
+  
+  exit;
+}
