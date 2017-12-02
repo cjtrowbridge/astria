ff10a78dd7dc9a400f2e519d92d04124da6ca4d6
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-12-01 16:28:22 -0800

    Update Page.Build.php

--- core/architect/Crawler/Page.Build.php
+++ core/architect/Crawler/Page.Build.php
@@ -17,9 +17,27 @@ function Architect_Crawler_Build(){
   $Query1Value = 'plumbing';
   $Query2Value = '95603';
   
+  $Actions = array();
+  
   for($i = $Crawler['RangeMin']; $i < $Crawler['RangeMax']; $i+=$Crawler['RangeIncrement']){
-    echo $Crawler['Protocol'].'://'.$Crawler['Domain'].'/'.$Crawler['Path'].'?'.$Crawler['QueryVariable1'].'='.$Query1Value.'&'.$Crawler['QueryVariable2'].'='.$Query2Value.'&'.$Crawler['RangeVariable'].'='.$i.'<br>';
+    $URL = $Crawler['Protocol'].'://'.$Crawler['Domain'].'/'.$Crawler['Path'].'?'.$Crawler['QueryVariable1'].'='.$Query1Value.'&'.$Crawler['QueryVariable2'].'='.$Query2Value.'&'.$Crawler['RangeVariable'].'='.$i
+    $Actions[$URL] = $URL;
   }
+  
+  //Find any of these whcih are already inserted while not yet done.
+  $SQL = "SELECT URL FROM CrawlerTask WHERE Data IS NULL AND (".PHP_EOL;
+  foreach($Actions as $Action){
+    $SQL.="URL LIKE '".Sanitize($Action)."' OR".PHP_EOL;
+  }
+  $SQL.="1=2 )";
+  $Alreadys = Query($SQL);
+    
+  //Remove any actions which are already queued
+  foreach($Alreadys as $Already){
+    unset($Action[$Already]);
+  }
+  
+  pd($Actions);
   
   exit;
 }
