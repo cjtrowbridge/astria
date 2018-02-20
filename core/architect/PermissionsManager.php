c5b8d03a568f4cf3ec7eb2a11d418c3b9bad4bdc
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-02-19 17:53:07 -0800

    Create PermissionsManager.php

---
+++ core/architect/PermissionsManager.php
@@ -0,0 +1,17 @@
+<?php
+
+function PermissionsManager(){
+  //Handle posts and update objects
+  
+  
+  TemplateBootstrap4('Permissions Manager','PermissionsManagerBodyCallback();');
+}
+
+function PermissionsManagerBodyCallback(){
+
+
+
+  ?><h1>Select a User or Group to Edit Permissions...</h1>
+  
+  <?php
+}
