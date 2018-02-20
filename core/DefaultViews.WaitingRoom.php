95c875aebd703f38edd314d3a65ef6520d4aecb2
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-02-19 16:43:38 -0800

    Update DefaultViews.WaitingRoom.php

--- core/DefaultViews.WaitingRoom.php
+++ core/DefaultViews.WaitingRoom.php
@@ -10,7 +10,8 @@ function WaitingRoomBodyCallback(){
 		<div class="row">
 			<div class="col-xs-12">
 				<h1>Waiting</h1>
-        <p>Welcome to the waiting room.</p>
+				<p>Welcome to the waiting room.</p>
+				<a href="/logout">Log Out</a>
 			</div>
 		</div>
 	</div>
