b43eda2b78d37cf399aff23eab2898eb452ba308
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-02-19 17:56:25 -0800

    Update PermissionsManager.php

--- core/architect/PermissionsManager.php
+++ core/architect/PermissionsManager.php
@@ -12,6 +12,25 @@ function PermissionsManagerBodyCallback(){
 
 
   ?><h1>Select a User or Group to Edit Permissions...</h1>
-  
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-md-6">
+        <h2>Users</h2>
+        <?php
+          $SQL = "SELECT UserID, Email, FirstName, LastName FROM User";
+          $Users = Query($SQL);
+          echo ArrTabler($Users);
+        ?>
+      </div>
+      <div class="col-xs-12 col-md-6">
+        <h2>Groups</h2>
+        <?php
+          $SQL = "SELECT GropuID, Name, Description FROM UserGroup";
+          $Groups = Query($SQL);
+          echo ArrTabler($Groups);
+        ?>
+      </div>
+    </div>
+  </div>
   <?php
 }
