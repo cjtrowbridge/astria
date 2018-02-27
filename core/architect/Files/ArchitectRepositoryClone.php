8f0c61389d1a0d4b1ca0254a3b36aee50ec17cf3
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-02-26 20:23:20 -0800

    Update ArchitectRepositoryClone.php

--- core/architect/Files/ArchitectRepositoryClone.php
+++ core/architect/Files/ArchitectRepositoryClone.php
@@ -4,12 +4,11 @@ function ArchitectRepositoryClone(){
   
   if(isset($_POST['origin'])){
     pd($_POST);
-    /*
-    if(mkdir($_SERVER['DOCUMENT_ROOT'].$_POST['newDirectory'],0775)==false){
-      die('<p>Unable to create: '.$_GET['path'].'</p>');
-    }
-    header('Location: /architect/files/?path='.$_POST['newDirectory']);
-    */
+    
+    $Command = 'cd '.$_POST['pwd'].' && git clone '.$_POST['origin'];
+    echo $Command;
+    
+    //header('Location: /architect/files/?path='.$_POST['newDirectory']);
     exit;
   }
   
@@ -31,7 +30,7 @@ function ArchitectRepositoryCloneBodyCallback(){
     <input type="text" class="form-control" name="origin" id="origin"><br>
     Into:</br>
     <input type="text" class="form-control" name="pwd" id="pwd" value="<?php echo $_GET['path']; ?>"><br>
-    <p><b>Note 1:</b> This will literally call 'cd [into] && git clone [origin]'. So it's going to make a subdirectory. Alternatively, you can add an argument to the origin field which will be appended to the command. Keep in mind this will create a subdirectory unless you specify otherwise.</p>
+    <p><b>Note 1:</b> This will literally call 'cd [into] && git clone [origin]'. There is no sanitation. And it's going to make a subdirectory. Alternatively, you can add an argument to the origin field which will be appended to the command. Keep in mind this will create a subdirectory unless you specify otherwise.</p>
     <p><b>Note 2:</b> This might take a while if it is really big or complicated. Don't refresh or click again or IDK what will happen D:</p>
     <input type="submit" class="btn btn-block btn-success" value="Clone Repository">
   </form>
