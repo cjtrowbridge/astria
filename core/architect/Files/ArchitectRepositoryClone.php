4c369d07efe2c7312bfdb0f8593a8f6b8ab5be93
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-02-26 20:16:46 -0800

    Update ArchitectRepositoryClone.php

--- core/architect/Files/ArchitectRepositoryClone.php
+++ core/architect/Files/ArchitectRepositoryClone.php
@@ -27,9 +27,9 @@ function ArchitectRepositoryCloneBodyCallback(){
   <h1>Clone Repository</h1>
   <form action="/architect/files/clone-repository/" method="post" class="form">
     <input type="text" class="form-control" name="origin" id="origin"><br>
+    <p><b>Note:</b> This might take a while if it is really big or complicated. Don't refresh or click again or IDK what will happen D:</p>
     <input type="submit" class="btn btn-block btn-success" value="Clone Repository">
   </form>
-  <p><b>Note:</b> This might take a while if it is really big or complicated. Don't refresh or click again or IDK what will happen D:</p>
   
   <?php
 }
