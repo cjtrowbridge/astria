be6bc46104a3c61608b2f8e33ad9a9e1fc561500
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-02-26 20:21:13 -0800

    Update ArchitectRepositoryClone.php

--- core/architect/Files/ArchitectRepositoryClone.php
+++ core/architect/Files/ArchitectRepositoryClone.php
@@ -30,7 +30,7 @@ function ArchitectRepositoryCloneBodyCallback(){
     Origin:<br>
     <input type="text" class="form-control" name="origin" id="origin"><br>
     Into:</br>
-    <input type="text" class="form-control" name="pwd" id="pwd" value="<?php echo $_GET['path']; ?>><br>
+    <input type="text" class="form-control" name="pwd" id="pwd" value="<?php echo $_GET['path']; ?>"><br>
     <p><b>Note 1:</b> This will literally call 'cd [into] && git clone [origin]'. So it's going to make a subdirectory. Alternatively, you can add an argument to the origin field which will be appended to the command. Keep in mind this will create a subdirectory unless you specify otherwise.</p>
     <p><b>Note 2:</b> This might take a while if it is really big or complicated. Don't refresh or click again or IDK what will happen D:</p>
     <input type="submit" class="btn btn-block btn-success" value="Clone Repository">
